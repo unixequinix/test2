d17591f51066a153cec0e6e05f5cb42d36a94140
Alejandro González <alex@acidtango.com>
2015-05-03 12:55:11 +0200

    Email Url configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.host, port: 3000 }
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.host }
 end
