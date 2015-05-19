5debc1be5ce74b58ac763169c00c28cc279d9898
Alejandro González <alex@acidtango.com>
2015-05-19 16:37:05 +0200

    Order mailer

--- app/controllers/customers/payments_controller.rb
+++ app/controllers/customers/payments_controller.rb
@@ -10,10 +10,11 @@ class Customers::PaymentsController < Customers::BaseController
       amount = params[:Ds_Amount].to_f / 100 # last two digits are decimals
       if success
         @order = Order.find_by(number: params[:Ds_Order])
-        @order.complete!
         @credit_log = CreditLog.create(customer_id: @order.customer.id, transaction_type: CreditLog::CREDITS_PURCHASE, amount: @order.credits_total)
         payment = Payment.new(transaction_type: params[:Ds_TransactionType], card_country: params[:Ds_Card_Country], paid_at: "#{params[:Ds_Date]}, #{params[:Ds_Hour]}", order: @order, response_code: response, authorization_code: params[:Ds_AuthorisationCode], currency: params[:Ds_Currency], merchant_code: params[:Ds_MerchantCode], amount: amount,  terminal: params[:Ds_Terminal], success: true)
         payment.save!
+        @order.complete!
+        send_mail_for(@order)
       end
     end
     render nothing: true
@@ -29,6 +30,12 @@ class Customers::PaymentsController < Customers::BaseController
     if !current_customer.assigned_admission.nil?
       @admission = Admission.find(current_customer.assigned_admission.id)
     end
+  end
+
+  private
+
+  def send_mail_for(order)
+    OrderMailer.completed_email(order).deliver_later
   end
 
 end
