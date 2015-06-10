a1c74d8d5a1579b4fe88730845d64c605b169054
Manuel Cavero <manuel@acidtango.com>
2015-06-10 16:01:50 +0200

    Add fastclick and separator on footer languages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 
 //= require customer/top-up-buttons
 //= require customer/nav-mobile
+//= require customer/fastclick
 //= require admin/header
 //= require vendor/fastclick
 //= require vendor/jquery.cookiebar
--- app/assets/javascripts/application_mobile.js
+++ app/assets/javascripts/application_mobile.js
@@ -3,6 +3,7 @@
 //= require turbolinks
 
 //= require customer/top-up-buttons
+//= require customer/fastclick
 //= require admin/header
 //= require vendor/fastclick
 //= require vendor/jquery.cookiebar
