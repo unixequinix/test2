d83d7bbe465e369183e115dcf615e560d94b857e
Manuel Cavero <manuel@acidtango.com>
2015-06-10 18:12:40 +0200

    Refactor variables

--- app/assets/javascripts/admin_mobile.js
+++ app/assets/javascripts/admin_mobile.js
@@ -3,8 +3,8 @@
 //= require turbolinks
 
 //= require vendor/mmenu/jquery.mmenu.min
-//= require admin/mobile/side_menu
 
+//= require admin/mobile/side_menu
 //= require admin/file_fields
 //= require admin/delete_all
 //= require admin/input_focus
