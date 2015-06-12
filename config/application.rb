6fae59e7b4d796aad1cb82b1f711033d1aec400e
Alejandro González <alex@acidtango.com>
2015-06-12 16:28:55 +0200

    background Fixed

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module Gspot
     config.assets.precompile += %w[welcome_admin.css welcome_admin.js]
     config.assets.precompile += %w[admin.css admin.js]
     config.assets.precompile += %w[admin_mobile.css admin_mobile.js]
+    config.assets.precompile += %w[customer/configuration/background/background_fixed.css]
+    config.assets.precompile += %w[customer/configuration/background/background_repeat.css]
 
     config.paperclip_defaults = {
       storage: :s3,
