ff1b884f5b80f9f7f13857dd32c4e4da14e85218
Alejandro González <alex@acidtango.com>
2015-06-19 13:09:19 +0200

    New front message

--- Capfile
+++ Capfile
@@ -4,7 +4,7 @@ require 'capistrano/setup'
 # Include default deployment tasks
 require 'capistrano/deploy'
 require 'capistrano/rails'
-require 'capistrano/rbenv'
+# require 'capistrano/rbenv'
 
 # Include tasks from other gems included in your Gemfile
 #
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -29,6 +29,7 @@ set :linked_dirs, fetch(:linked_dirs) + %w{certs}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
+server 'sonar-pre.glownet.com', user: 'ubuntu', roles: %w{web app db}
 server 'bbf.glownet.com', user: 'ubuntu', roles: %w{web app db}
 
 
