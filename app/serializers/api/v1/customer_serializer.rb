a9b0ae383dbf93270eb5dca25b8be68185713168
Alejandro González <alex@acidtango.com>
2015-06-24 18:38:48 +0200

    Customer api

--- app/serializers/api/v1/customer_serializer.rb
+++ app/serializers/api/v1/customer_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class CustomerSerializer < Api::V1::BaseSerializer
-      attributes :email, :name, :surname, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+      attributes :email, :name, :surname, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmed_email, :confirmed_at, :confirmation_sent_at, :assigned_gtag, :refund, :assigned_ticket, :credit_logs
 
       def current_sign_in_ip
         object.current_sign_in_ip.to_s
@@ -17,6 +17,18 @@ module Api
 
       def last_sign_in_at
         object.last_sign_in_at.in_time_zone.strftime("%y-%m-%d %H:%M:%S") if object.last_sign_in_at
+      end
+
+      def confirmed_email
+        !object.confirmed_at.nil?
+      end
+
+      def assigned_gtag
+        object.assigned_gtag_registration.gtag if object.assigned_gtag_registration
+      end
+
+      def assigned_ticket
+        object.assigned_admission.ticket if object.assigned_admission
       end
     end
   end
