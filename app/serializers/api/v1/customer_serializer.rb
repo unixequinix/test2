eb1c6754b4a84a73ff98644eb12878052181fcd0
Alejandro González <alex@acidtango.com>
2015-07-15 17:25:58 +0200

    Remove claims from customer serializer

--- app/serializers/api/v1/customer_serializer.rb
+++ app/serializers/api/v1/customer_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class CustomerSerializer < Api::V1::BaseSerializer
-      attributes :email, :name, :surname, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmed_email, :confirmed_at, :confirmation_sent_at, :assigned_gtag, :assigned_ticket, :total_credits, :ticket_credits, :purchased_credits, :refundable_credits, :refunded, :refund_status, :completed_claim, :claims
+      attributes :email, :name, :surname, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmed_email, :confirmed_at, :confirmation_sent_at, :assigned_gtag, :assigned_ticket, :total_credits, :ticket_credits, :purchased_credits, :refundable_credits, :refunded, :refund_status, :completed_claim
 
       def current_sign_in_ip
         object.current_sign_in_ip.to_s
