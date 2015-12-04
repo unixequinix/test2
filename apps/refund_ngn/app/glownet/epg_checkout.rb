1279dbacd70dbde02ca885c542845b583f87f4b7
Alejandro González <alex@acidtango.com>
2015-12-04 16:43:31 +0100

    Password validation

--- apps/refund_ngn/app/glownet/epg_checkout.rb
+++ apps/refund_ngn/app/glownet/epg_checkout.rb
@@ -25,16 +25,13 @@ class EpgCheckout
       'encrypted' => encryptedValue,
       'integrityCheck' => sha256ParamsIntegrityCheck
     }
-    binding.pry
     uri = URI.parse(@epg_values[:url])
     uri.query = URI.encode_www_form(parameters)
-    binding.pry
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(uri.request_uri)
     response = http.request(request).body
-    binding.pry
   end
 
   private
