6e14b47f1be4256c6e84f694fa370344d31be698
Alejandro González <alex@acidtango.com>
2015-12-04 15:23:33 +0100

    Refund admin panel fix

--- apps/administration/app/controllers/admins/events_controller.rb
+++ apps/administration/app/controllers/admins/events_controller.rb
@@ -63,7 +63,7 @@ class Admins::EventsController < Admins::BaseController
     params.require(:event).permit(:aasm_state, :name, :url, :location,
       :start_date, :end_date, :description, :support_email, :style, :logo,
       :background_type, :background, :features, :locales, :payment_service,
-      :refund_service, :gtag_registration, :info, :disclaimer, :host_country,
+      :refund_services, :gtag_registration, :info, :disclaimer, :host_country,
       :currency, :registration_parameters, :agreed_event_condition_message)
   end
 
