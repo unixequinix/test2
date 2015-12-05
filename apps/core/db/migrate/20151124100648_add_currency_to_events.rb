cd93e25b1df8569ad6009b755d3c306ad6cb4bc5
Alejandro González <alex@acidtango.com>
2015-12-05 12:48:50 +0100

    Encryption fix

--- apps/core/db/migrate/20151124100648_add_currency_to_events.rb
+++ apps/core/db/migrate/20151124100648_add_currency_to_events.rb
@@ -1,5 +1,5 @@
 class AddCurrencyToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :currency, :string, null: false
+    add_column :events, :currency, :string, null: false, default: 'USD'
   end
 end
