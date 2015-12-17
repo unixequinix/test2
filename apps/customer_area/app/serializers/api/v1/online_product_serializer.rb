1f9ec8dd9c72ff15193b51fa869dea1be6855034
Alejandro González <alex@acidtango.com>
2015-12-17 09:43:09 +0100

    Centralize customer area in the same engine

--- apps/customer_area/app/serializers/api/v1/online_product_serializer.rb
+++
@@ -1,7 +0,0 @@
-module Api
-  module V1
-    class OnlineProductSerializer < Api::V1::BaseSerializer
-      attributes :name, :description, :price
-    end
-  end
-end
--- apps/customer_area/app/serializers/api/v1/order_item_serializer.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class OrderItemSerializer < Api::V1::BaseSerializer
-      attributes :online_product, :amount, :total
-      has_one :online_product
-
-    end
-  end
-end
