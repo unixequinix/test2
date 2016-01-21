0c13acf0da24acfae6704d3eff2f8758381fdf7c
Alejandro González <alex@acidtango.com>
2016-01-21 14:49:45 +0100

    Payer forms

---
+++ apps/customer_area/app/glownet/payments/braintree_payer.rb
@@ -0,0 +1,11 @@
+class Payments::BraintreePayer
+
+  def initialize
+  end
+
+  def start(params)
+  end
+
+  def charge(params)
+  end
+end
---
+++ spec/customer_area/app/glownet/payments/braintree_payer_spec.rb
@@ -0,0 +1,4 @@
+require "rails_helper"
+
+RSpec.describe Payments::BraintreePayer, type: :domain_logic do
+end
