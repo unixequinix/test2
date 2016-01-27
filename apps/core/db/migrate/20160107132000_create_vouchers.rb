b49f62cd1b38d4b6649dd9711435f914c12e07f2
Quino <quino@acidtango.com>
2016-01-27 11:53:22 +0100

    Adds index to migrations with event

--- apps/core/db/migrate/20160107132000_create_vouchers.rb
+++ apps/core/db/migrate/20160107132000_create_vouchers.rb
@@ -2,7 +2,6 @@ class CreateVouchers < ActiveRecord::Migration
   def change
     create_table :vouchers do |t|
       t.integer :counter, null: false, default: 0
-
       t.datetime :deleted_at, index: true
       t.timestamps null: false
     end
