b28790e23c3719a61bd02cd190ea1b37393f3aab
rubenmoya <rmoyarodriguez@gmail.com>
2016-02-02 13:09:09 +0100

    Add tests

---
+++ spec/api/controllers/api/v1/events/banned_gtags_controller_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::Events::BannedGtagsController, :type => :controller do
+  describe "GET index" do
+    before do
+      @event = FactoryGirl.create :event
+      @gtag1 = FactoryGirl.create :gtag, event: @event
+      @gtag2 = FactoryGirl.create :gtag, event: @event
+      @gtag3 = FactoryGirl.create :gtag, event: @event
+      FactoryGirl.create :banned_gtag, gtag: @gtag1
+      FactoryGirl.create :banned_gtag, gtag: @gtag2
+      FactoryGirl.create :banned_gtag, gtag: @gtag3
+    end
+
+    context "with authentication" do
+      before(:each) do
+        http_login
+      end
+
+      it "has a 200 status code" do
+        get :index, :event_id => Event.last.id
+
+        expect(response.status).to eq 200
+      end
+
+      it "returns all the banned gtags" do
+        get :index, :event_id => Event.last.id
+
+        body = JSON.parse(response.body)
+        banned_tags = body.map { |m| m["tag_uid"] }
+
+        expect(banned_tags).to match_array(Gtag.all.map(&:tag_uid))
+      end
+    end
+    context "without authentication" do
+      it "has a 401 status code" do
+        get :index, :event_id => Event.last.id
+
+        expect(response.status).to eq 401
+      end
+    end
+  end
+end
