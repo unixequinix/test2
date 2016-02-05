03a55f786089294f1a27f0cf40d17e7069651f56
rubenmoya <rmoyarodriguez@gmail.com>
2016-02-05 16:45:45 +0100

    Finish preevent_product webservice

--- apps/api/app/serializers/api/v1/preevent_product_serializer.rb
+++ apps/api/app/serializers/api/v1/preevent_product_serializer.rb
@@ -1,15 +1,26 @@
 module Api
   module V1
     class PreeventProductSerializer < Api::V1::BaseSerializer
-      attributes :id, :name, :credentials, :credits
+      attributes :id, :name
 
-      # TODO: Add credentials and credits
+      def attributes(*args)
+        hash = super
+        hash[:credits] = credits if credits.present?
+        hash[:credentials] = credentials if credentials.present?
+        hash
+      end
+
       def credentials
-        [2, 5]
+        selected = object.preevent_product_items.select do |item|
+          item.preevent_item.purchasable_type == "CredentialType"
+        end
+        selected.map(&:id)
       end
 
       def credits
-        selected = object.preevent_product_items.select{ |item| item.preevent_item.purchasable_type == "Credit" }
+        selected = object.preevent_product_items.select do |item|
+          item.preevent_item.purchasable_type == "Credit"
+        end
         selected.map(&:amount).inject(:+)
       end
     end
