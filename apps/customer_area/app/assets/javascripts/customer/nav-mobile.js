1bbaa086caa299814ca75c2bfa5892d45475af1a
Manuel Cavero <manuel@acidtango.com>
2016-02-08 15:57:21 +0100

    Include styles for the mobile aside menu

--- apps/customer_area/app/assets/javascripts/customer/nav-mobile.js
+++ apps/customer_area/app/assets/javascripts/customer/nav-mobile.js
@@ -1,8 +1,20 @@
 function showNavMobile() {
-  var trigger = $(".mobile-trigger");
+  var trigger             = $("#mobile-trigger"),
+      triggerIcon         = $('.mobile-trigger i'),
+      target              = $('#mobile-options'),
+      unscrollableContent = $('.container-main'),
+      close               = $('.mobile-options');
 
   trigger.on("click", function(event){
-    $('#mobile-options').toggleClass('open-nav');
+    triggerIcon.toggleClass('fa-cog fa-times');
+    target.toggleClass('open-nav');
+    unscrollableContent.toggleClass('unscrollable-view');
+  });
+
+  close.on("click", function() {
+    target.removeClass('open-nav');
+    unscrollableContent.removeClass('unscrollable-view');
+    triggerIcon.toggleClass('fa-cog fa-times');
   });
 };
 
