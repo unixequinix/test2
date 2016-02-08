17e71124c89711d6d43ced0abd4d2abc34c49aab
Quino <quino@acidtango.com>
2016-02-08 13:28:42 +0100

    Adds CustomerOrder: Migration, Factory, Spec, Model

---
+++ apps/core/app/models/customer_order.rb
@@ -0,0 +1,42 @@
+# == Schema Information
+#
+# Table name: customer_orders
+#
+#  id                        :integer          not null, primary key
+#  event_id                  :integer          not null
+#  preevent_product_id       :integer          not null
+#  customer_event_profile_id :integer          not null
+#  counter                   :integer
+#  aasm_state                :string           default("unredeemed"), not null
+#  deleted_at                :datetime
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#
+
+class CustomerOrder < ActiveRecord::Base
+  acts_as_paranoid
+  acts_as_list column: :counter, scope: :event_id
+
+  # Associations
+  belongs_to :event
+  belongs_to :preevent_product
+  belongs_to :customer_event_profile
+
+  # Validations
+  validates :state, presence: true
+
+
+  include AASM
+  aasm do
+    state :unredeemed, initial: true
+    state :redeemed
+
+    event :redeem do
+      transitions from: :unredeemed, to: :redeemed
+    end
+
+    event :unredeem do
+      transitions from: :redeemed, to: :unredeemed
+    end
+  end
+end
---
+++ apps/core/db/migrate/20160208124402_create_customer_orders.rb
@@ -0,0 +1,14 @@
+class CreateCustomerOrders < ActiveRecord::Migration
+  def change
+    create_table :customer_orders do |t|
+      t.integer :event_id, index: true, null: false
+      t.integer :preevent_product_id, null: false
+      t.integer :customer_event_profile_id, null: false
+      t.integer :counter
+      t.string  :aasm_state, null: false, default: "unredeemed"
+
+      t.datetime :deleted_at, index: true
+      t.timestamps null: false
+    end
+  end
+end
