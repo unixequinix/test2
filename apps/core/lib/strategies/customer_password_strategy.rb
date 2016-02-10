5fa0ddec4bf597668a5c9223a97f8d0afbf31414
Jake <jake@glownet.com>
2016-02-10 16:05:12 +0100

    rubocop madness

--- apps/core/lib/strategies/customer_password_strategy.rb
+++ apps/core/lib/strategies/customer_password_strategy.rb
@@ -1,19 +1,23 @@
 class CustomerPasswordStrategy < ::Warden::Strategies::Base
   def valid?
-    customer_data = params.fetch("customer", {})
-    !(customer_data["email"].blank? || customer_data["password"].blank?)
+    !(c_attr("email").blank? || c_attr("password").blank?)
   end
 
   def authenticate!
-    customer = Customer.find_by(email: params["customer"].fetch("email"),
-                                event_id: params["customer"].fetch("event_id"))
-    password_not_ok = !Authentication::Encryptor.compare(customer.encrypted_password,
-                                                     params["customer"].fetch("password"))
+    customer = Customer.find_by(email: c_attr("email"), event_id: c_attr("event_id"))
 
-    fail!(message: "errors.messages.unauthorized") && return if customer.nil? || password_not_ok
+    not_ok = !Authentication::Encryptor.compare(customer.encrypted_password, c_attr("password"))
+
+    fail!(message: "errors.messages.unauthorized") && return if customer.nil? || not_ok
     fail!(message: "errors.messages.unconfirmed") && return if customer.confirmed_at.nil?
 
     customer.update_tracked_fields!(request)
     success!(customer)
+  end
+
+  private
+
+  def c_attr(attr_name)
+    params["customer"].fetch(attr_name)
   end
 end
