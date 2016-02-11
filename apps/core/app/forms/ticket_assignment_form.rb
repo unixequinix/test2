34773c2f79d23c3ac6f85b09585dd0dc28f84ba8
Quino <quino@acidtango.com>
2016-02-11 15:49:39 +0100

    Fixes gtag assignments messages

--- apps/core/app/forms/ticket_assignment_form.rb
+++ apps/core/app/forms/ticket_assignment_form.rb
@@ -9,26 +9,24 @@ class TicketAssignmentForm
   def save(ticket_fetcher, current_customer_event_profile, current_event)
     ticket = ticket_fetcher.find_by(code: code.strip)
     companies = CompanyTicketType.companies(current_event).join(", ")
-    binding.pry
     if ticket.nil?
       errors.add(:ticket_assignment, I18n.t("alerts.admissions", companies: companies)) && return
     end
 
-    binding.pry
     errors.add(:ticket_assignment, full_messages.join(". ")) && return unless valid?
     persist!(ticket, current_customer_event_profile)
   end
 
   private
 
-  def persist!(ticket, profile)
-    profile.save
-    profile.credential_assignments.create(credentiable: ticket)
-    binding.pry
+  def persist!(ticket, customer_event_profile)
+    customer_event_profile.save
+    customer_event_profile.credential_assignments.create(credentiable: ticket)
     CreditLog.create(
-      customer_event_profile: profile,
+      customer_event_profile: customer_event_profile,
       transaction_type: CreditLog::TICKET_ASSIGNMENT,
       amount: ticket.preevent_product_items_credits.sum(:amount)
     ) if ticket.preevent_product_items_credits.present?
+    return customer_event_profile
   end
 end
