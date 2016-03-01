9e6850a0605120216c55e6b2070f345c825fba8e
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-01 13:21:30 +0100

    Add fetcher to API

--- apps/api/app/controllers/api/v1/events/base_controller.rb
+++ apps/api/app/controllers/api/v1/events/base_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::Events::BaseController < Api::BaseController
   before_action :fetch_current_event
+  before_filter :enable_fetcher
+
+  private
+
+  def enable_fetcher
+    @fetcher = Multitenancy::ApiFetcher.new(current_event)
+  end
 end
---
+++ apps/api/app/glownet/multitenancy/api_fetcher.rb
@@ -0,0 +1,89 @@
+class Multitenancy::ApiFetcher
+  def initialize(event)
+    @event = event
+  end
+
+  def accesses
+    Access.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  def catalog_items
+    CatalogItem.where(event: @event)
+  end
+
+  def company_event_agreements
+    CompanyEventAgreement.where(event: @event)
+  end
+
+  def company_ticket_types
+    CompanyTicketType.where(event: @event)
+  end
+
+  def companies
+    Company.where(event: @event)
+  end
+
+  def credential_types
+    CredentialType.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  def credits
+    Credit.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  def customers
+    Customer.where(event: @event)
+  end
+
+  def customer_event_profiles
+    CustomerEventProfile.where(event: @event)
+  end
+
+  def event_parameters
+    EventParameter.where(event: @event)
+  end
+
+  def device_general_parameters
+    EventParameter.where(event: @event, parameters: { category: "device", group: "general" })
+      .includes(:parameter)
+  end
+
+  def gtags
+    Gtag.where(event: @event)
+  end
+
+  def packs
+    Pack.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  def stations
+    Station.where(event: @event)
+  end
+
+  def sale_stations
+    Station.joins(:station_type)
+      .where(event: @event, station_types: { name: Station::SALE_STATIONS })
+  end
+
+  def station_catalog_items
+    StationCatalogItem.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  def tickets
+    Ticket.where(event: @event)
+  end
+
+  def vouchers
+    Voucher.joins(:catalog_item).where(catalog_items: { event_id: @event.id })
+  end
+
+  private
+
+  def admin?
+    @user.is_admin?
+  end
+
+  def manager?
+    @user.is_manager?
+  end
+end
