19b45fe031fcdc678a3503a8298e0ad668aaf37f
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-02 18:54:56 +0100

    Fix API tests and factories related

---
+++ spec/api/controllers/api/v1/events/accesses_controler_spec.rb
@@ -0,0 +1,4 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::Events::AccessesController, type: :controller do
+end
---
+++ spec/api/controllers/api/v1/events/credential_types_controller_spec.rb
@@ -0,0 +1,4 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::Events::CredentialTypesController, type: :controller do
+end
