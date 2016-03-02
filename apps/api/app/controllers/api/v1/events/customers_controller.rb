7132c6f9362fa573933fcc33a72b226f4e47730e
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-02 13:52:42 +0100

    Add customer show webservice

--- apps/api/app/controllers/api/v1/events/customers_controller.rb
+++ apps/api/app/controllers/api/v1/events/customers_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Events::CustomersController < Api::V1::Events::BaseController
     render json: @fetcher.customer_event_profiles,
            each_serializer: Api::V1::CustomerEventProfileSerializer
   end
+
+  def show
+    render json: @fetcher.customer_event_profiles.find_by(id: params[:id]),
+           serializer: Api::V1::CustomerEventProfileSerializer
+  end
 end
