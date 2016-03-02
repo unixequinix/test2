6765fded87c494c1473c38705b10ee57c631c3fc
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-02 12:13:28 +0100

    Fix company ticket type serializer

--- apps/api/app/serializers/api/v1/company_ticket_type_serializer.rb
+++ apps/api/app/serializers/api/v1/company_ticket_type_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::CompanyTicketTypeSerializer < Api::V1::BaseSerializer
-  attributes :id, :name, :company_ticket_type_ref, :company_id, :company_name, :catalog_item_id
+  attributes :id, :name, :company_id, :catalog_item_id, :company_name, :company_ticket_type_ref
 
   def company_id
     object.company_event_agreement.company.id
@@ -11,5 +11,9 @@ class Api::V1::CompanyTicketTypeSerializer < Api::V1::BaseSerializer
 
   def catalog_item_id
     object.credential_type.catalog_item_id
+  end
+
+  def company_ticket_type_ref
+    object.company_code
   end
 end
