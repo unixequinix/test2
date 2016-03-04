73861f3f9145671b16c04994aabee8a590e96505
Manuel Cavero <manuel@acidtango.com>
2016-03-04 13:21:26 +0100

    Update

--- apps/customer_area/app/assets/javascripts/customer/placeholder-to-label.js
+++ apps/customer_area/app/assets/javascripts/customer/placeholder-to-label.js
@@ -1,23 +1,21 @@
-function placeholderToLabel() {
-  $( "input" ).each(function() {
-    if( this.value ) {
-      $(this).prev().addClass("to-be-label");
-    };
-  });
-
-  $("input").blur(function(){
-    $(this).prev().removeClass("to-be-label");
-    if( this.value ) {
-      $(this).prev().addClass("to-be-label");
-    };
-  });
-
-  $("input").focusin(function(){
-    $(this).prev().addClass("to-be-label");
-  });
-};
-
-$(document).on('page:load', placeholderToLabel);
-$(document).ready(placeholderToLabel);
-
-
+//function placeholderToLabel() {
+//  $( "input" ).each(function() {
+//    if( this.value ) {
+//      $(this).prev().addClass("to-be-label");
+//    };
+//  });
+//
+//  $("input").blur(function(){
+//    $(this).prev().removeClass("to-be-label");
+//    if( this.value ) {
+//      $(this).prev().addClass("to-be-label");
+//    };
+//  });
+//
+//  $("input").focusin(function(){
+//    $(this).prev().addClass("to-be-label");
+//  });
+//};
+//
+//$(document).on('page:load', placeholderToLabel);
+//$(document).ready(placeholderToLabel);
