53fd6e8ed3aa70a960efef6b3a0c60ec972601a0
Manuel Cavero <manuel@acidtango.com>
2016-03-04 10:11:09 +0100

    Add placeholder to label styles. Refactor ticket show and detail view and css. Fix the tickets list html

---
+++ apps/customer_area/app/assets/javascripts/customer/placeholder-to-label.js
@@ -0,0 +1,23 @@
+function placeholderToLabel() {
+  $( "input" ).each(function() {
+    if( this.value ) {
+      $(this).prev().addClass("to-be-label");
+    };
+  });
+
+  $("input").blur(function(){
+    $(this).prev().removeClass("to-be-label");
+    if( this.value ) {
+      $(this).prev().addClass("to-be-label");
+    };
+  });
+
+  $("input").focusin(function(){
+    $(this).prev().addClass("to-be-label");
+  });
+};
+
+$(document).on('page:load', placeholderToLabel);
+$(document).ready(placeholderToLabel);
+
+
