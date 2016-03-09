6ff8ee2586958ef56e3ea44be96678e355fb6a6c
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-09 13:42:24 +0100

    Test with serializers

--- apps/api/app/serializers/api/v1/ticket_serializer.rb
+++ apps/api/app/serializers/api/v1/ticket_serializer.rb
@@ -2,44 +2,44 @@ class Api::V1::TicketSerializer < Api::V1::BaseSerializer
   cache key: 'ticket', expires_in: 3.hours
   attributes :id, :reference, :credential_redeemed
 
-  def attributes(*args)
-    hash = super
+  # def attributes(*args)
+  #   hash = super
+  #
+  #   if customer_id
+  #     hash[:customer_id] = customer_id
+  #   else
+  #     hash[:purchaser_first_name] = purchaser_first_name if purchaser_first_name
+  #     hash[:purchaser_last_name] = purchaser_last_name if purchaser_last_name
+  #     hash[:purchaser_email] = purchaser_email if purchaser_email
+  #   end
+  #
+  #   hash[:credential_type_id] = credential_type_id if credential_type_id
+  #   hash
+  # end
 
-    if customer_id
-      hash[:customer_id] = customer_id
-    else
-      hash[:purchaser_first_name] = purchaser_first_name if purchaser_first_name
-      hash[:purchaser_last_name] = purchaser_last_name if purchaser_last_name
-      hash[:purchaser_email] = purchaser_email if purchaser_email
-    end
+  # def credential_type_id
+  #   ticket_type = object.company_ticket_type
+  #   ticket_type && ticket_type.credential_type_id
+  # end
 
-    hash[:credential_type_id] = credential_type_id if credential_type_id
-    hash
-  end
-
-  def credential_type_id
-    ticket_type = object.company_ticket_type
-    ticket_type && ticket_type.credential_type_id
-  end
-
-  def customer_id
-    cred = object.credential_assignments.first
-    cred && cred.customer_event_profile_id
-  end
+  # def customer_id
+  #   cred = object.credential_assignments.first
+  #   cred && cred.customer_event_profile_id
+  # end
 
   def reference
     object.code
   end
 
-  def purchaser_first_name
-    object.purchaser && object.purchaser.first_name
-  end
-
-  def purchaser_last_name
-    object.purchaser && object.purchaser.last_name
-  end
-
-  def purchaser_email
-    object.purchaser && object.purchaser.email
-  end
+  # def purchaser_first_name
+  #   object.purchaser && object.purchaser.first_name
+  # end
+  #
+  # def purchaser_last_name
+  #   object.purchaser && object.purchaser.last_name
+  # end
+  #
+  # def purchaser_email
+  #   object.purchaser && object.purchaser.email
+  # end
 end
