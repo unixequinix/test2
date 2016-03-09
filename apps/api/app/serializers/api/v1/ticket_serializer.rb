32d0b497f5c51637be1942ebe5aae9d75d0c91f9
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-09 17:22:21 +0100

    Fix serializer

--- apps/api/app/serializers/api/v1/ticket_serializer.rb
+++ apps/api/app/serializers/api/v1/ticket_serializer.rb
@@ -5,14 +5,14 @@ class Api::V1::TicketSerializer < Api::V1::BaseSerializer
   def attributes(*args)
     hash = super
 
-    if customer_id
+    if object.customer_id
       hash[:customer_id] = object.customer_id
     else
       hash[:purchaser_first_name] = object.purchaser_first_name if object.purchaser_first_name
       hash[:purchaser_last_name] = object.purchaser_last_name if object.purchaser_last_name
       hash[:purchaser_email] = object.purchaser_email if object.purchaser_email
     end
-    
+
     hash
   end
 end
