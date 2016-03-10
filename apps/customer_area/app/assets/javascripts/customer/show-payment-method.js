571b369a0c01b5816bb638a00a08f13ad74846c0
Manuel Cavero <manuel@acidtango.com>
2016-03-10 18:42:22 +0100

    Set focus on the first input when display the paymemnt method container like a boss

--- apps/customer_area/app/assets/javascripts/customer/show-payment-method.js
+++ apps/customer_area/app/assets/javascripts/customer/show-payment-method.js
@@ -3,9 +3,12 @@ function showPaymentMethod() {
       html = $("html"),
       body = $("body");
 
+  //$(".container-method").find('input').css("background-color", "red");
+
   trigger.on("click", function(event) {
     var target = this.dataset.target;
 
+    $(".container-method").find('input').focus();
     $("#method-" + target).toggleClass("show-container").siblings(".container-method").removeClass("show-container");
     $(this).toggleClass("button-selected").siblings().removeClass("button-selected");
   });
