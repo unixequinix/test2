2faca8ffad40456ef8d1705f8df2da8ba74ff061
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-10 15:56:28 +0100

    Remove api parameters

--- apps/api/app/glownet/multitenancy/api_fetcher.rb
+++ apps/api/app/glownet/multitenancy/api_fetcher.rb
@@ -1,8 +1,6 @@
 class Multitenancy::ApiFetcher
   def initialize(event)
     @event = event
-    @pagination_enabled = @event.get_parameter("device", "general", "enable_api_pagination")
-    @per_page = @event.get_parameter("device", "general", "api_records_per_page")
   end
 
   def accesses
@@ -80,7 +78,22 @@ class Multitenancy::ApiFetcher
                purchasers.last_name as purchaser_last_name,
                purchasers.email as purchaser_email")
       .where(event: @event)
-      render json: DB.select_value(sql)
+  end
+
+  def t_tickets
+    Ticket.joins(:credential_assignments, :company_ticket_type)
+      .joins("FULL OUTER JOIN purchasers
+              ON purchasers.credentiable_id = tickets.id
+              AND purchasers.credentiable_type = 'Ticket'
+              AND purchasers.deleted_at IS NULL")
+      .select("tickets.id, tickets.code as reference, tickets.credential_redeemed,
+               tickets.company_ticket_type_id, tickets.updated_at,
+               credential_assignments.customer_event_profile_id as customer_id,
+               company_ticket_types.credential_type_id as credential_type_id ,
+               purchasers.first_name as purchaser_first_name,
+               purchasers.last_name as purchaser_last_name,
+               purchasers.email as purchaser_email")
+      .where(event: @event)
   end
 
   def sql_tickets
