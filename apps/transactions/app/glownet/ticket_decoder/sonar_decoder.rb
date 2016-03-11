1e8988b8b804209135e85c55b71e8212c7decfd3
Jake <jcpenche@gmail.com>
2016-03-11 19:52:28 +0100

    finishes the sonar decoder

--- apps/transactions/app/glownet/ticket_decoder/sonar_decoder.rb
+++ apps/transactions/app/glownet/ticket_decoder/sonar_decoder.rb
@@ -1,19 +1,29 @@
 class TicketDecoder::SonarDecoder
-  def self.perform(encrypted_string)
-    encrypted_string.slice!(0)
-    encrypted_string = [reverse_hex(encrypted_string)].pack("H*")
+  def self.perform(ticket_code)
+    ticket_number = decode(ticket_code)
+    return nil unless verify_prefix(ticket_number)
+    ticket_number.to_s[-3..-1].to_i
+  end
+
+  def self.decode(ticket_code)
+    ticket_code.slice!(0)
+    ticket_code = [reverse_hex(ticket_code)].pack("H*")
     cipher = OpenSSL::Cipher.new("BF-CBC").decrypt
     key = Rails.application.secrets.ticket_config_1
     cipher.key = key
     cipher.iv = key.slice((key.length / 2)..key.length)
-    decrypted_string = cipher.update(encrypted_string) << cipher.final
 
-    code = reverse_hex(decrypted_string.unpack("H*").first).to_i(16)
-    verify_code(code)
+    begin
+      decrypted_string = cipher.update(ticket_code) << cipher.final
+    rescue OpenSSL::Cipher::CipherError
+      return nil
+    end
+
+    reverse_hex(decrypted_string.unpack("H*").first).to_i(16)
   end
 
-  def self.verify_code(code)
-    code
+  def self.verify_prefix(code)
+    code.to_s.starts_with? "2016"
   end
 
   def self.reverse_hex(data)
