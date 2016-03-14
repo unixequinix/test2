030bb77cd36e10d05d3ec09b6d10680ed70480be
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-14 13:27:55 +0100

    Refactors banned tickets

--- apps/company_api/app/controllers/companies/api/v1/banned_tickets_controller.rb
+++ apps/company_api/app/controllers/companies/api/v1/banned_tickets_controller.rb
@@ -12,18 +12,18 @@ class Companies::Api::V1::BannedTicketsController < Companies::Api::V1::BaseCont
 
   def create
     t_code = params[:tickets_blacklist][:ticket_reference]
-    t_type = TicketDecoder::SonarDecoder.perform(t_code)
+    ctt = TicketDecoder::SonarDecoder.perform(t_code)
 
-    @ticket = @fetcher.tickets.find_or_create_by!(code: t_code, company_ticket_type_id: t_type)
+    @ticket = @fetcher.tickets.find_by_code(t_code)
+    @ticket ||= @fetcher.tickets.create!(company_ticket_type_id: ctt, code: t_code)
     @ticket.ban!
-    
+
     render(status: :created, json: @ticket, serializer: Companies::Api::V1::BannedTicketSerializer)
   end
 
   def destroy
     @banned_ticket = BannedTicket.includes(:ticket)
-                     .find_by(tickets: { code: params[:id],
-                                         event_id: current_event.id })
+                     .find_by(tickets: { code: params[:id], event_id: current_event.id })
 
     render(status: :not_found, json: :not_found) && return if @banned_ticket.nil?
     render(status: :internal_server_error, json: :internal_server_error) &&
