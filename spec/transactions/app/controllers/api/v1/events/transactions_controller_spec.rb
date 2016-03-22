af18a4617b0402aac5bd99174a9d6525b8f18be1
Jake <jcpenche@gmail.com>
2016-03-22 16:06:56 +0100

    moved specs to root of app

--- spec/transactions/app/controllers/api/v1/events/transactions_controller_spec.rb
+++ spec/transactions/app/controllers/api/v1/events/transactions_controller_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "rails_helper"
 
 RSpec.describe Api::V1::Events::TransactionsController, type: :controller do
   include ControllerMacros
--- spec/transactions/app/glownet/jobs/base_spec.rb
+++ spec/transactions/app/glownet/jobs/base_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "rails_helper"
 
 RSpec.describe Jobs::Base, type: :job do
   let(:base) { Jobs::Base }
