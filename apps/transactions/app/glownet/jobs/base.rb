360ce36df2cc234890d3e52bec2fd6d6a4ca2e3e
Jake <jcpenche@gmail.com>
2016-03-28 20:35:17 +0200

    transactions should be filtered by event, devce id and device db index

--- apps/transactions/app/glownet/jobs/base.rb
+++ apps/transactions/app/glownet/jobs/base.rb
@@ -2,7 +2,7 @@ class Jobs::Base < ActiveJob::Base
   def self.write(atts)
     klass = "#{ atts[:transaction_category] }_transaction".classify.constantize
     obj_atts = extract_attributes(klass, atts)
-    obj = klass.find_by(atts.slice(:device_id, :device_db_index)) || klass.create(obj_atts)
+    obj = klass.find_by(atts.slice(:event_id, :device_uid, :device_db_index)) || klass.create(obj_atts)
     atts[:transaction_id] = obj.id
     workers = descendants.select { |worker| worker::SUBSCRIPTIONS.include? atts[:transaction_type] }
     workers.each { |worker|  worker.perform_later(atts) }
