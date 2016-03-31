edf2a182e68b6a6c91d354541b0b90e7ff4251ed
Jake <jcpenche@gmail.com>
2016-03-31 19:35:52 +0200

    assets cannot have custom domain name AND SSL

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "assets.glownet.com"
+  config.action_controller.asset_host = "d20gpgevp4t82e.cloudfront.net"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
