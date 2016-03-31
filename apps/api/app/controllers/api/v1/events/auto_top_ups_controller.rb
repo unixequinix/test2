da087f86918ef2a65ae17f240b1625bcf0d5ea21
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-31 09:29:09 +0200

    auto topup webservice

--- apps/api/app/controllers/api/v1/events/auto_top_ups_controller.rb
+++ apps/api/app/controllers/api/v1/events/auto_top_ups_controller.rb
@@ -2,30 +2,14 @@ class Api::V1::Events::AutoTopUpsController < Api::V1::Events::BaseController
   skip_before_action :verify_authenticity_token
 
   def create
-    render(status: :bad_request, json: { error: "Params missing." }) && return unless verify_params!
+    uid = params[:auto_top_up][:gtag_uid]
+    payment = params[:auto_top_up][:payment_method]
+    render(status: :bad_request, json: { error: "params missing" }) && return unless uid && payment
 
-    customer = Gtag.find_by_tag_uid(permitted_params[:gtag_uid])
-               .credential_assignments
-               .first
-               .customer_event_profile
+    profile = Gtag.find_by_tag_uid(uid).assigned_customer_event_profile
+    payer = "Autotopup::#{payment.capitalize}AutoPayer".constantize.pay(profile)
 
-    method = "Autotopup::#{permitted_params[:payment_method].capitalize}AutoPayer".constantize
-    payment = method.new(customer)
-    payment.start
-
-    binding.pry
-    errors = payment.errors
-    render(status: :unprocessable_entity, json: { errors: errors }) && return if errors
-    render(status: :created, json: { customer_id: customer.id })
-  end
-
-  private
-
-  def verify_params!
-    permitted_params.key?("gtag_uid") && permitted_params.key?("payment_method")
-  end
-
-  def permitted_params
-    params.require(:auto_top_up).permit(:gtag_uid, :payment_method)
+    render(status: :unprocessable_entity, json: :no_content) && return unless payer
+    render(status: :created, json: :created)
   end
 end
--- spec/api/app/controllers/api/v1/events/auto_top_ups_controller_spec.rb
+++ spec/api/app/controllers/api/v1/events/auto_top_ups_controller_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe Api::V1::Events::AutoTopUpsController, type: :controller do
   let(:cep) { create(:customer_event_profile, event: event) }
   let(:ca) { create(:credential_assignment_g_a, customer_event_profile: cep) }
   let(:tag_uid) { ca.credentiable.tag_uid }
-  let(:payer) { Autotopup::PaypalAutoPayer.new(cep) }
-  let(:params) { { gtag_uid: tag_uid, payment_method: %w(paypal).sample } }
+  let(:params) { { gtag_uid: tag_uid, payment_method: "paypal" } }
   let(:invalid_params) { { uid: "error", method: "error" } }
 
   describe "POST create" do
@@ -19,34 +18,24 @@ RSpec.describe Api::V1::Events::AutoTopUpsController, type: :controller do
       context "when the params exists" do
         context "when the request is valid" do
           context "when the agreement is signed" do
-            before(:each) do
-              expect(payer).to receive(:start).and_return(nil)
-              post :create, event_id: event.id, auto_top_up: params
-            end
-
             it "returns a 201 status code" do
+              allow(Autotopup::PaypalAutoPayer).to receive(:pay).and_return(true)
+              post :create, event_id: event.id, auto_top_up: params
               expect(response.status).to eq(201)
-            end
-
-            it "returns the customer_id and the amout" do
-              body = JSON.parse(response.body)
-              expect(body).to include("customer_id", "amount")
             end
           end
 
           context "when the agreement is not signed" do
-            before(:each) do
-              post :create, event_id: event.id, auto_top_up: params
-            end
-
             it "returns a 422 status code" do
+              post :create, event_id: event.id, auto_top_up: params
               expect(response.status).to eq(422)
             end
           end
         end
 
-        context "when the request is invalid" do
+        context "when the request fails" do
           it "returns a 422 status code" do
+            post :create, event_id: event.id, auto_top_up: params
             expect(response.status).to eq(422)
           end
         end
