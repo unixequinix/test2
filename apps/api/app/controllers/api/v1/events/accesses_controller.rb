7fa1c124924bb4bcc1e47c230dcbfff36e0384d5
rubenmoya <rmoyarodriguez@gmail.com>
2016-03-31 17:14:04 +0200

    Adds cache to accesses webservice

--- apps/api/app/controllers/api/v1/events/accesses_controller.rb
+++ apps/api/app/controllers/api/v1/events/accesses_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::Events::AccessesController < Api::V1::Events::BaseController
   def index
-    render json: @fetcher.accesses, each_serializer: Api::V1::AccessSerializer
+    if request.headers["If-Modified-Since"]
+      date = request.headers["If-Modified-Since"].to_time + 1
+      accesses = @fetcher.accesses.where("accesses.updated_at > ?", date)
+    else
+      accesses = @fetcher.accesses
+    end
+
+    response.headers["Last-Modified"] = accesses.maximum(:updated_at).to_s
+    render(json: accesses, each_serializer: Api::V1::AccessSerializer)
   end
 end
