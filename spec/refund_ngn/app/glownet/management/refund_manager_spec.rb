b0bf65a316b88fadc3b9917019da7b27602823f7
Jake <jcpenche@gmail.com>
2016-04-05 17:32:07 +0200

    fixes refund manager tests

--- spec/refund_ngn/app/glownet/management/refund_manager_spec.rb
+++ spec/refund_ngn/app/glownet/management/refund_manager_spec.rb
@@ -14,28 +14,28 @@ RSpec.describe Management::RefundManager, type: :domain_logic do
     end
 
     it "should include the payents required to start the refund and respective amounts" do
-      results = subject.get_online_payments
+      results = subject.online_payments
       expect(results.map(&:last).sum).to be(150.0)
     end
 
     it "should not include more payments than necessary" do
-      expect(subject.get_online_payments.size).to be(2)
+      expect(subject.online_payments.size).to be(2)
     end
 
     it "should include the least amount of payments possible" do
       payments[rand(payments.size)].update_attributes! amount: 200
-      expect(subject.get_online_payments.size).to be(1)
-      expect(subject.get_online_payments.last.last).to be(150.0)
+      expect(subject.online_payments.size).to be(1)
+      expect(subject.online_payments.last.last).to be(150.0)
     end
 
     it "should include the remainder amount in the last transaction" do
-      expect(subject.get_online_payments.last.last).to be(50.0)
+      expect(subject.online_payments.last.last).to be(50.0)
     end
 
     it "should only include successfull payments" do
       payments[rand(payments.size)].update_attributes! amount: 200, success: false
-      expect(subject.get_online_payments.size).to be(2)
-      expect(subject.get_online_payments.last.last).to be(50.0)
+      expect(subject.online_payments.size).to be(2)
+      expect(subject.online_payments.last.last).to be(50.0)
     end
   end
 end
