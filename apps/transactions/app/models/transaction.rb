884d3aacd03847b62bc5c2daa0567f8f7f40fac0
rubenmoya <rmoyarodriguez@gmail.com>
2016-04-07 19:54:33 +0200

    Removes event_id from mandatory and simbolizes keys

--- apps/transactions/app/models/transaction.rb
+++ apps/transactions/app/models/transaction.rb
@@ -8,7 +8,7 @@ class Transaction < ActiveRecord::Base
   validates_presence_of :transaction_type
 
   def self.mandatory_fields
-    %w( event_id transaction_origin transaction_category transaction_type customer_tag_uid
+    %w( transaction_origin transaction_category transaction_type customer_tag_uid
         operator_tag_uid station_id device_uid device_db_index device_created_at status_code
         status_message )
   end
