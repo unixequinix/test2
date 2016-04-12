0edc373ab6ff8fc3272c6f4dab80d279f58846ee
rubenmoya <rmoyarodriguez@gmail.com>
2016-04-12 22:01:41 +0200

    Refactors paypal autopayer

--- apps/payment_ngn/app/glownet/autotopup/paypal_auto_payer.rb
+++ apps/payment_ngn/app/glownet/autotopup/paypal_auto_payer.rb
@@ -1,20 +1,19 @@
 class Autotopup::PaypalAutoPayer
   def self.start(tag_uid, order_id) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     profile = Gtag.find_by_tag_uid(tag_uid)&.assigned_customer_event_profile
-    return { errors: "Customer not found" } if profile.nil?
+    return { errors: "Customer not found" } unless profile
     event = profile.event
     credit = event.credits.standard
 
     p_gateway = profile.payment_gateway_customers.find_by(gateway_type: "paypal")
-    return { errors: "No agreement accepted" } if p_gateway.nil?
+    return { errors: "No agreement accepted" } unless p_gateway
 
-    order = Order.new(customer_event_profile: profile, number: order_id)
+    order = Order.create(customer_event_profile: profile, number: order_id)
     amount = p_gateway.autotopup_amount
     value = credit.value
-    order.order_items << OrderItem.new(catalog_item_id: credit.catalog_item.id,
-                                       amount: amount,
-                                       total: amount * value)
-    order.save
+    total = amount * value
+    order.order_items.create(catalog_item: credit.catalog_item, amount: amount, total: total)
+
     Payments::BraintreeDataRetriever.new(event, order)
 
     data = { event_id: event.id, order_id: order.id, customer_id: p_gateway.token }
@@ -24,6 +23,6 @@ class Autotopup::PaypalAutoPayer
                                              "auto")
 
     return { errors: charge.errors.to_json } unless charge.success?
-    { gtag_uid: tag_uid, credit_amount: amount, money_amount: amount * value, credit_value: value }
+    { gtag_uid: tag_uid, credit_amount: amount, money_amount: total, credit_value: value }
   end
 end
