0d9721fc51b6e2544122e20d7a3187783b750af5
rubenmoya <rmoyarodriguez@gmail.com>
2016-04-12 21:09:03 +0200

    Adds missing tests

--- spec/core/app/models/station_group_spec.rb
+++ spec/core/app/models/station_group_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe StationGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:station_group) { build(:station_group) }
+
+  it "is expected to be valid" do
+    expect(station_group).to be_valid
+  end
 end
--- spec/core/app/models/station_type_spec.rb
+++ spec/core/app/models/station_type_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe StationType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:station_type) { build(:station_type) }
+
+  it "is expected to be valid" do
+    expect(station_type).to be_valid
+  end
 end
