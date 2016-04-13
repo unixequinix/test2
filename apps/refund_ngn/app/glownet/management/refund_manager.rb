ec9c1b7ff0322486da49d19c05314acbf861c68c
Jake <jake@glownet.com>
2016-04-13 02:13:02 +0200

    small refactor

--- apps/refund_ngn/app/glownet/management/refund_manager.rb
+++ apps/refund_ngn/app/glownet/management/refund_manager.rb
@@ -5,12 +5,13 @@ class Management::RefundManager
 
   def execute
     online_payments.map do |payment, amount|
-      "Payments::#{payment.payment_type.camelcase}Refunder".constantize.new(payment, amount).start
+      klass = "Payments::#{payment.payment_type.camelcase}Refunder".constantize
+      klass.new(payment, amount).start
     end
   end
 
   def online_payments
-    # array because we are going to delete and add and dont want to mess AR relations
+    # .to_a because we are going to delete and add and dont want to mess AR relations
     payments = @profile.payments.where(success: true).sort_by { |p| p.amount.to_f }.reverse.to_a
     reduce_payments(payments, @profile.refundable_money_amount)
   end
