5f9766c472e761fe3e438b67eda5af1c2a825830
Alejandro González <alex@acidtango.com>
2016-04-13 17:09:09 +0200

    Administration paypal_nvp parameters

---
+++ apps/payment_ngn/app/forms/paypal_nvp_payment_settings_form.rb
@@ -0,0 +1,20 @@
+class PaypalNvpPaymentSettingsForm < BaseSettingsForm
+  attribute :user, String
+  attribute :password, String
+  attribute :signature, String
+  attribute :merchant_id, String
+  attribute :autotopup, Boolean
+  attribute :event_id, Integer
+
+  validates_presence_of :user
+  validates_presence_of :password
+  validates_presence_of :signature
+  validates_presence_of :merchant_id
+  validates_presence_of :event_id
+
+  private
+
+  def persist!
+    persist_parameters(Parameter.where(category: "payment", group: "paypal_nvp"))
+  end
+end
