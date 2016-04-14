7474d48918130d7d868073a813b93c9e82680256
Quino <quino@acidtango.com>
2016-04-14 12:26:21 +0200

    Adds purchases sorter spec

--- spec/customer_area/app/glownet/sorters/purchases_sorter_spec.rb
+++ spec/customer_area/app/glownet/sorters/purchases_sorter_spec.rb
@@ -14,17 +14,50 @@ RSpec.describe Sorters::PurchasesSorter, type: :domain_logic do
       expect(sorted_purchases).to be_an_instance_of(Hash)
       expect(sorted_purchases.keys).to eq(%w(Voucher Access))
       expect(sorted_purchases.count).to eq(2)
+      expect(sorted_purchases["Access"].count).to eq(1)
+      expect(sorted_purchases["Voucher"].count).to eq(1)
     end
 
     it "with a pack, .sort should sort its inner items by category and stores it in a hash " do
       create(:customer_order, customer_event_profile: profile,
                               catalog_item: create(:pack_item_catalog_item))
-      binding.pry
       sorted_purchases = Sorters::PurchasesSorter.new(profile.purchases).sort(format: :hash)
-      binding.pry
       expect(sorted_purchases).to be_an_instance_of(Hash)
       expect(sorted_purchases.keys).to eq(%w(Voucher Access))
       expect(sorted_purchases.count).to eq(2)
+      expect(sorted_purchases["Access"].count).to eq(1)
+      expect(sorted_purchases["Voucher"].count).to eq(1)
     end
+
+    it ".sort should simple purchases and items inside a pack " do
+      create(:customer_order, customer_event_profile: profile,
+                              catalog_item: create(:pack_item_catalog_item))
+      create(:customer_order, customer_event_profile: profile,
+                              catalog_item: create(:voucher_catalog_item))
+      create(:customer_order, customer_event_profile: profile,
+                              catalog_item: create(:access_catalog_item))
+      sorted_purchases = Sorters::PurchasesSorter.new(profile.purchases).sort(format: :hash)
+      expect(sorted_purchases).to be_an_instance_of(Hash)
+      expect(sorted_purchases.keys).to eq(%w(Voucher Access))
+      expect(sorted_purchases.count).to eq(2)
+      expect(sorted_purchases["Access"].count).to eq(2)
+      expect(sorted_purchases["Voucher"].count).to eq(2)
+    end
+
+    it ".sort should simple purchases and items inside a pack " do
+      customer_order_a = create(:customer_order, customer_event_profile: profile,
+                              catalog_item: create(:voucher_catalog_item))
+      customer_order_b = create(:customer_order, customer_event_profile: profile,
+                              catalog_item: customer_order_a.catalog_item)
+      sorted_purchases = Sorters::PurchasesSorter.new(profile.purchases).sort(format: :hash)
+      expect(sorted_purchases).to be_an_instance_of(Hash)
+      expect(sorted_purchases.keys).to eq(%w(Voucher))
+      expect(sorted_purchases.count).to eq(1)
+      binding.pry
+      expect(sorted_purchases["Voucher"].count).to eq(1)
+      expect(sorted_purchases["Voucher"].first.total_amount).to eq(
+                                          customer_order_a.amount + customer_order_b.amount)
+    end
+
   end
 end
