7fbe256168b3c594355e94ea0e7a957900cfb403
Jake <jcpenche@gmail.com>
2016-04-18 18:31:34 +0200

    final refactor of Profile Checker to always return a credential assignment

--- apps/core/app/forms/gtag_assignment_form.rb
+++ apps/core/app/forms/gtag_assignment_form.rb
@@ -10,13 +10,14 @@ class GtagAssignmentForm
   validates_presence_of :tag_serial_number, unless: :simple?
 
   def save(fetcher, current_customer)
-    gtag = fetcher.find_by(tag_uid: tag_uid.strip.upcase,
-                           tag_serial_number: tag_serial_number.to_s.strip.upcase)
-    add_error("alerts.gtag.invalid") && return unless gtag
-    assignment = Profile::Checker.for_credentiable(gtag, current_customer)
-    add_error("alerts.gtag.already_assigned") && return unless assignment
+    number = tag_serial_number.to_s.strip.upcase
+    gtag = fetcher.find_by(tag_uid: tag_uid.strip.upcase, tag_serial_number: number)
 
-    GtagMailer.assigned_email(assignment).deliver_later
+    add_error("alerts.gtag.invalid") && return unless gtag
+    credentiable = Profile::Checker.for_credentiable(gtag, current_customer)
+    add_error("alerts.gtag.already_assigned") && return unless credentiable
+
+    GtagMailer.assigned_email(credentiable).deliver_later
   end
 
   private
