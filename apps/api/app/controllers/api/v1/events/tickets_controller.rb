9542ad169935a0d907a937dec1898c2c53688b40
rubenmoya <rmoyarodriguez@gmail.com>
2016-04-20 18:45:26 +0200

    Fixes

--- apps/api/app/controllers/api/v1/events/tickets_controller.rb
+++ apps/api/app/controllers/api/v1/events/tickets_controller.rb
@@ -34,6 +34,6 @@ class Api::V1::Events::TicketsController < Api::V1::Events::BaseController
     @ticket = current_event.tickets.find_by_code(params[:id])
 
     render(status: :not_found, json: :not_found) && return if @ticket.nil?
-    render(json: @ticket, serializer: Api::V1::TicketSerializer)
+    render(json: @ticket, serializer: Api::V1::TicketWithCustomerSerializer)
   end
 end
