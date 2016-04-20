f94ebd7f3c55af9da59f809e9a32de2012d81310
rubenmoya <rmoyarodriguez@gmail.com>
2016-04-20 13:53:48 +0200

    Adds customer to serializers

--- apps/api/app/serializers/api/v1/ticket_with_customer_serializer.rb
+++ apps/api/app/serializers/api/v1/ticket_with_customer_serializer.rb
@@ -1,8 +1,5 @@
 class Api::V1::TicketWithCustomerSerializer < Api::V1::BaseSerializer
-  attributes :id, :reference, :credential_redeemed, :credential_type_id
-  has_one :assigned_customer_event_profile,
-          key: :customer, 
-          serializer: Api::V1::CustomerEventProfileSerializer
+  attributes :id, :reference, :credential_redeemed, :credential_type_id, :customer
 
   def reference
     object.code
@@ -13,4 +10,8 @@ class Api::V1::TicketWithCustomerSerializer < Api::V1::BaseSerializer
     ticket_type && ticket_type.credential_type_id
   end
 
+  def customer
+    serializer = Api::V1::CustomerEventProfileSerializer.new(object.assigned_customer_event_profile)
+    ActiveModel::Serializer::Adapter::Json.new(serializer).as_json[:customer_event_profile]
+  end
 end
