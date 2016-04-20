67c423d3d4b3cde10c7a82123447285f37c9efd0
Jake <jcpenche@gmail.com>
2016-04-20 20:25:41 +0200

    master merge

--- apps/customer_area/app/validators/bsb_validator.rb
+++
@@ -1,8 +0,0 @@
-class BsbValidator < ActiveModel::Validator
-  def validate(record)
-    bsb = record.bsb.gsub(/[\s-]/, "").strip
-    unless bsb.length == 6
-      record.errors.add :bsb, record.errors.generate_message(:bsb, :invalid)
-    end
-  end
-end
