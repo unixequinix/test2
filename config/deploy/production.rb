e93ddf0be1df609cc5cde2d7866751dd1f1fdb14
Jake <jake@glownet.com>
2016-04-22 00:07:44 +0200

    log level of env, not deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :branch, "master"
 set :rails_env, "production"
 
 # Default value for :log_level is :debug
-set :log_level, :debug
+set :log_level, :info
 
 # Link certification folder
 set :linked_dirs, fetch(:linked_dirs) + %w(certs)
