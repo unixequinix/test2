67bacc9dfb4716016df7a336f6df2287fd5ea40d
Alejandro González <alex@acidtango.com>
2016-04-21 12:28:13 +0200

    Hotfix enviroment

---
+++ config/deploy/hotfix.rb
@@ -0,0 +1,55 @@
+set :branch, "master"
+set :rails_env, "hotfix"
+
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Link certification folder
+set :linked_dirs, fetch(:linked_dirs) + %w(certs)
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server "hotfix.glownet.com", user: "ubuntu", roles: %w(web app db)
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+
+set :default_run_options,   pty: true
+set :ssh_options, forward_agent: true, auth_methods: %w(publickey)
+
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
