9586f6fecd56170537d9f32f56242517a41f433a
Quino <quino@acidtango.com>
2016-04-29 19:35:40 +0200

    Adds payment icon names in locales

--- apps/customer_area/app/helpers/customer_area_helper.rb
+++ apps/customer_area/app/helpers/customer_area_helper.rb
@@ -3,8 +3,7 @@ module CustomerAreaHelper
     if %i(paypal redsys braintree stripe paypal_nvp).include? payment_service
       fa_icon t("orders.payment-buttons.#{payment_service}.icon")
     elsif %i(ideal sofort wirecard).include? payment_service
-      klass = t("orders.payment-buttons.#{payment_service}.icon")
-      content_tag(:i, "", class: "pf #{klass}")
+      content_tag(:i, "", class: t("orders.payment-buttons.#{payment_service}.icon"))
     end
   end
 end
