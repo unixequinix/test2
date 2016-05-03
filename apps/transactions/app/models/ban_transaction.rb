0c2277c16738c22a18ccc79b13e01f9a3b1e25ec
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-03 14:05:22 +0200

    Adds info to ban transaction

--- apps/transactions/app/models/ban_transaction.rb
+++ apps/transactions/app/models/ban_transaction.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: ban_transactions
+#
+#  id                 :integer          not null, primary key
+#  event_id           :integer
+#  transaction_origin :string
+#  transaction_type   :string
+#  station_id         :integer
+#  profile_id         :integer
+#  device_uid         :string
+#  device_db_index    :integer
+#  device_created_at  :string
+#  customer_tag_uid   :string
+#  operator_tag_uid   :string
+#  banneable_id       :integer          not null
+#  banneable_type     :string           not null
+#  reason             :text
+#  status_code        :integer
+#  status_message     :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class BanTransaction < Transaction
   belongs_to :banneable, polymorphic: true
 
