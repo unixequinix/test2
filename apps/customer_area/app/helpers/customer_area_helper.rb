46c9ad55681aa92cd138886d4d06c917e363bb10
Quino <quino@acidtango.com>
2016-05-03 10:49:12 +0200

    Changes wirecard icon to credit card icon

--- apps/customer_area/app/helpers/customer_area_helper.rb
+++ apps/customer_area/app/helpers/customer_area_helper.rb
@@ -1,8 +1,8 @@
 module CustomerAreaHelper
   def icon_to_draw(payment_service)
-    if %i(paypal redsys braintree stripe paypal_nvp).include? payment_service
+    if %i(paypal redsys braintree stripe paypal_nvp wirecard).include? payment_service
       fa_icon t("orders.payment-buttons.#{payment_service}.icon")
-    elsif %i(ideal sofort wirecard).include? payment_service
+    elsif %i(ideal sofort).include? payment_service
       content_tag(:i, "", class: t("orders.payment-buttons.#{payment_service}.icon"))
     end
   end
