4f63e15f9b96909d306c2ef083b81c62c4d6cbd6
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-06 13:48:28 +0200

    Changes to email's footer

--- apps/customer_area/app/mailer_previews/order_mailer_preview.rb
+++ apps/customer_area/app/mailer_previews/order_mailer_preview.rb
@@ -1,6 +1,6 @@
 class OrderMailerPreview < ActionMailer::Preview
   def completed_email
-    profile = Profile.first
+    profile = Profile.first || FactoryGirl.create(:profile, event: Event.first)
     order = FactoryGirl.create(:order, profile: profile)
     order.update!(completed_at: Time.now)
     order.order_items.create(amount: 10, total: 20, catalog_item: CatalogItem.first)
