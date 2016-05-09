453aa27ddda347a752fd49624ab9705cb587595d
Quino <quino@acidtango.com>
2016-05-09 12:31:24 +0200

    New branch

--- spec/customer_area/app/glownet/payments/sofort_data_retriever_spec.rb
+++ spec/customer_area/app/glownet/payments/sofort_data_retriever_spec.rb
@@ -1,24 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Payments::SofortDataRetriever, type: :domain_logic do
+  let(:order) do
+    create(:order)
+  end
+
+  let(:event) do
+    order.profile.event
+  end
+
+  let(:sofort_data_retriever) do
+    params = {}
+    params[:consumer_ip_address] = "192.168.1.1"
+    params[:consumer_user_agent] = "chrome"
+
+    Payments::SofortDataRetriever.new(event, order).with_params(params)
+  end
+
   context ".url_for_redirection" do
-    let(:order) do
-      create(:order)
-    end
-
-    let(:event) do
-      order.profile.event
-    end
-
-    let(:sofort_data_retriever) do
-      params = {}
-      params[:consumer_ip_address] = "192.168.1.1"
-      params[:consumer_user_agent] = "chrome"
-
-      Payments::SofortDataRetriever.new(event, order).with_params(params)
-    end
-
     it "should return an url needed for redirection when wirecard option is chosen" do
+      binding.pry
+      expect(:sofort_data_retriever).to receive(:url_for_redirection).and_return(14)
+      binding.pry
+
     end
   end
 end
