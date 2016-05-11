16540829314c8c4aca8e39e545406b8d61ce214f
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-11 12:20:25 +0200

    Fixes GA-1349
    - Allows ticket assignation when ticket doesn't have a credential type
    - Fixes customer credit creator
    - Adds TODOs to event states

--- apps/core/app/forms/ticket_assignment_form.rb
+++ apps/core/app/forms/ticket_assignment_form.rb
@@ -3,38 +3,41 @@ class TicketAssignmentForm
   include Virtus.model
 
   attribute :code, String
-
   validates_presence_of :code
 
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
   def save(ticket_fetcher, current_profile, current_event)
     ticket = ticket_fetcher.find_by(code: code.strip)
 
-    add_error("alerts.ticket_doesnt_exist") and return if ticket.blank?
-    companies = CompanyTicketType.companies(current_event).join(", ")
+    if ticket.blank?
+      companies = CompanyTicketType.companies(current_event).to_sentence
+      add_error("alerts.admissions", companies: companies) && return
+    end
 
-    return unless valid_ticket?(ticket, companies, current_profile)
+    if ticket.company_ticket_type.credential_type
+      items = open_pack(ticket)
+      infinites = items.all? { |item| item.catalogable.try(:entitlement)&.infinite? }
+      items_owned = current_profile.customer_orders.map(&:catalog_item)
+
+      add_error("alerts.credential_already_assigned") if infinites && (items - items_owned).empty?
+    end
+
+    add_error("alerts.ticket_already_assigned") if ticket.assigned_ticket_credential.present?
+    add_error(full_messages.to_sentence) unless valid?
+
+    return if errors.any?
 
     current_profile.save
     current_profile.credential_assignments.create(credentiable: ticket)
-    CustomerCreditTicketCreator.new.assign(ticket) if ticket.credits.present?
-    CustomerOrderTicketCreator.new.save(ticket)
+    if ticket.company_ticket_type.credential_type
+      CustomerCreditTicketCreator.new.assign(ticket) if ticket.credits.present?
+      CustomerOrderTicketCreator.new.save(ticket)
+    end
+
     current_profile
   end
 
   private
-
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def valid_ticket?(ticket, companies, current_profile)
-    items = open_pack(ticket)
-    infinites = items.all? { |item| item.catalogable.try(:entitlement)&.infinite? }
-    items_owned = current_profile.customer_orders.map(&:catalog_item)
-
-    add_error("alerts.admissions", companies: companies) if ticket.nil?
-    add_error("alerts.ticket_already_assigned") if ticket.assigned_ticket_credential.present?
-    add_error("alerts.credential_already_assigned") if infinites && (items - items_owned).empty?
-    add_error(full_messages.to_sentence) unless valid?
-    return true unless errors.any?
-  end
 
   def open_pack(ticket)
     c_item = ticket.credential_type_item
