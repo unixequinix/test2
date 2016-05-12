b3d1086f78ff27bd823ceb44989b80c96556b2c2
Quino <quino@acidtango.com>
2016-05-12 17:31:05 +0200

    Profile edition allows to change the autotopup amount

--- apps/customer_area/app/controllers/events/registrations_controller.rb
+++ apps/customer_area/app/controllers/events/registrations_controller.rb
@@ -24,6 +24,7 @@ class Events::RegistrationsController < Events::BaseController
   end
 
   def update
+    current_customer.profile.payment_gateway_customers.find_by_gateway_type("paypal_nvp").update(autotopup_amount: params[:autotopup_amount])
     @edit_profile_form = EditProfileForm.new(current_customer)
     if @edit_profile_form.validate(permitted_params) && @edit_profile_form.save
       redirect_to after_update_path
