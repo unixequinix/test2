9e9135584c3ce38d6a97511d0be7a704d9f58ed3
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-18 17:42:55 +0200

    Adds mailer for agreement acceptance

---
+++ apps/customer_area/app/mailers/agreement_mailer.rb
@@ -0,0 +1,18 @@
+class AgreementMailer < ApplicationMailer
+  def accepted_email(customer)
+    @event = customer.event
+    subject = I18n.t("email.customer.agreement.accepted.subject")
+
+    config_parameters(customer, @event)
+    mail(to: customer.email, reply_to: @event.support_email, subject: subject)
+  end
+
+  private
+
+  def config_parameters(customer, event)
+    headers["X-No-Spam"] = "True"
+    headers["In-Reply-To"] = event.support_email
+    @name = [customer.first_name, customer.last_name].join(" ")
+    @event = event
+  end
+end
