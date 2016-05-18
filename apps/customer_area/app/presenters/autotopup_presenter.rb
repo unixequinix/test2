b15be7b8de3e4068eaa94e495b748f8467a8593a
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-18 16:37:26 +0200

    Adds another validation to autotopup_presenter
    Now we check if the autotopup method has active the boolean of autotopup

--- apps/customer_area/app/presenters/autotopup_presenter.rb
+++ apps/customer_area/app/presenters/autotopup_presenter.rb
@@ -1,7 +1,7 @@
 class AutotopupPresenter < BasePresenter
   def can_render?
     @event.gtag_assignation? && @profile.active_credentials? && @event.agreement_acceptance? &&
-      @event.selected_autotopup_payment_services.present?
+      @event.autotopup_payment_services.present?
   end
 
   def path
