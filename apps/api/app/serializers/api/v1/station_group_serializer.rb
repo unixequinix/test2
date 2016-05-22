d3e0a6bd697b7326ca59ad7a92f1eb4053b6cf84
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-22 12:19:07 +0200

    Fixes station order

--- apps/api/app/serializers/api/v1/station_group_serializer.rb
+++ apps/api/app/serializers/api/v1/station_group_serializer.rb
@@ -1,8 +1,15 @@
 class Api::V1::StationGroupSerializer < Api::V1::BaseSerializer
-  attributes :station_group, :icon_slug
-  has_many :stations, order: 'created_at DESC', serializer: Api::V1::StationSerializer
+  attributes :station_group, :icon_slug, :stations
 
   def station_group
     object.name
+  end
+
+  def stations
+    object.stations
+          .where(event: current_event)
+          .where.not(station_types: { name: "customer_portal" })
+          .order(created_at: :desc)
+          .map { |s| Api::V1::StationSerializer.new(s) }
   end
 end
