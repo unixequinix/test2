57dd66e7d46ec6ad5d780366e259018eaed55b7f
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-22 11:24:47 +0200

    Stations order

--- apps/api/app/serializers/api/v1/station_group_serializer.rb
+++ apps/api/app/serializers/api/v1/station_group_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::StationGroupSerializer < Api::V1::BaseSerializer
   attributes :station_group, :icon_slug
-  has_many :stations, serializer: Api::V1::StationSerializer
+  has_many :stations, order: 'created_at DESC', serializer: Api::V1::StationSerializer
 
   def station_group
     object.name
