bba37531ca2970aef8e2f5f62b143374befed6da
rubenmoya <rmoyarodriguez@gmail.com>
2016-05-26 13:38:49 +0200

    [WIP] PDF Generation

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -6,3 +6,4 @@ Mime::Type.register "image/svg+xml", :svg
 Mime::Type.register "application/vnd.ms-fontobject", :eot
 Mime::Type.register "application/x-woff", :woff
 Mime::Type.register "application/octet-stream", :ttf
+Mime::Type.register "application/pdf", :pdf
