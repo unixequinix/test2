63870217d9166282271b99fe572f1034a50fe370
Quino <quino@acidtango.com>
2016-06-01 18:05:33 +0200

    Start

--- apps/customer_area/app/glownet/refund_service.rb
+++ apps/customer_area/app/glownet/refund_service.rb
@@ -10,7 +10,18 @@ class RefundService
     status_ok = %w( SUCCESS PENDING ).include? refund.status
     return false unless status_ok
     @claim.complete!
-    @profile.update_balance_after_refund(refund)
+    update_balance_after_refund(refund)
     ClaimMailer.completed_email(@claim, @profile.event).deliver_later
   end
+
+  def update_balance_after_refund(refund)
+    neg = (refund.amount * -1)
+    params = {
+      amount: neg, refundable_amount: neg, credit_value: @claim.event.standard_credit_price,
+      payment_method: refund.payment_solution, transaction_origin: "refund",
+      created_in_origin_at: Time.zone.now
+    }
+    customer_credits.create!(params)
+  end
+
 end
