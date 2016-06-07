d1bfc934c685d28ce38c103ce08685a0f63c48f5
Jake <jcpenche@gmail.com>
2016-06-07 17:34:35 +0200

    refactoring tests

--- apps/core/app/glownet/entitlement/position_creator.rb
+++ apps/core/app/glownet/entitlement/position_creator.rb
@@ -1,6 +1,5 @@
 class Entitlement::PositionCreator < Entitlement::PositionManager
   def create_new_position
-    return 1 if last_element.blank?
-    last_element.memory_position + last_element.memory_length
+    last_element.blank? ? 1 : last_element.memory_position + last_element.memory_length
   end
 end
--- spec/core/app/glownet/entitlement/position_creator_spec.rb
+++ spec/core/app/glownet/entitlement/position_creator_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe Entitlement::PositionCreator, type: :domain_logic do
   let(:position_creator) do
     Entitlement::PositionCreator.new(create(:entitlement, :with_access))
   end
+
   describe ".create_new_position" do
-    context "last position is nil" do
-      it "should return 1 (as a first position)" do
-        allow(position_creator).to receive(:last_element).and_return(nil)
-        expect(position_creator.create_new_position).to be(1)
-      end
+    it "when last position is nil returns 1 (as a first position)" do
+      allow(position_creator).to receive(:last_element).and_return(nil)
+      expect(position_creator.create_new_position).to be(1)
     end
-    context "last position is nil" do
-      it "should return 1 (as a first position)" do
-        allow(position_creator).to receive(:last_element).and_return( { memory_position: 1, memory_length: 2 } )
-        expect(position_creator.create_new_position).to be(1)
-      end
+
+    it "last position not nil returns the sum of position and length" do
+      obj = instance_double("Entitlement", memory_position: 11, memory_length: 5)
+      allow(position_creator).to receive(:last_element).and_return( obj )
+      expect(position_creator.create_new_position).to be(16)
     end
   end
 end
