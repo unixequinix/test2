4ca85675d0c61c319ca9b743022963de25dd456f
Jake <jake@glownet.com>
2016-06-17 23:19:45 +0200

    adds missing transactions bassed on device transactions

--- apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
+++ apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
@@ -1,9 +1,7 @@
 class Admins::Events::MissingTransactionsController < Admins::Events::BaseController
   def index
-    categories = %w(Credit Money Credential Access Order)
-    @missing = {device: {}, profiles: []}
-
-   
-
+    @devices = current_event.device_transactions.group_by(&:device_uid).select do |_, ts|
+      ts.last.transaction_type != "pack_device"
+    end
   end
 end
