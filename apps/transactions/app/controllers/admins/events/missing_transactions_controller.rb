bc10d5d0a4999cad67d82230f1037fafed047294
Jake <jake@glownet.com>
2016-06-17 23:54:19 +0200

    orders devices transactions of missing

--- apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
+++ apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
@@ -1,6 +1,6 @@
 class Admins::Events::MissingTransactionsController < Admins::Events::BaseController
   def index
-    @devices = current_event.device_transactions.group_by(&:device_uid).select do |_, ts|
+    @devices = current_event.device_transactions.order(device_created_at: :asc).group_by(&:device_uid).select do |_, ts|
       ts.last.transaction_type != "pack_device"
     end
   end
