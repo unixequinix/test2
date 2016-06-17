14c9ead67cbdf8a68284e78077d8a4b59d4ecef9
Jake <jake@glownet.com>
2016-06-18 00:18:53 +0200

    addiitions to missing_transactiosn

--- apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
+++ apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
@@ -3,5 +3,8 @@ class Admins::Events::MissingTransactionsController < Admins::Events::BaseContro
     @devices = current_event.device_transactions.order(device_created_at: :asc).group_by(&:device_uid).select do |_, ts|
       ts.last.transaction_type != "pack_device"
     end
+
+    @assets = {}
+    Device.where(mac: @devices.map{|uid, _| uid }).each { |d| @assets[d.mac] = d.asset_tracker }
   end
 end
