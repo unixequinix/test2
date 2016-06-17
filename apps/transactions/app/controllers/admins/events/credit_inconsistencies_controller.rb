b55595a4fe1a457f27f14d863226db845995f555
Jake <jake@glownet.com>
2016-06-17 15:03:14 +0200

    removes credit inconsistencies and missing in order to accelerate

--- apps/transactions/app/controllers/admins/events/credit_inconsistencies_controller.rb
+++ apps/transactions/app/controllers/admins/events/credit_inconsistencies_controller.rb
@@ -3,47 +3,6 @@ class Admins::Events::CreditInconsistenciesController < Admins::Events::BaseCont
     @issues = []
 
 
-    Profile.find_by_sql("SELECT * FROM profiles AS profiles")
 
-
-
-
-    current_event.profiles.joins(:credit_transactions,
-                                    :access_transactions,
-                                    :credential_transactions,
-                                    :money_transactions,
-                                    :order_transactions,
-                                    :customer_credits)
-                           .select('profiles.id', 'credit_transactions.gtag_counter', 'order_transactions.gtag_counter', 'money_transactions.gtag_counter', 'credential_transactions.gtag_counter', 'access_transactions.gtag_counter')
-                           .page(params[:page]).per(100)
-                           .each do |profile|
-      next if profile.missing_transactions?
-      credits = profile.customer_credits
-      last_credit = credits.first
-      amount_sum_credit = credits.map(&:amount).sum.round(2)
-      refundable_sum_credit = credits.map(&:refundable_amount).sum.round(2)
-
-      next unless last_credit
-      next if last_credit.final_balance == amount_sum_credit &&
-              last_credit.final_refundable_balance == refundable_sum_credit &&
-              last_credit.final_balance >= 0 &&
-              last_credit.final_refundable_balance >= 0
-
-      ib = (last_credit.final_balance - amount_sum_credit).round(2)
-      irb = (last_credit.final_refundable_balance - refundable_sum_credit).round(2)
-
-      @issues << {
-        id: profile.id,
-        profile: profile,
-        amount_sum_credit: amount_sum_credit,
-        refundable_sum_credit: refundable_sum_credit,
-        final_balance: last_credit.final_balance,
-        final_refundable_balance: last_credit.final_refundable_balance,
-        inconsistent_balance: ib,
-        inconsistent_refundable_balance: irb
-      }
-    end
-    @issues = @issues.sort_by! { |i| i[:inconsistent_balance] }
-    @issues = Kaminari.paginate_array(@issues, total_count: @issues.size).page(params[:page]).per(100)
   end
 end
--- apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
+++ apps/transactions/app/controllers/admins/events/missing_transactions_controller.rb
@@ -3,25 +3,7 @@ class Admins::Events::MissingTransactionsController < Admins::Events::BaseContro
     categories = %w(Credit Money Credential Access Order)
     @missing = {device: {}, profiles: []}
 
-    categories.each do |cat|
-      trans = "#{cat}Transaction".constantize.where(event: current_event).group_by(&:device_uid)
-      trans.each do |uid, transactions|
-        indexes = transactions.map(&:device_db_index).map(&:to_i).sort
-        all_indexes = (1..indexes.last.to_i).to_a
-        subset = all_indexes - indexes
-        next if subset.empty?
-        @missing[:device][uid] = {} unless @missing[uid]
-        @missing[:device][uid][cat.downcase] = subset
-      end
-    end
-
-    @missing[:profiles] = current_event.profiles
-                                       .includes(:credit_transactions,
-                                                :customer,
-                                                :active_tickets_assignment,
-                                                :active_gtag_assignment,
-                                                credential_assignments: :credentiable)
-                                       .select(&:missing_transactions?)
+   
 
   end
 end
