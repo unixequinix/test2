14a5716a06f85126d946dd55e2c34ef1be793531
Jake <jcpenche@gmail.com>
2016-06-22 09:59:52 +0200

    typo to previous

--- apps/transactions/app/glownet/operations/credit_updater.rb
+++ apps/transactions/app/glownet/operations/credit_updater.rb
@@ -1,4 +1,4 @@
-class Credit < ActiveJob::Base
+class CreditUpdater < ActiveJob::Base
   def perform(credit_id, counter, date)
     credit = CustomerCredit.find(credit_id)
     credit.update! gtag_counter: counter, created_in_origin_at: date
