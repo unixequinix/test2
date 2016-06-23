2f4b0de7946dea002d965443b571b64a4a1c3f40
rubenmoya <rmoyarodriguez@gmail.com>
2016-06-23 18:07:01 +0200

    Refactors credit inconsistencies

--- apps/transactions/app/controllers/admins/events/credit_inconsistencies_controller.rb
+++ apps/transactions/app/controllers/admins/events/credit_inconsistencies_controller.rb
@@ -1,32 +1,5 @@
 class Admins::Events::CreditInconsistenciesController < Admins::Events::BaseController
-  def index # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    @issues = []
-
-    current_event.profiles.includes(:customer_credits).each do |profile|
-      credits = profile.customer_credits
-      last_credit = credits.first
-      amount_sum_credit = credits.map(&:amount).sum.round(2)
-      refundable_sum_credit = credits.map(&:refundable_amount).sum.round(2)
-
-      next unless last_credit
-      next if last_credit.final_balance == amount_sum_credit &&
-        last_credit.final_refundable_balance == refundable_sum_credit &&
-        last_credit.final_balance >= 0 &&
-        last_credit.final_refundable_balance >= 0
-
-      ib = (last_credit.final_balance - amount_sum_credit).round(2)
-      irb = (last_credit.final_refundable_balance - refundable_sum_credit).round(2)
-
-      @issues << {
-        profile: profile,
-        amount_sum_credit: amount_sum_credit,
-        refundable_sum_credit: refundable_sum_credit,
-        final_balance: last_credit.final_balance,
-        final_refundable_balance: last_credit.final_refundable_balance,
-        inconsistent_balance: ib,
-        inconsistent_refundable_balance: irb
-      }
-    end
-    @issues = @issues.sort_by { |i| i[:inconsistent_balance] }
+  def index
+    @issues = Profile.customer_credits_sum(current_event)
   end
 end
