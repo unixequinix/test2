fa59fabe579d89944cb5a5cfb93ca630a0db7ffb
rubenmoya <rmoyarodriguez@gmail.com>
2016-06-23 14:33:56 +0200

    Adds devices views

--- apps/transactions/app/models/transaction.rb
+++ apps/transactions/app/models/transaction.rb
@@ -9,6 +9,10 @@ class Transaction < ActiveRecord::Base
 
   TYPES = %w(access ban credential credit money order device).freeze
 
+  def self.class_for_type(type)
+    "#{type}_transaction".classify.constantize
+  end
+
   def self.mandatory_fields
     %w( transaction_origin transaction_category transaction_type customer_tag_uid
         operator_tag_uid station_id device_uid device_db_index device_created_at status_code
