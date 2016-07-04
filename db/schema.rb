77bc7a1e62e26e342a835d389232f508c6a9a688
Jake <jcpenche@gmail.com>
2016-07-04 17:26:54 +0200

    moves migration to take effect after customer credit removal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630104754) do
+ActiveRecord::Schema.define(version: 20160704172500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -290,25 +290,6 @@ ActiveRecord::Schema.define(version: 20160630104754) do
 
   add_index "credits", ["deleted_at"], name: "index_credits_on_deleted_at", using: :btree
 
-  create_table "customer_credits", force: :cascade do |t|
-    t.integer  "profile_id",                                                     null: false
-    t.string   "transaction_origin",                                             null: false
-    t.string   "payment_method",                                                 null: false
-    t.decimal  "amount",                   precision: 8, scale: 2, default: 0.0, null: false
-    t.decimal  "refundable_amount",        precision: 8, scale: 2, default: 0.0, null: false
-    t.decimal  "final_balance",            precision: 8, scale: 2, default: 0.0, null: false
-    t.decimal  "final_refundable_balance", precision: 8, scale: 2, default: 0.0, null: false
-    t.decimal  "credit_value",             precision: 8, scale: 2, default: 1.0, null: false
-    t.datetime "deleted_at"
-    t.datetime "created_at",                                                     null: false
-    t.datetime "updated_at",                                                     null: false
-    t.datetime "created_in_origin_at"
-    t.integer  "gtag_counter"
-    t.integer  "online_counter",                                   default: 0
-  end
-
-  add_index "customer_credits", ["deleted_at"], name: "index_customer_credits_on_deleted_at", using: :btree
-
   create_table "customer_orders", force: :cascade do |t|
     t.integer  "profile_id",      null: false
     t.integer  "catalog_item_id", null: false
@@ -533,49 +514,6 @@ ActiveRecord::Schema.define(version: 20160630104754) do
   add_index "money_transactions", ["event_id"], name: "index_money_transactions_on_event_id", using: :btree
   add_index "money_transactions", ["profile_id"], name: "index_money_transactions_on_profile_id", using: :btree
   add_index "money_transactions", ["station_id"], name: "index_money_transactions_on_station_id", using: :btree
-
-  create_table "oauth_access_grants", force: :cascade do |t|
-    t.integer  "resource_owner_id", null: false
-    t.integer  "application_id",    null: false
-    t.string   "token",             null: false
-    t.integer  "expires_in",        null: false
-    t.text     "redirect_uri",      null: false
-    t.datetime "created_at",        null: false
-    t.datetime "revoked_at"
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
-
-  create_table "oauth_access_tokens", force: :cascade do |t|
-    t.integer  "resource_owner_id"
-    t.integer  "application_id"
-    t.string   "token",             null: false
-    t.string   "refresh_token"
-    t.integer  "expires_in"
-    t.datetime "revoked_at"
-    t.datetime "created_at",        null: false
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
-  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id", using: :btree
-  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
-
-  create_table "oauth_applications", force: :cascade do |t|
-    t.string   "name",                      null: false
-    t.string   "uid",                       null: false
-    t.string   "secret",                    null: false
-    t.text     "redirect_uri",              null: false
-    t.string   "scopes",       default: "", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-  end
-
-  add_index "oauth_applications", ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type", using: :btree
-  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "online_orders", force: :cascade do |t|
     t.integer  "customer_order_id", null: false
