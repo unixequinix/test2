bc2e8299badf7eda1ec75ebc088615adc5f34476
rubenmoya <rmoyarodriguez@gmail.com>
2016-07-07 18:04:57 +0200

    Changes how devices ws works

--- apps/api/app/controllers/api/v1/devices_controller.rb
+++ apps/api/app/controllers/api/v1/devices_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DevicesController < Api::BaseController
-  def update
-    device = Device.find_by_mac(params[:id])
-    render(json: :updated) && return if device.update(asset_tracker: params[:asset_tracker])
+  def create
+    device = Device.find_by(mac: params["mac"], imei: params["imei"], serial_number: params["serial_number"])
+    render(json: :updated) && return if device.update(asset_tracker: params["asset_id"])
     render(status: :unprocessable_entity, json: device.errors.full_messages)
   end
 
