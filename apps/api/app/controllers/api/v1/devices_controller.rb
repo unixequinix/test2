b7e4bd1bb5a895759d9d83b81bbc7e9b48d3cb3a
rubenmoya <rmoyarodriguez@gmail.com>
2016-07-08 13:09:58 +0200

    Adds device creation in devices webservice

--- apps/api/app/controllers/api/v1/devices_controller.rb
+++ apps/api/app/controllers/api/v1/devices_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::DevicesController < Api::BaseController
   def create
-    device = Device.find_by(mac: params["mac"], imei: params["imei"], serial_number: params["serial_number"])
+    device = Device.find_or_create_by(mac: params["mac"],
+                                      imei: params["imei"],
+                                      serial_number: params["serial_number"])
     render(json: :updated) && return if device.update(asset_tracker: params["asset_id"])
     render(status: :unprocessable_entity, json: device.errors.full_messages)
   end
