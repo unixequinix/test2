6664816bc5600c4f1f9b2b7239f7c8557c3170b5
Alejandro González <alex@acidtango.com>
2016-07-26 18:50:44 +0200

    Improvements in the asset tracker view

--- apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
+++ apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
@@ -9,7 +9,6 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
       hash = { device_uid: d_tr.device_uid }
       hash[:asset_tracker] = asset_trackers[d_tr.device_uid]&.first&.asset_tracker
       transaction = transactions.find { |t| t["device_uid"] == d_tr.device_uid }
-      hash[:device_db_index] = d_tr.device_db_index
       hash[:transactions_count] = transaction["transactions_count"]
       hash[:device_counter] = transaction["device_counter"]
       hash[:transaction_type] = transaction["transaction_type"].humanize
