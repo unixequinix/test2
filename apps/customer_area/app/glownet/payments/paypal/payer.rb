daa86ba880bcb2bda2a792bd75f6a6981894f876
rubenmoya <rmoyarodriguez@gmail.com>
2016-07-28 11:33:55 +0200

    Rename customer_credit_creator to credit_writer

--- apps/customer_area/app/glownet/payments/paypal/payer.rb
+++ apps/customer_area/app/glownet/payments/paypal/payer.rb
@@ -1,19 +1,19 @@
 class Payments::Paypal::Payer < Payments::Paypal::BasePayer
-  def start(customer_order_creator, customer_credit_creator)
+  def start(customer_order_creator, credit_writer)
     @order.start_payment!
     charge_object = charge
     return charge_object unless charge_object.success?
     create_agreement(charge_object, @params[:autotopup_amount]) if create_agreement?
-    notify_payment(charge_object, customer_order_creator, customer_credit_creator)
+    notify_payment(charge_object, customer_order_creator, credit_writer)
     charge_object
   end
 
   private
 
-  def notify_payment(charge, customer_order_creator, customer_credit_creator)
+  def notify_payment(charge, customer_order_creator, credit_writer)
     return unless charge.transaction.status == "settling"
     create_payment(@order, charge)
-    customer_credit_creator.save_order(@order, "auto_topup")
+    credit_writer.save_order(@order, "auto_topup")
     customer_order_creator.save(@order, "paypal", "paypal")
     @order.complete!
     send_mail_for(@order, @event)
