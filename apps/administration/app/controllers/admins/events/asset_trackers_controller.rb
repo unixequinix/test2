86ac76ef05c94f0311e526f30e49e906270ed154
Alejandro González <alex@acidtango.com>
2016-07-30 19:12:46 +0200

    Improvements in asset tracker

--- apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
+++ apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
@@ -3,21 +3,19 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
     devices = current_event.device_transactions.select(:device_uid).uniq
     asset_trackers = Device.all.select(:mac, :asset_tracker).group_by(&:mac)
     transactions = Device.transactions_count(current_event)
-
-    @colors = { wrong_transactions: "#E53A40", not_packed: "#fc913a", packed: "#56A902" }
     @devices = { wrong_transactions: [], not_packed: [], packed: [] }
 
     devices.each do |d_tr|
-      device_info = { device_uid: d_tr.device_uid }
-      device_info[:asset_tracker] = asset_trackers[d_tr.device_uid]&.first&.asset_tracker
+      hash = { device_uid: d_tr.device_uid }
+      hash[:asset_tracker] = asset_trackers[d_tr.device_uid]&.first&.asset_tracker
       transaction = transactions.find { |t| t["device_uid"] == d_tr.device_uid }
-      device_info[:transactions_count] = transaction["transactions_count"]
-      device_info[:device_counter] = transaction["device_counter"]
-      device_info[:transaction_type] = transaction["transaction_type"].humanize
-      @devices[:not_packed] <<
-        hash && next if transaction["transaction_type"] != "pack_device"
-      @devices[:wrong_transactions] <<
-        hash && next if wrong?(device_info[:device_counter], device_info[:transactions_count])
+      hash[:transactions_count] = transaction["transactions_count"]
+      hash[:device_counter] = transaction["device_counter"]
+      hash[:transaction_type] = transaction["transaction_type"].humanize
+
+      @colors = { wrong_transactions: "#E53A40", not_packed: "#fc913a", packed: "#56A902" }
+      @devices[:not_packed] << hash && next if transaction["transaction_type"] != "pack_device"
+      @devices[:wrong_transactions] << hash && next if wrong?(hash[:device_counter] != hash[:transactions_count])
       @devices[:packed] << hash
     end
   end
