970089cc66a4be64333809032f470826fe50cdf5
Alejandro González <alex@acidtango.com>
2016-07-30 13:38:10 +0200

    Improvements in asset tracker

--- apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
+++ apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
@@ -3,6 +3,13 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
     devices = current_event.device_transactions.select(:device_uid).uniq
     asset_trackers = Device.all.select(:mac, :asset_tracker).group_by(&:mac)
     transactions = Device.transactions_count(current_event)
+    classify_devices(devices, asset_trackers, transactions)
+  end
+
+  private
+
+  def classify_devices(devices, asset_trackers, transactions)
+    @colors = { wrong_transactions: "#E53A40", not_packed: "#fc913a", packed: "#56A902" }
     @devices = { wrong_transactions: [], not_packed: [], packed: [] }
 
     devices.each do |d_tr|
@@ -12,11 +19,15 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
       hash[:transactions_count] = transaction["transactions_count"]
       hash[:device_counter] = transaction["device_counter"]
       hash[:transaction_type] = transaction["transaction_type"].humanize
-
-      @colors = { wrong_transactions: "#E53A40", not_packed: "#fc913a", packed: "#56A902" }
       @devices[:not_packed] << hash && next if transaction["transaction_type"] != "pack_device"
-      @devices[:wrong_transactions] << hash && next if hash[:device_counter] != hash[:transactions_count]
+      @devices[:wrong_transactions] <<
+        hash && next if wrong?(hash[:device_counter], hash[:transactions_count])
       @devices[:packed] << hash
     end
+  end
+
+  def wrong?(device_counter, transaction_count)
+    diff = hash[:device_counter] - hash[:transactions_count]
+    diff > 5 || diff < -5
   end
 end
