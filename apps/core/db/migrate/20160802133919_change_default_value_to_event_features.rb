36d169334637cf78d13f47cd429d8c23546bc35e
rubenmoya <rmoyarodriguez@gmail.com>
2016-08-02 12:04:51 +0200

    Fixes

--- apps/core/db/migrate/20160802133919_change_default_value_to_event_features.rb
+++ apps/core/db/migrate/20160802133919_change_default_value_to_event_features.rb
@@ -1,5 +1,10 @@
 class ChangeDefaultValueToEventFeatures < ActiveRecord::Migration
   def change
+    Event.all.each do |event|
+      features = event.features + 384
+      event.update!(features: features)
+    end
+
     change_column_default :events, :features, 416
   end
 end
