e9a3c07d385ea39edc3ed7c3925528c4b217bbf7
rubenmoya <rmoyarodriguez@gmail.com>
2016-08-03 13:18:01 +0200

    Fixes GA-1992

--- apps/customer_area/app/assets/javascripts/customer/placeholder-to-label.js
+++ apps/customer_area/app/assets/javascripts/customer/placeholder-to-label.js
@@ -2,16 +2,17 @@ function placeholderToLabel() {
   var target = $("input");
 
   target.each(function() {
-    if( this.value ) {
+    if(this.value || $(this).is(":focus")) {
       $(this).prev().addClass("to-be-label");
     }
+
     $(this).on("focus", function(event) {
       $(this).prev().addClass("to-be-label");
     });
 
     $(this).on("blur", function(event){
       $(this).prev().removeClass("to-be-label");
-      if( this.value ) {
+      if(this.value) {
         $(this).prev().addClass("to-be-label");
       }
     });
