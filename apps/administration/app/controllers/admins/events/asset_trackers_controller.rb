0df51bcaea9c3331c8018b00ae0d7fec3d5b5f85
rubenmoya <rmoyarodriguez@gmail.com>
2016-09-01 20:26:51 +0200

    fix: (admin_panel) asset_tracker

--- apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
+++ apps/administration/app/controllers/admins/events/asset_trackers_controller.rb
@@ -1,9 +1,6 @@
 class Admins::Events::AssetTrackersController < Admins::Events::BaseController
   def index
-    devices = current_event.device_transactions
-                           .status_ok
-                           .order(device_created_at: :asc)
-                           .group_by(&:device_uid)
+    devices = current_event.device_transactions.status_ok.order(device_created_at: :asc).group_by(&:device_uid)
     @assets = {}
     Device.where(mac: devices.map { |mac, _| mac }).each { |d| @assets[d.mac] = { asset_tracker: d.asset_tracker } }
     transactions_sql = Device.transactions_count(current_event)
@@ -12,11 +9,12 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
       init = transactions.select { |t| t.transaction_type == "device_initialization" }
       pack = transactions.select { |t| t.transaction_type == "pack_device" }
       server_trans = transactions_sql.find { |t| t["device_uid"] == device }
-      device_trans = (pack.map(&:number_of_transactions).sum - init.map(&:number_of_transactions).sum) + init.count
 
       if init.count - pack.count == 1
         status = "in_use"
+        device_trans = (pack.map(&:number_of_transactions).sum - init[0..-2].map(&:number_of_transactions).sum) + init.count
       elsif init.count == pack.count
+        device_trans = (pack.map(&:number_of_transactions).sum - init.map(&:number_of_transactions).sum) + init.count
         if device_trans == server_trans["transactions_count"]
           status = "packed"
         else
@@ -28,7 +26,7 @@ class Admins::Events::AssetTrackersController < Admins::Events::BaseController
       @assets[device].merge!(device_transactions: device_trans,
                              server_transactions: server_trans["transactions_count"],
                              transaction_type: server_trans["transaction_type"],
-                             last_pack_device: pack.last && pack.last[:number_of_transactions],
+                             last_device_transaction: transactions.last[:number_of_transactions],
                              status: status)
     end
     @assets = @assets.group_by { |a| a.last[:status] }
