630ba54196738e96883f05ac143e93f57bd8f9b4
rubenmoya <rmoyarodriguez@gmail.com>
2016-09-22 19:45:50 +0200

    WIP

--- apps/refund_ngn/app/controllers/admins/events/refund_settings_controller.rb
+++ apps/refund_ngn/app/controllers/admins/events/refund_settings_controller.rb
@@ -13,6 +13,7 @@ class Admins::Events::RefundSettingsController < Admins::Events::BaseController
         category: "event"
       }
     ).includes(:parameter)
+    @direct_claim_profiles = direct_claim_profiles.count
   end
 
   def edit
@@ -75,7 +76,45 @@ class Admins::Events::RefundSettingsController < Admins::Events::BaseController
     end
   end
 
+  def paypal_refund
+    profiles = direct_claim_profiles.map do |profile|
+      claim = Claim.new(service_type: Claim::DIRECT,
+                        fee: current_event.refund_fee(Claim::DIRECT),
+                        minimum: current_event.refund_minimun(Claim::DIRECT),
+                        profile: profile,
+                        gtag: profile.active_gtag_assignment.credentiable,
+                        total: profile.refundable_money,
+                        aasm_state: "in_progress")
+      claim.generate_claim_number!
+      claim.save!
+      claim.start_claim!
+
+      if Management::RefundManager.new(profile, profile.refundable_money).execute
+        RefundService.new(claim).create(amount: profile.refundable_money_after_fee(Claim::DIRECT),
+                                        currency: current_event.currency,
+                                        message: "Created direct refund",
+                                        payment_solution: Claim::DIRECT,
+                                        status: "SUCCESS")
+      end
+    end
+
+    if profiles.all?
+      flash[:notice] = I18n.t("alerts.updated")
+    else
+      flash[:alert] = I18n.t("alerts.error")
+    end
+    redirect_to admins_event_refund_settings_url(current_event)
+  end
+
   private
+
+  def direct_claim_profiles
+    claim_ids = Claim.completed.where(profile_id: current_event.profiles.pluck(:id)).pluck(:profile_id)
+    current_event.profiles.where("id NOT IN (?)", claim_ids)
+                          .where("refundable_credits > 0")
+                          .select(&:valid_balance?)
+                          .select(&:enough_money?)
+  end
 
   def permitted_params
     params_names = Parameter.where(group: @refund_service, category: "refund").map(&:name)
