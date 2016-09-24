7bc2573216b46d906f5e83297fabaa6e0ffd8dc3
rubenmoya <rmoyarodriguez@gmail.com>
2016-09-24 22:52:09 +0200

    refactor: (transactions) recalculate_profile_balance moved to profile

--- apps/transactions/app/models/credit_transaction.rb
+++ apps/transactions/app/models/credit_transaction.rb
@@ -45,20 +45,7 @@ class CreditTransaction < Transaction
   after_update :recalculate_profile_balance
 
   def recalculate_profile_balance
-    transactions = profile.credit_transactions.status_ok.not_record_credit
-
-    if transactions.sum(:gtag_counter) != 0
-      fb = transactions.last.final_balance
-      frb = transactions.last.final_refundable_balance
-    else
-      fb = transactions.sum(:credits)
-      frb = transactions.sum(:refundable_credits)
-    end
-
-    profile.update(credits: transactions.sum(:credits),
-                   refundable_credits: transactions.sum(:refundable_credits),
-                   final_balance: fb,
-                   final_refundable_balance: frb)
+    profile.recalculate_balance
   end
 
   def description
--- spec/core/factories/profiles.rb
+++ spec/core/factories/profiles.rb
@@ -13,6 +13,11 @@
 FactoryGirl.define do
   factory :profile do
     event
+    banned false
+    credits 0
+    refundable_credits 0
+    final_balance 0
+    final_refundable_balance 0
 
     trait :with_customer do
       after(:build) do |profile|
