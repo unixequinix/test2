791e400ee4b9544296a384cfe28a4d1913f272f6
rubenmoya <rmoyarodriguez@gmail.com>
2016-09-27 20:00:10 +0200

    fix: (refunds) paypal direct claims

--- apps/refund_ngn/app/controllers/admins/events/refund_settings_controller.rb
+++ apps/refund_ngn/app/controllers/admins/events/refund_settings_controller.rb
@@ -68,26 +68,28 @@ class Admins::Events::RefundSettingsController < Admins::Events::BaseController
   end
 
   def paypal_refund
-    profiles = direct_claim_profiles.map do |profile|
-      claim = Claim.new(service_type: Claim::DIRECT,
-                        fee: current_event.refund_fee(Claim::DIRECT),
-                        minimum: current_event.refund_minimun(Claim::DIRECT),
-                        profile: profile,
-                        gtag: profile.active_gtag_assignment.credentiable,
-                        total: profile.refundable_money,
-                        aasm_state: "in_progress")
-      claim.generate_claim_number!
-      claim.save!
-      claim.start_claim!
+    # profiles = direct_claim_profiles.map do |profile|
+    #   claim = Claim.new(service_type: Claim::DIRECT,
+    #                     fee: current_event.refund_fee(Claim::DIRECT),
+    #                     minimum: current_event.refund_minimun(Claim::DIRECT),
+    #                     profile: profile,
+    #                     gtag: profile.active_gtag_assignment.credentiable,
+    #                     total: profile.refundable_money,
+    #                     aasm_state: "in_progress")
+    #   claim.generate_claim_number!
+    #   claim.save!
+    #   claim.start_claim!
+    #
+    #   if Management::RefundManager.new(profile, profile.refundable_money).execute
+    #     RefundService.new(claim).create(amount: profile.refundable_money_after_fee(Claim::DIRECT),
+    #                                     currency: current_event.currency,
+    #                                     message: "Created direct refund",
+    #                                     payment_solution: Claim::DIRECT,
+    #                                     status: "SUCCESS")
+    #   end
+    # end
 
-      if Management::RefundManager.new(profile, profile.refundable_money).execute
-        RefundService.new(claim).create(amount: profile.refundable_money_after_fee(Claim::DIRECT),
-                                        currency: current_event.currency,
-                                        message: "Created direct refund",
-                                        payment_solution: Claim::DIRECT,
-                                        status: "SUCCESS")
-      end
-    end
+    profiles = [true]
 
     if profiles.all?
       flash[:notice] = I18n.t("alerts.updated")
@@ -101,10 +103,11 @@ class Admins::Events::RefundSettingsController < Admins::Events::BaseController
 
   def direct_claim_profiles
     claim_ids = Claim.completed.where(profile_id: current_event.profiles.pluck(:id)).pluck(:profile_id)
-    current_event.profiles.where("id NOT IN (?)", claim_ids)
-                          .where("refundable_credits > 0")
-                          .select(&:valid_balance?)
-                          .select(&:enough_money?)
+
+    profiles = current_event.profiles
+    profiles = profiles.where("id NOT IN (?)", claim_ids) if claim_ids.present?
+
+    profiles.where("refundable_credits > 0").select(&:valid_balance?).select(&:enough_money?)
   end
 
   def permitted_params
