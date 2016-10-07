dd6e8608d5b47bf5fc2b36c0c0ccade356c7f6ba
Jake <jcpenche@gmail.com>
2016-10-07 13:49:57 +0200

    Revert "fix (deployment): adds application.yml to linked files"
    This reverts commit f2a47cd650feef9f1984767210a0b1493efc78a3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :deploy_to, "~/glownet_web"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w(config/database.yml config/application.yml config/newrelic.yml config/sidekiq.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml config/newrelic.yml config/sidekiq.yml)
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w(log store tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
