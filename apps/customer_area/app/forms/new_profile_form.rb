ca326aed5c939f6520efe9688ab12094faecc9b3
rubenmoya <rmoyarodriguez@gmail.com>
2016-10-07 13:38:37 +0200

    fix

--- apps/customer_area/app/forms/new_profile_form.rb
+++ apps/customer_area/app/forms/new_profile_form.rb
@@ -25,8 +25,15 @@ class NewProfileForm
   validates :event_id, :email, :first_name, :last_name, :password, presence: true
   validates_acceptance_of :agreed_on_registration, accept: true
   validates_acceptance_of :recaptcha, accept: true
-  validate :custom_inputs
   validate :email_uniqueness
+  validates :phone, presence: true, if: -> { event && event.phone? }
+  validates :birthdate, presence: true, if: -> { event && event.birthdate? }
+  validates :phone, presence: true, if: -> { event && event.phone? }
+  validates :postcode, presence: true, if: -> { event && event.postcode? }
+  validates :address, presence: true, if: -> { event && event.address? }
+  validates :city, presence: true, if: -> { event && event.city? }
+  validates :country, presence: true, if: -> { event && event.country? }
+  validates :gender, presence: true, if: -> { event && event.gender? }
 
   def save
     if valid?
@@ -45,20 +52,6 @@ class NewProfileForm
     errors[:email] << msg if Customer.exists?(email: email.downcase,
                                               event_id: event_id,
                                               deleted_at: nil)
-  end
-
-  def custom_inputs # rubocop:disable all
-    event = Event.find(event_id)
-    # TODO: Refactor
-    errors[:phone] << I18n.t("errors.messages.blank") if phone.blank? && event.phone?
-    errors[:address] << I18n.t("errors.messages.blank") if address.blank? && event.address?
-    errors[:city] << I18n.t("errors.messages.blank") if city.blank? && event.city?
-    errors[:country] << I18n.t("errors.messages.blank") if country.blank? && event.country?
-    errors[:postcode] << I18n.t("errors.messages.blank") if postcode.blank? && event.postcode?
-    errors[:gender] << I18n.t("errors.messages.blank") if gender.blank? && event.gender?
-    errors[:birthdate] << I18n.t("errors.messages.blank") if birthdate.blank? && event.birthdate?
-    errors[:agreed_event_condition] <<
-      I18n.t("errors.messages.accepted") if !agreed_event_condition && event.agreed_event_condition?
   end
 
   def persist!
