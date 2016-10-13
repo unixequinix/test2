d1fcdeaa6f78a3c10bcff7237572ee49476f81dc
rubenmoya <rmoyarodriguez@gmail.com>
2016-10-13 16:01:55 +0200

    fix (profile_checker): handle deleted profiles

--- apps/core/app/glownet/profile/checker.rb
+++ apps/core/app/glownet/profile/checker.rb
@@ -1,25 +1,26 @@
 class Profile::Checker
-  def self.for_transaction(gtag, tr_profile, event_id)
+  # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity
+  def self.for_transaction(gtag, tr_profile_id, event_id)
     tg_profile = gtag.assigned_profile&.id
+    tr_profile = Profile.find_by_id(tr_profile_id)
 
-    if tr_profile.present? && tg_profile.present? && tg_profile != tr_profile
-      profile = Profile.find(tr_profile)
-      message = "Profile fraud - Transaction: #{tr_profile.inspect}, Gtag: #{tg_profile.inspect}"
-      raise message if gtag.assigned_profile.customer
+    if tr_profile.present? && tg_profile.present? && tg_profile != tr_profile_id
+      message = "Profile fraud - Transaction: #{tr_profile_id.inspect}, Gtag: #{tg_profile.inspect}"
+      raise message if tr_profile.active_gtag_assignment.present?
 
       gtag.assigned_gtag_credential.unassign!
-      profile.credential_assignments.find_or_create_by!(credentiable: gtag, aasm_state: :assigned)
+      tr_profile.credential_assignments.find_or_create_by!(credentiable: gtag, aasm_state: :assigned)
 
       Transaction::TYPES.each do |type|
         klass = Transaction.class_for_type(type)
-        klass.where(profile_id: tg_profile).update_all(profile_id: tr_profile)
+        klass.where(profile_id: tg_profile).update_all(profile_id: tr_profile_id)
       end
 
-      profile.credit_transactions.last.try(:recalculate_profile_balance)
-      return tr_profile
+      tr_profile.transactions.credit.last.try(:recalculate_profile_balance)
+      return tr_profile_id
     end
 
-    return tr_profile if tr_profile
+    return tr_profile_id if tr_profile
     return tg_profile if tg_profile
 
     profile = Profile.create!(event_id: event_id)
@@ -30,7 +31,7 @@ class Profile::Checker
   # TODO: when o_profile and c_profile are present, merge them.
   def self.for_credentiable(obj, customer)
     o_profile = obj.assigned_profile
-    raise "Credentiable Fraud detected" if o_profile&.customer
+    return if o_profile&.customer
 
     c_profile = customer.profile
     profile = o_profile || c_profile || customer.create_profile!(event: obj.event)
