33a7a76fac922d56eb039885ce758534ad61296d
Rubén Moya <rmoyarodriguez@gmail.com>
2016-11-03 18:10:14 +0100

    Merge (#65)
    * added application.yml to gitignore
    
    * fix (eventbrite): added rescue clause for wrong event access in index
    
    * fix (deploy):added github as deploy repository
    
    * fix (eventbrite): call to eventbrite api mus be in begin rescue
    
    * fix (eventbrite): call to eventbrite api mus be in begin rescue
    
    * catching all exception
    
    * fix (customer_area): add asterisk to custom fields
    
    * refactor (simple_form): add browser validations
    
    * fix: view
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix (deployment): adds application.yml to linked files
    
    * fix refunds presenter
    
    * Revert "fix (deployment): adds application.yml to linked files"
    
    This reverts commit f2a47cd650feef9f1984767210a0b1493efc78a3.
    
    * fix presenters
    
    * fix view
    
    * fix
    
    * fixes (GA-2230): Load the improved Italian translation for email strings
    
    * fixes (presenters): gtag assignment button hidden during event
    
    * refactor (eventbrite): Use pagination to import all tickets
    
    * fix (deploy): remove lock version
    
    * refactor (eventbirte): views are more usable now in master too
    
    * fix (eventbrite): Amend to previous commit
    
    * fix (eventbrite): Amend to previous commit
    
    * fix (eventbrite): more explicit views
    
    * fix (eventbrite): capture exception when no errors are found
    
    * fix (profile_checker): handle deleted profiles
    
    * refactor (deploy): hotfix points to master
    
    * fix (eventbrite): Alex fixes a pagination starts form 0 problem in attendees
    
    * Add receive communications (#28)
    
    * feat (GA-2322): Add a second "Receive communications" checkbox
    
    * fix (GA-2324): change the error when ticket is not found in ticket assignation
    
    * fix (simple_form): remove browser validations
    
    * fix (customer): small bug fix on customer selected_data
    
    * refactor (email): italian translation adapted to movement
    
    * feature (api): send redeemed with online_orders
    
    * feature (api): amend to last commit
    
    * fix (asset_tracker): create empty hash if device is not in asset tracker
    
    * fix (stripe): handle errors and show them in the view
    
    * fix (presenters): wrong text in refund
    
    * fix (migrations): removes data change in stations
    
    * refactor (sessions): change session name
    
    * Release 0.1.1 (#57)
    
    * refactor (core): remove purchasers (#54)
    * fix (eventbrite): avoid crash when eventbrite description is null (#55)
    * fix (customer_area): receive communications two (#56)
    
    * fix (purchasers): remove (#62)
    
    * refactor (core): remove purchasers (#54)
    
    * fix (eventbrite): avoid crash when eventbrite description is null (#55)
    
    * fix (customer_area): receive communications two (#56)
    
    * fix (csv): queries for csv
    
    * fix (presenters): remove refund presenter since its not used (#59) (#61)
    
    * fix (gtag): prevents gtags validation form executing under wrong conditions
    
    * fix (gtag): applies previous activation parameters to newly created gtag activation
    
    * fix (eventbrite): importer now creates ticket and then updates, no uniqueness errors

--- db/migrate/20160601145823_renames_point_of_sale_station.rb
+++ db/migrate/20160601145823_renames_point_of_sale_station.rb
@@ -1,7 +1,5 @@
 class RenamesPointOfSaleStation < ActiveRecord::Migration
   def change
-    Station.where(category: "point_of_sales").each do |s|
-      s.update!(category: "vendor")
-    end
+
   end
 end
