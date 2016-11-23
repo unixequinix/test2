4f4b23cafc96f879df7ad1ef4b2b4d85795fc295
rubenmoya <rmoyarodriguez@gmail.com>
2016-11-23 14:25:52 +0100

    fix (eventbrite): ban tickets instead of deleting them

--- app/controllers/events/tickets_controller.rb
+++ app/controllers/events/tickets_controller.rb
@@ -3,6 +3,6 @@ class Events::TicketsController < Events::BaseController
     @ticket = current_event.tickets.find_by(id: params[:id], customer: current_customer)
 
     ticket_catalog_item = @ticket.ticket_type&.catalog_item
-    @products = ticket_catalog_item&.is_a?(Pack) ? ticket_catalog_item.catalog_items : [catalog_item]
+    @products = ticket_catalog_item&.is_a?(Pack) ? ticket_catalog_item.catalog_items : [ticket_catalog_item]
   end
 end
