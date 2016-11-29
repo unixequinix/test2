4df9aea1eacd43d2d6acbe054005a66e8096e80d
rubenmoya <rmoyarodriguez@gmail.com>
2016-11-29 16:44:11 +0100

    fix (api): add Last-Modified to event parameters

--- app/controllers/api/v1/events/parameters_controller.rb
+++ app/controllers/api/v1/events/parameters_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::Events::ParametersController < Api::V1::Events::BaseController
   def index
+    modified = request.headers["If-Modified-Since"]
+    render(status: 304, json: :none) && return if modified && current_event.updated_at.httpdate <= modified
+
     gtag_base = current_event.gtag_settings
     gtag_common = gtag_base.reject { |key| Gtag::DEFINITIONS.keys.include?(key.to_sym) }
 
     gtag_settings = gtag_common.merge(gtag_base[gtag_base["gtag_type"]]).map { |k, v| { name: k, value: v } }
     device_settings = current_event.device_settings.map { |k, v| { name: k, value: v } }
 
+    response.headers["Last-Modified"] = current_event.updated_at&.httpdate
     render json: device_settings + gtag_settings
   end
 end
