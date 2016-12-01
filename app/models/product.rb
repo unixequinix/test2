2635a46d046995d7ca76abd5f75b9b058bec25ab
Jake <jcpenche@gmail.com>
2016-12-01 19:57:21 +0100

    fix (destroy_events): fix to previous commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,6 +20,7 @@
 class Product < ActiveRecord::Base
   belongs_to :event
   has_many :station_products, dependent: :destroy
+  has_many :sale_items, dependent: :destroy
 
   validates :name, :event_id, presence: true
   validates :name, uniqueness: { scope: :event_id }
