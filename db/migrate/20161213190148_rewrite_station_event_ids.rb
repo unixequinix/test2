40e2bbb117edb9019da5e774c4531454586cc313
Jake <jcpenche@gmail.com>
2016-12-14 01:33:29 +0100

    fix (stations): fix method missing on event#stations

--- db/migrate/20161213190148_rewrite_station_event_ids.rb
+++ db/migrate/20161213190148_rewrite_station_event_ids.rb
@@ -1,7 +1,8 @@
 class RewriteStationEventIds < ActiveRecord::Migration
   def change
     Event.all.each do |e|
-      e.stations.each.with_index { |station, i| station.update station_event_id: i + 1 }
+      Station.where(event_id: e.id).each.with_index { |station, i| station.update station_event_id: i + 1 }
     end
   end
 end
+
