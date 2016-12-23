b442d74f569f08da5b45370d02f764528f97dfde
Jake <jcpenche@gmail.com>
2016-12-23 19:48:12 +0100

    fix (GA-2671): transactions pdf was showing no transactions

--- app/controllers/events/credits_histories_controller.rb
+++ app/controllers/events/credits_histories_controller.rb
@@ -2,12 +2,10 @@ class Events::CreditsHistoriesController < Events::BaseController
   layout false
 
   def download
-    @pdf_transactions = CreditTransaction.status_ok.not_record_credit
-                                         .where(event: @current_event, customer: current_customer)
-                                         .order("device_created_at asc")
+    gtag = current_customer.active_gtag
+    @pdf_transactions = gtag ? gtag.transactions.credit.status_ok.order(:gtag_counter) : []
     html = render_to_string(action: :history, layout: false)
     pdf = WickedPdf.new.pdf_from_string(html)
-    send_data(pdf, filename: "transaction_history_#{current_customer.email}.pdf",
-                   disposition: "attachment")
+    send_data(pdf, filename: "transaction_history_#{current_customer.email}.pdf", disposition: "attachment")
   end
 end
