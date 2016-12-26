d6faacc683c3ad570ac612cee7f50bd84b621640
Jake <jcpenche@gmail.com>
2016-12-26 18:07:06 +0100

    fix (base_controller): check current event exists before enabling api

--- app/controllers/api/v1/events/base_controller.rb
+++ app/controllers/api/v1/events/base_controller.rb
@@ -15,7 +15,8 @@ class Api::V1::Events::BaseController < Api::BaseController
   end
 
   def api_enabled
-    return unless @current_event.finished?
+    return unless current_event
+    return unless current_event.finished?
     render(status: :unauthorized, json: :unauthorized)
   end
 
