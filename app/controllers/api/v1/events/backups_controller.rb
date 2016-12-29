8cbca66ca747ce1718fc1be0aea945c67f34c0fc
Jake <jcpenche@gmail.com>
2016-12-29 16:56:53 +0100

    fix (db_backups): upload to s3 working

--- app/controllers/api/v1/events/backups_controller.rb
+++ app/controllers/api/v1/events/backups_controller.rb
@@ -3,10 +3,13 @@ class Api::V1::Events::BackupsController < Api::V1::Events::BaseController
     keys = [:device_uid, :backup_created_at, :backup].any? { |i| params[i] }
 
     secrets = Rails.application.secrets
-    s3 = AWS::S3.new(access_key_id: secrets.s3_access_key_id, secret_access_key: secrets.s3_secret_access_key)
+    credentials = Aws::Credentials.new(secrets.s3_access_key_id, secrets.s3_secret_access_key)
+    s3 = Aws::S3::Resource.new(region:'eu-west-1', credentials: credentials)
+
+
 
     time = Time.parse(params[:backup_created_at]).to_i
-    name = "gspot/event/#{params[:event_id]}/backups/#{params[:device_uid]}/#{params[:device_uid]}-#{time}"
+    name = "/gspot/event/#{params[:event_id]}/backups/#{params[:device_uid]}/#{params[:device_uid]}-#{time}"
     obj = s3.bucket(Rails.application.secrets.s3_bucket).object(name)
 
     obj.upload_file(params[:backup])
