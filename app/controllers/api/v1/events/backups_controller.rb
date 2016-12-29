87dc89073a98a24b48c772183b395fa1fa5f7ef6
Jake <jcpenche@gmail.com>
2016-12-29 17:26:14 +0100

    fix (backups): created a real file out of parameters

--- app/controllers/api/v1/events/backups_controller.rb
+++ app/controllers/api/v1/events/backups_controller.rb
@@ -6,13 +6,11 @@ class Api::V1::Events::BackupsController < Api::V1::Events::BaseController
     credentials = Aws::Credentials.new(secrets.s3_access_key_id, secrets.s3_secret_access_key)
     s3 = Aws::S3::Resource.new(region:'eu-west-1', credentials: credentials)
 
-
-
     time = Time.parse(params[:backup_created_at]).to_i
     name = "/gspot/event/#{params[:event_id]}/backups/#{params[:device_uid]}/#{params[:device_uid]}-#{time}"
     obj = s3.bucket(Rails.application.secrets.s3_bucket).object(name)
 
-    obj.upload_file(params[:backup])
+    obj.upload_file(File.read(params[:backup].tempfile))
 
     render(status: :bad_request, json: { error: "params missing" }) && return unless keys
     render(status: :created, json: :created)
