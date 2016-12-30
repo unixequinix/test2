0236b77b33764e6947a67bf04c74257ad6137bf8
Jake <jcpenche@gmail.com>
2016-12-30 16:23:18 +0100

    fix (balance_updater): undo last commit

--- app/jobs/transactions/credit/balance_updater.rb
+++ app/jobs/transactions/credit/balance_updater.rb
@@ -2,10 +2,6 @@ class Transactions::Credit::BalanceUpdater < Transactions::Base
   TRIGGERS = %w( sale topup refund fee record_credit sale_refund ).freeze
 
   def perform(atts)
-    gtag = Gtag.find_by(event_id: atts[:event_id], id: atts[:gtag_id])
-    gtag = Gtag.find_by(tag_uid: atts[:customer_tag_uid].to_s.scan(/.{1,2}/).reverse.join) unless gtag
-
-    gtag.recalculate_balance
-
+    Gtag.find_by(event_id: atts[:event_id], id: atts[:gtag_id]).recalculate_balance
   end
 end
