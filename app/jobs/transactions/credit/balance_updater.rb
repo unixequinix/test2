b5e81bb610b9e548d9618d32bbadaed7e1baf421
Jake <jcpenche@gmail.com>
2016-12-30 16:18:47 +0100

    fix (balance_updater): update balance for reverse_tags

--- app/jobs/transactions/credit/balance_updater.rb
+++ app/jobs/transactions/credit/balance_updater.rb
@@ -2,6 +2,10 @@ class Transactions::Credit::BalanceUpdater < Transactions::Base
   TRIGGERS = %w( sale topup refund fee record_credit sale_refund ).freeze
 
   def perform(atts)
-    Gtag.find(atts[:gtag_id]).recalculate_balance
+    gtag = Gtag.find_by(event_id: atts[:event_id], atts[:gtag_id])
+    gtag = Gtag.find_by(tag_uid: atts[:customer_tag_uid].to_s.scan(/.{1,2}/).reverse.join) unless gtag
+
+    gtag.recalculate_balance
+
   end
 end
