4ba0543ce3423958184f2b4b6af2bd9e247a54d6
Jake <jcpenche@gmail.com>
2017-01-03 22:04:37 +0100

    fix (gtag): adding uniqueness index

--- db/migrate/20170103202722_add_index_to_gtag_uid.rb
+++ db/migrate/20170103202722_add_index_to_gtag_uid.rb
@@ -1,7 +1,9 @@
 class AddIndexToGtagUid < ActiveRecord::Migration[5.0]
   def change
-    to_delete = Gtag.select(:event_id, :tag_uid, :activation_counter).group(:event_id, :tag_uid, :activation_counter).having("count(*) > 1")
-    puts to_delete.pluck(:event_id).inspect
+    tags = Gtag.select(:id, :event_id, :tag_uid, :activation_counter).group(:event_id, :tag_uid, :activation_counter).having("count(*) > 1")
+    SaleItem.wher(credit_transaction: Transaction.wher(gtag: tags)).destroy_all
+    Transaction.wher(gtag: tags)
+    tags.destroy_all
     add_index :gtags, [:event_id, :tag_uid, :activation_counter], unique: true
   end
 end
