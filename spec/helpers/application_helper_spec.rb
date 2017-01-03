7c7c16c5b01087aeb96ed62c0288d53980253262
Jake <jcpenche@gmail.com>
2017-01-03 14:53:16 +0100

    test (helper): tests .format_abstract_value

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,27 +1,43 @@
 require "spec_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
-  subject { ApplicationHelper }
-
   describe ".title" do
     before { @env = Rails.env }
 
     it "returns a different title depending on the enviroment" do
       allow(@env).to receive(:production?).and_return(false)
-      expect(title).to eq("[#{Rails.env.upcase}] Glownet")
+      expect(helper.title).to eq("[#{Rails.env.upcase}] Glownet")
     end
 
     it "returns only glownet if enviroment is production" do
       allow(@env).to receive(:production?).and_return(true)
-      expect(title).to eq("Glownet")
+      expect(helper.title).to eq("Glownet")
     end
   end
 
   describe ".format_abstract_value" do
-    before { allow(subject).to receive(:fa_icon).and_return "icon" }
+    it "return a check icon when true" do
+      expect(helper.format_abstract_value(true)).to include('fa-check')
+    end
 
-    it "return an icon when true" do
-      expect(format_abstract_value(true)).to include?("icon")
+    it "return a check icon when 'true' in string" do
+      expect(helper.format_abstract_value('true')).to include('fa-check')
+    end
+
+    it "return a times icon when false" do
+      expect(helper.format_abstract_value(false)).to include('fa-times')
+    end
+
+    it "return a times icon when 'false' in string" do
+      expect(helper.format_abstract_value('false')).to include('fa-times')
+    end
+
+    it "return 'empty icon when nil" do
+      expect(helper.format_abstract_value(nil)).to include('Empty')
+    end
+
+    it "return 'empty icon when empty string" do
+      expect(helper.format_abstract_value('')).to include('Empty')
     end
   end
 end
