53c308c5253c2e85489f9c07667ff78362a80c65
Alex Kazakov <kazakov_ab@mail.ru>
2017-01-11 06:09:04 +0100

    fix (refunds_csv): CSV contains first and last name of customer

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -28,7 +28,7 @@ class Refund < ActiveRecord::Base
 
   scope :query_for_csv, lambda { |event|
     joins(:customer)
-      .select("refunds.id, customers.email, refunds.amount, refunds.fee, refunds.money, refunds.status, refunds.field_a,
+      .select("refunds.id, customers.email, customers.first_name, customers.last_name, refunds.amount, refunds.fee, refunds.money, refunds.status, refunds.field_a,
                refunds.field_b, refunds.created_at").where(customers: { event_id: event.id })
   }
 
