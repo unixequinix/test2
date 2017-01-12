503412a2fe5cea96d00ab63c42d229c500f1fc72
Jake <jcpenche@gmail.com>
2017-01-12 13:56:11 +0100

    fix (migration): unify tickets and gtags before adding index

--- db/migrate/20170103202722_add_index_to_gtag_uid.rb
+++ db/migrate/20170103202722_add_index_to_gtag_uid.rb
@@ -1,10 +1,14 @@
 class AddIndexToGtagUid < ActiveRecord::Migration[5.0]
   def change
     tags = Gtag.select(:event_id, :tag_uid, :activation_counter).group(:event_id, :tag_uid, :activation_counter).having("count(*) > 1").map(&:tag_uid)
-    tags = Gtag.where(tag_uid: tags)
-    SaleItem.where(credit_transaction: Transaction.where(gtag: tags)).delete_all
-    Transaction.where(gtag: tags).delete_all
-    tags.destroy_all
+    Gtag.where(tag_uid: tags).group_by { |tag| [tag.tag_uid, tag.activation_counter] }.each do |_, tags|
+      next if tags.size == 1
+      first = tags.first
+      rest = tags.drop(1)
+      Transaction.where(gtag: rest).update_all(gtag: first, updated_at: Time.zone.now)
+      rest.map(&:delete)
+    end
+
     add_index :gtags, [:event_id, :tag_uid, :activation_counter], unique: true
   end
 end
--- db/migrate/20170112123749_add_index_to_ticket_codes_within_event.rb
+++ db/migrate/20170112123749_add_index_to_ticket_codes_within_event.rb
@@ -1,5 +1,14 @@
 class AddIndexToTicketCodesWithinEvent < ActiveRecord::Migration[5.0]
   def change
+    tickets = Ticket.select(:event_id, :code).group(:event_id, :code).having("count(*) > 1").map(&:code)
+    Ticket.where(code: tickets).group_by(&:code).each do |_, tickets|
+      next if tickets.size == 1
+      first = tickets.first
+      rest = tickets.drop(1)
+      Transaction.where(ticket: rest).update_all(ticket: first, updated_at: Time.zone.now)
+      rest.map(&:delete)
+    end
+
     add_index :tickets, [:event_id, :code], unique: true
   end
 end
