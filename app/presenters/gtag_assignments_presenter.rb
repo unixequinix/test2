704a836407b0101dbe4c05c1ee9eb75dd9d3ab0a
Jake <jcpenche@gmail.com>
2017-01-12 21:56:31 +0100

    fix (GA-2738): text not displaying propperly for gtag remove

--- app/presenters/gtag_assignments_presenter.rb
+++ app/presenters/gtag_assignments_presenter.rb
@@ -3,6 +3,10 @@ class GtagAssignmentsPresenter < BasePresenter
     @event.gtag_assignation? && @customer.active_credentials?
   end
 
+  def gtag_tag_uid
+    @gtag.tag_uid
+  end
+
   def path
     @gtag ? "gtag_assignments" : "new_gtag_assignments"
   end
@@ -15,15 +19,8 @@ class GtagAssignmentsPresenter < BasePresenter
     @event.gtag_assignation?
   end
 
-  def gtag_assignments_enabled
-    return unless @event.gtag_assignation? && valid_event_state?
-    snippet_gtag_assignments
-  end
-
-  def snippet_gtag_assignments
-    text = "#{context.t('dashboard.gtag_registration.button')} <span class='hint'>#{context.t('dashboard.gtag_registration.hint')}</span>"
-    path = context.event_gtag_assignment_path(context.current_event, gtag.id)
-    context.link_to(text, path, method: :delete, class: "btn btn-action btn-action-secondary")
+  def gtag_show_remove?
+    @event.gtag_assignation? && valid_event_state?
   end
 
   def valid_event_state?
