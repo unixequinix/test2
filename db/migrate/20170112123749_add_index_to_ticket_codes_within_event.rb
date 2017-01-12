d3d8c6ad56a157adf72b323666df273e22feba6b
Jake <jcpenche@gmail.com>
2017-01-12 13:58:53 +0100

    fix (migration): ammend to last commit

--- db/migrate/20170112123749_add_index_to_ticket_codes_within_event.rb
+++ db/migrate/20170112123749_add_index_to_ticket_codes_within_event.rb
@@ -5,7 +5,7 @@ class AddIndexToTicketCodesWithinEvent < ActiveRecord::Migration[5.0]
       next if tickets.size == 1
       first = tickets.first
       rest = tickets.drop(1)
-      Transaction.where(ticket_id: rest.map(&:id)).update_all(ticket: first, updated_at: Time.zone.now)
+      Transaction.where(ticket_id: rest.map(&:id)).update_all(ticket_id: first.id, updated_at: Time.zone.now)
       rest.map(&:delete)
     end
 
