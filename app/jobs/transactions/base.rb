2c427bbc33bc62bcd65d29d4e5761a335c529e00
Jake <jcpenche@gmail.com>
2017-01-16 15:52:18 +0100

    fix (gastrobande): ammend to last commit

--- app/jobs/transactions/base.rb
+++ app/jobs/transactions/base.rb
@@ -35,7 +35,7 @@ class Transactions::Base < ActiveJob::Base
   def preformat_atts(atts)
     atts[:activation_counter] = 1 if atts[:activation_counter].to_i.zero?
     atts[:transaction_origin] = Transaction::ORIGINS[:device]
-    atts[:station_id] = Station.find_by(event_id: atts[:event_id], station_event_id: atts[:station_id])&.id
+    atts[:station_id] = Station.find_by(event_id: atts[:event_id], station_event_id: atts[:station_id])&.id unless atts[:event_id].to_i.eql?(33)
     atts[:customer_tag_uid] = atts[:customer_tag_uid].to_s.upcase if atts.key?(:customer_tag_uid)
     atts[:order_item_counter] = atts[:order_item_id] if atts.key?(:order_item_id)
     atts[:device_created_at_fixed] = atts[:device_created_at]
