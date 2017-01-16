9c0fac0e47dbc720de5d2e3414144fe3396870b4
Jake <jcpenche@gmail.com>
2017-01-16 18:03:11 +0100

    fix (order_redeemer): gtag find is by id only

--- app/jobs/transactions/order/order_redeemer.rb
+++ app/jobs/transactions/order/order_redeemer.rb
@@ -2,7 +2,7 @@ class Transactions::Order::OrderRedeemer < Transactions::Base
   TRIGGERS = %w(order_redeemed).freeze
 
   def perform(atts)
-    gtag = Gtag.find_by(atts[:gtag_id])
+    gtag = Gtag.find(atts[:gtag_id])
     gtag.customer.order_items.find_by(counter: atts[:order_item_counter]).update!(redeemed: true)
   end
 end
