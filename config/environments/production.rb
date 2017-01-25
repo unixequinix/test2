065cb9545953672a534f71c50e12ac370dd23213
Jake <jcpenche@gmail.com>
2017-01-25 19:37:01 +0100

    fix (cdn): remove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.action_controller.asset_host = "assets.glownet.com"
+  # config.action_controller.asset_host = "assets.glownet.com"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
