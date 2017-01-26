a38942b9b49ee57a24bbc44de9d1f78c192c15b8
Jake <jcpenche@gmail.com>
2017-01-26 17:32:33 +0100

    Revert "fix (transactions): ammend for failing transactions. To be reverted"
    This reverts commit fb3e845181f45ba2cc03dab338dc7027fb12b23f.

--- app/jobs/transactions/credit/balance_updater.rb
+++ app/jobs/transactions/credit/balance_updater.rb
@@ -2,12 +2,6 @@ class Transactions::Credit::BalanceUpdater < Transactions::Base
   TRIGGERS = %w(sale topup refund fee record_credit sale_refund).freeze
 
   def perform(atts)
-    gtag = if atts[:gtag_id]
-             Gtag.find(atts[:gtag_id])
-           else
-             Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
-           end
-
-    gtag.recalculate_balance
+    Gtag.find(atts[:gtag_id]).recalculate_balance
   end
 end
