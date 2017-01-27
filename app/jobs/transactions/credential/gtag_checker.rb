f18aff3e3e384625cfc10b0a6713b777cefa54d6
Jake <jcpenche@gmail.com>
2017-01-27 18:58:33 +0100

    Revert "fix (transactions): performs checks for online transactions placed onsite"
    This reverts commit 008b052a6f16d7bfa347d5bb03add57bf87e5552.

--- app/jobs/transactions/credential/gtag_checker.rb
+++ app/jobs/transactions/credential/gtag_checker.rb
@@ -2,12 +2,6 @@ class Transactions::Credential::GtagChecker < Transactions::Base
   TRIGGERS = %w(gtag_checkin record_purchase).freeze
 
   def perform(atts)
-    gtag = if atts[:gtag_id]
-             Gtag.find(atts[:gtag_id])
-           else
-             Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
-           end
-    Transaction.find(atts[:transaction_id]).update!(gtag: gtag) unless atts[:gtag_id]
-    gtag.update!(customer_id: atts[:customer_id])
+    Gtag.find(atts[:gtag_id]).update!(customer_id: atts[:customer_id])
   end
 end
--- app/jobs/transactions/credit/balance_updater.rb
+++ app/jobs/transactions/credit/balance_updater.rb
@@ -2,12 +2,6 @@ class Transactions::Credit::BalanceUpdater < Transactions::Base
   TRIGGERS = %w(sale topup refund fee record_credit sale_refund).freeze
 
   def perform(atts)
-    gtag = if atts[:gtag_id]
-             Gtag.find(atts[:gtag_id])
-           else
-             Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
-           end
-    Transaction.find(atts[:transaction_id]).update!(gtag: gtag) unless atts[:gtag_id]
-    gtag.recalculate_balance
+    Gtag.find(atts[:gtag_id]).recalculate_balance
   end
 end
