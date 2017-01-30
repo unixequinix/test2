dd9f99b2b3504203bef04d29f104663100efe8a2
Jake <jcpenche@gmail.com>
2017-01-30 10:11:24 +0100

    fix (transactions): performs checks for online transactions placed onsite

--- app/jobs/transactions/credit/balance_updater.rb
+++ app/jobs/transactions/credit/balance_updater.rb
@@ -2,6 +2,12 @@ class Transactions::Credit::BalanceUpdater < Transactions::Base
   TRIGGERS = %w(sale topup refund fee record_credit sale_refund).freeze
 
   def perform(atts)
-    Gtag.find(atts[:gtag_id]).recalculate_balance
+    gtag = if atts[:gtag_id]
+             Gtag.find(atts[:gtag_id])
+           else
+             Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
+           end
+    Transaction.find(atts[:transaction_id]).update!(gtag: gtag) unless atts[:gtag_id]
+    gtag.recalculate_balance
   end
 end
