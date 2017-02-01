24a0bd7a72cea6d6736a4b5a2b0a3e7c6513fbaa
Jake <jcpenche@gmail.com>
2017-02-01 17:26:51 +0100

    fix (tickets): import now works

--- app/controllers/admins/events/tickets_controller.rb
+++ app/controllers/admins/events/tickets_controller.rb
@@ -62,7 +62,7 @@ class Admins::Events::TicketsController < Admins::Events::BaseController
       CSV.foreach(file, headers: true, col_sep: ";", encoding: "ISO8859-1:utf-8").with_index do |row, _i|
         c_name = row.field("company_name")
         com = Company.find_by("LOWER(name) = ?", c_name.downcase) || Company.create!(name: c_name)
-        agree = com.company_event_agreements.find_or_create_by!(event: @current_event, state: "granted")
+        agree = com.company_event_agreements.find_or_create_by!(event: @current_event)
 
         ticket_type_atts = { name: row.field("ticket_type"), company_code: row.field("company_code"), company_event_agreement: agree }
         ticket_type = @current_event.ticket_types.find_or_create_by!(ticket_type_atts)
