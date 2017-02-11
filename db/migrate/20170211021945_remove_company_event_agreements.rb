8c4514218c874205316df1b5395188bab64eeb33
Jake <jcpenche@gmail.com>
2017-02-11 06:49:24 +0100

    fix (debug): remove puts

--- db/migrate/20170211021945_remove_company_event_agreements.rb
+++ db/migrate/20170211021945_remove_company_event_agreements.rb
@@ -10,11 +10,9 @@ class RemoveCompanyEventAgreements < ActiveRecord::Migration[5.0]
       old_company = Company.find(company_id)
       new_company = Company.create!(name: old_company.name, access_token: old_company.access_token, event_id: event_id)
       tts = ActiveRecord::Base.connection.exec_query("SELECT id FROM ticket_types WHERE ticket_types.company_event_agreement_id = #{id}").rows.flatten
-      puts tts.inspect
       tts.each { |id| TicketType.find(id).update!(company_id: new_company.id) }
       (event.ticket_types - TicketType.find(tts)).each { |tt| tt.update_attribute(:company_id, event.companies.find_or_create_by!(name: "Glownet").id) }
     end
-
 
     remove_reference :ticket_types, :company_event_agreement, foreign_key: true, index: true
     ActiveRecord::Base.connection.exec_query("DELETE FROM company_event_agreements")
