1a63ac5a6bd2854a0c3446a925e03cc53e1c4a33
Jake <jcpenche@gmail.com>
2017-02-11 06:58:03 +0100

    refactor (views): currency and missing transactions view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,13 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) { |builder| render(association.to_s.singularize + "_fields", f: builder) }
     link_to(name, "#", class: "add_fields", data: { id: id, fields: fields.delete("\n") })
   end
-  # :nocov:
+
+  def number_to_event_currency(number)
+    number_to_currency number, format: "%n %u", unit: @current_event.currency + " "
+  end
 
   def number_to_token(number)
-    number_to_currency number, unit: @current_event.credit.name + " "
+    number_to_currency number, format: "%n %u", unit: @current_event.credit.name + " "
   end
 
   def title
