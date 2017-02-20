395c85f0e8bea6e078fe1fec0f861f4d4aa0b651
Jake <jcpenche@gmail.com>
2017-02-20 04:11:14 +0100

    fix (asset_tracker): updates algorithm and bases on normal queries

--- app/models/device.rb
+++ app/models/device.rb
@@ -16,30 +16,6 @@
 class Device < ActiveRecord::Base
   before_validation :upcase_asset_tracker!
 
-  def self.transactions_count(event) # rubocop:disable Metrics/MethodLength
-    sql = <<-SQL
-      SELECT to_json(json_agg(row_to_json(cep)))
-      FROM (
-        SELECT transaction.device_uid, count(transaction.device_uid) as transactions_count,
-        (
-          SELECT action
-          FROM device_transactions
-          WHERE device_transactions.device_uid = transaction.device_uid
-          AND device_transactions.event_id = #{event.id}
-          ORDER BY device_created_at DESC
-          LIMIT 1
-        ) as action
-        FROM (SELECT device_uid FROM device_transactions WHERE device_transactions.event_id = #{event.id}) as transaction
-        GROUP BY device_uid
-        ORDER BY device_uid
-      ) cep
-    SQL
-    conn = ActiveRecord::Base.connection
-    sql = conn.select_value(sql)
-    conn.close
-    JSON.parse(sql)
-  end
-
   def upcase_asset_tracker!
     asset_tracker&.upcase!
   end
