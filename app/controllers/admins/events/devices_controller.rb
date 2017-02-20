c232effb8595027bc1d6fe5ecaeb7f8c557ff832
Jake <jcpenche@gmail.com>
2017-02-20 04:11:14 +0100

    fix (asset_tracker): updates algorithm and bases on normal queries

--- app/controllers/admins/events/devices_controller.rb
+++ app/controllers/admins/events/devices_controller.rb
@@ -2,15 +2,17 @@ class Admins::Events::DevicesController < Admins::Events::BaseController
   # rubocop:disable all
   def index
     authorize Device.new
-    devices = @current_event.device_transactions.order(device_created_at: :asc).group_by(&:device_uid)
     @assets = {}
-    Device.where(mac: devices.map { |mac, _| mac }).each { |d| @assets[d.mac] = { asset_tracker: d.asset_tracker } }
-    transactions_sql = Device.transactions_count(@current_event)
+    @device_transactions = @current_event.device_transactions.order(:device_created_at)
+    @devices = Device.where()
 
-    devices.map do |device, transactions|
-      init = transactions.select { |t| t.action == "device_initialization" }
-      pack = transactions.select { |t| t.action == "pack_device" }
-      server_trans = transactions_sql.find { |t| t["device_uid"] == device }
+    transaction_counts = @current_event.transactions.onsite.group(:device_uid).count
+    @devices = Device.where(mac: transaction_counts.keys)
+
+    transaction_counts.each do |uid, count|
+      device_transactions = @device_transactions.select { |t| t.device_uid.eql?(uid) }
+      init = device_transactions.select { |t| t.action.eql?("device_initialization") }
+      pack = device_transactions.select { |t| t.action.eql?("pack_device") }
 
       if (init.count - pack.count == 1) && (init.last&.device_created_at.to_s > pack.last&.device_created_at.to_s)
         status = "in_use"
@@ -18,17 +20,24 @@ class Admins::Events::DevicesController < Admins::Events::BaseController
 
       elsif (init.count == pack.count) && (pack.last&.device_created_at.to_s > init.last&.device_created_at.to_s)
         device_trans = (pack.map(&:number_of_transactions).sum - init.map(&:number_of_transactions).sum) + init.count
-        status = (device_trans == server_trans["transactions_count"]) ? "packed" : "wrong_transactions"
+        status = (device_trans == pack.last&.number_of_transactions) ? "packed" : "wrong_transactions"
+
       else
         status = "to_check"
+        device_trans = device_transactions.count
       end
-      @assets[device] = @assets[device] || {}
-      @assets[device].merge!(device_transactions: device_trans,
-                             server_transactions: server_trans["transactions_count"],
-                             action: server_trans["action"],
-                             last_device_transaction: transactions.last[:number_of_transactions],
-                             status: status)
+      @assets[uid] = @assets[uid] || {}
+      @assets[uid].merge!(device_transactions: device_trans,
+                          server_transactions: count,
+                          action: device_transactions.last&.action,
+                          last_device_transaction: device_transactions.last&.number_of_transactions,
+                          status: status,
+                          asset_tracker: @devices.find { |d| d.mac.eql?(uid) }.asset_tracker)
     end
     @assets = @assets.group_by { |a| a.last[:status] }
+  end
+
+  def show
+
   end
 end
