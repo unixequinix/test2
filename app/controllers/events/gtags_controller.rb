b4094fd89229e776be7bac4a9fdd85d2d35fe36c
Jake <jcpenche@gmail.com>
2017-02-27 00:25:29 +0100

    fix (UI): Ui deals with gtags with or without ticket types

--- app/controllers/events/gtags_controller.rb
+++ app/controllers/events/gtags_controller.rb
@@ -1,6 +1,6 @@
 class Events::GtagsController < Events::EventsController
   def show
     @gtag = @current_event.gtags.find_by(id: params[:id], customer: current_customer)
-    @products = @gtag.ticket_type&.catalog_item&.all_catalog_items
+    @products = @gtag.ticket_type&.catalog_item&.all_catalog_items || []
   end
 end
