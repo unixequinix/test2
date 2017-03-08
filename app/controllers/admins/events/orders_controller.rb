292b198c0faa9d504470b5e9a34ed81b1234ec48
Jake <jcpenche@gmail.com>
2017-03-08 19:01:15 +0100

    feature (UI): adds order graph to index page

--- app/controllers/admins/events/orders_controller.rb
+++ app/controllers/admins/events/orders_controller.rb
@@ -4,6 +4,12 @@ class Admins::Events::OrdersController < Admins::Events::BaseController
     authorize @orders
     @orders = @orders.page(params[:page])
     @counts = @current_event.orders.group(:status).count
+
+    @graph = %w(in_progress completed cancelled failed).map do |action|
+      data = OrderItem.where(order: @current_event.orders.where(status: action)).group_by_day(:created_at).sum(:amount)
+      data = data.collect { |k, v| [k, v.to_i.abs] }
+      { name: action, data: Hash[data] }
+    end
   end
 
   def show
