54e64d53de939a518ebc54ac4575418898b7c97f
Jake <jcpenche@gmail.com>
2017-03-08 16:18:02 +0100

    fix (eventbrite): trying to deal with concurrency in importer

--- app/jobs/eventbrite_importer.rb
+++ app/jobs/eventbrite_importer.rb
@@ -7,15 +7,19 @@ class EventbriteImporter < ActiveJob::Base
 
     event.tickets.where(code: barcodes).update_all(banned: true) && return unless order[:status].eql?("placed")
 
-    ticket_types = company.ticket_types
     order[:attendees].each do |attendee|
       attendee["barcodes"].each do |barcode|
         profile = attendee["profile"]
 
         begin
-          ctt = ticket_types.find_or_create_by(company_code: attendee["ticket_class_id"], event: event, name: attendee["ticket_class_name"])
-          ticket = ctt.tickets.find_or_create_by(code: barcode["barcode"], event: event)
-          ticket.update(purchaser_first_name: profile["first_name"], purchaser_last_name: profile["last_name"], purchaser_email: profile["email"])
+          ctt = TicketType.find_or_create_by(company: company, company_code: attendee["ticket_class_id"], event: event, name: attendee["ticket_class_name"]) # rubocop:disable Metrics/LineLength
+        rescue ActiveRecord::RecordNotUnique
+          retry
+        end
+
+        begin
+          ticket = Ticket.find_or_initialize_by(code: barcode["barcode"], company_ticket_type: ctt, event: event)
+          ticket.update!(purchaser_first_name: profile["first_name"], purchaser_last_name: profile["last_name"], purchaser_email: profile["email"])
         rescue ActiveRecord::RecordNotUnique
           retry
         end
