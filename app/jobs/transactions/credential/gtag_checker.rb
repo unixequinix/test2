ea8998325dc4318dd8c68766ad92a0ee93051705
Jake <jcpenche@gmail.com>
2017-03-13 13:39:21 +0100

    feature (gtag_checkin): redeems gtag on checkin for Gtag in the post

--- app/jobs/transactions/credential/gtag_checker.rb
+++ app/jobs/transactions/credential/gtag_checker.rb
@@ -2,12 +2,8 @@ class Transactions::Credential::GtagChecker < Transactions::Base
   TRIGGERS = %w(gtag_checkin record_purchase).freeze
 
   def perform(atts)
-    gtag = if atts[:gtag_id]
-             Gtag.find(atts[:gtag_id])
-           else
-             Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
-           end
+    gtag = atts[:gtag_id] ? Gtag.find(atts[:gtag_id]) : Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
     Transaction.find(atts[:transaction_id]).update!(gtag: gtag) unless atts[:gtag_id]
-    gtag.update!(customer_id: atts[:customer_id])
+    gtag.update!(customer_id: atts[:customer_id], redeemed: true)
   end
 end
