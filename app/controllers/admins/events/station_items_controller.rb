f8a863332641002448ccf7364281821dc46ff1b0
Jake <jcpenche@gmail.com>
2017-03-16 14:30:54 +0100

    tests (station_items): ammend to last commit

--- app/controllers/admins/events/station_items_controller.rb
+++ app/controllers/admins/events/station_items_controller.rb
@@ -1,7 +1,7 @@
 class Admins::Events::StationItemsController < Admins::Events::BaseController
   before_action :set_item_class, except: [:find_product]
 
-  def create # rubocop:disable Metrics/AbcSize
+  def create # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     params[:station_product][:product_id] = current_event.products.find_or_create_by(name: params[:station_product][:product_name]).id if params[:item_type].eql?("station_product") # rubocop:disable Metrics/LineLength
 
     @station = @current_event.stations.find(params[:station_id])
