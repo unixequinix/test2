42c90d16e50555d607c7c8b3567e33812c99e124
Jake <jcpenche@gmail.com>
2017-03-24 12:20:40 +0100

    fix (eventbrite): check to see why tickets are failing

--- app/jobs/eventbrite_importer.rb
+++ app/jobs/eventbrite_importer.rb
@@ -13,7 +13,7 @@ class EventbriteImporter < ActiveJob::Base
         begin
           ctt = event.ticket_types.find_or_create_by!(company: company, company_code: guest["ticket_class_id"], name: guest["ticket_class_name"])
           ticket = event.tickets.find_or_create_by!(code: barcode["barcode"], ticket_type: ctt, event: event)
-        rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
+        rescue ActiveRecord::RecordNotUnique
           retry
         end
         ticket.update!(purchaser_first_name: profile["first_name"], purchaser_last_name: profile["last_name"], purchaser_email: profile["email"])
