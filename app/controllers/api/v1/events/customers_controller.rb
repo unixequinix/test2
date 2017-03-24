abd4c48b11f5495b8e0b586a23959b4a254af943
Jake <jcpenche@gmail.com>
2017-03-24 16:56:19 +0100

    fix (cache): customers call uses rails provided cache

--- app/controllers/api/v1/events/customers_controller.rb
+++ app/controllers/api/v1/events/customers_controller.rb
@@ -3,9 +3,7 @@ class Api::V1::Events::CustomersController < Api::V1::Events::BaseController
 
   def index
     customers = customers_sql || []
-    date = @current_event.customers.maximum(:updated_at)&.httpdate
-
-    render_entity(customers, date)
+    fresh_when(@current_event.customers.new, etag: @current_event.customers, last_modified: @current_event.customers.maximum(:updated_at), public: true) || render(json: customers)
   end
 
   def show
