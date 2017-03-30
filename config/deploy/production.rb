00c68e65abd86f4459fd93fa14ddef74465678db
Jake <jcpenche@gmail.com>
2017-03-30 13:23:37 -0300

    fix (deploy): moves branches back to develop

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 server "gspot.glownet.com", user: "deploy", roles: %w(web app db)
 
-set :branch, "auto-refunds"
+set :branch, "develop"
 set :rails_env, "production"
 
 set :sidekiq_processes, 1
