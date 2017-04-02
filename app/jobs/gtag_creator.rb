c077c5121038df2b0051c944a54073f294934f54
Jake <jcpenche@gmail.com>
2017-04-02 18:18:50 -0300

    fix (gtag_creator): ammed to last commit

--- app/jobs/gtag_creator.rb
+++ app/jobs/gtag_creator.rb
@@ -1,17 +1,22 @@
 class GtagCreator < ActiveJob::Base
   def perform(atts)
     gtag = Gtag.find_or_initialize_by(event_id: atts[:event_id], tag_uid: atts[:tag_uid])
-    gtag.update(atts)
-    return unless atts[:balance]
-    gtag.update(balance)
-    atts = { customer_tag_uid: gtag.tag_uid,
-             gtag_counter: 0,
-             gtag_id: gtag.id,
-             final_balance: atts[:balance],
-             final_refundable_balance: atts[:balance],
-             refundable_credits: atts[:balance]
-    }
-    CreditTransaction.write!(event, "record_credit", :device, nil, nil, atts)
+    gtag.update!(atts)
+    credits = atts[:credits]
+
+    return if credits.blank?
+    gtag.update!(final_balance: credits, final_refundable_balance: credits, refundable_credits: credits)
+
+    t_atts = { customer_tag_uid: gtag.tag_uid,
+               gtag_counter: 0,
+               gtag_id: gtag.id,
+               final_balance: credits,
+               final_refundable_balance: credits,
+               refundable_credits: credits,
+               credits: credits }
+
+    CreditTransaction.write!(Event.find(atts[:event_id]), "record_credit", :device, nil, nil, t_atts)
+
   rescue ActiveRecord::RecordNotUnique
     retry
   end
