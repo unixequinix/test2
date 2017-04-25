21a224ce034956ce749ade692ca4030b56fb2ddc
Jake <jcpenche@gmail.com>
2017-04-25 18:41:18 +0200

    fix (vuchup): redirection has no params

--- app/controllers/events/vouchup_controller.rb
+++ app/controllers/events/vouchup_controller.rb
@@ -2,13 +2,6 @@ class Events::VouchupController < Events::PaymentsController
   before_action :set_order_details, only: %i[purchase refund]
 
   def purchase
-    atts = {
-      order_id: @order.id,
-      auth_token: "TEST",
-      success_url: event_vouchup_success_url(@current_event),
-      error_url: event_vouchup_error_url(@current_event)
-    }.to_param
-
-    redirect_to "https://vouch-up.com/pay/#{@current_event.slug}/#{@order.id}?#{atts}"
+    redirect_to "https://vouch-up.com/pay/#{@current_event.slug}/#{@order.id}"
   end
 end
