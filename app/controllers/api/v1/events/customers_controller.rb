a49a6f4fe24f78e5b889d1569826abc3dee1e8a0
juanbertgomez <juanbertgomez@gmail.com>
2017-04-28 16:27:31 +0200

    fix(GA-3112): optimize performance of customers query (#179)

--- app/controllers/api/v1/events/customers_controller.rb
+++ app/controllers/api/v1/events/customers_controller.rb
@@ -21,60 +21,70 @@ class Api::V1::Events::CustomersController < Api::V1::Events::BaseController
     return [].to_json unless ids.any?
 
     sql = <<-SQL
-      SELECT json_strip_nulls(array_to_json(array_agg(row_to_json(cep))))
-      FROM (
-        SELECT
-          customers.id,
-          customers.banned,
-          customers.updated_at,
-          customers.first_name,
-          customers.last_name,
-          customers.email,
-          cred.credentials,
-          ord.orders
+    SELECT json_strip_nulls(array_to_json(array_agg(row_to_json(cep))))
+    FROM (
+      SELECT
+        customers.id,
+        customers.banned,
+        customers.updated_at,
+        customers.first_name,
+        customers.last_name,
+        customers.email,
+        cred.credentials,
+        ord.orders
 
-        FROM customers
+      FROM customers
 
-        LEFT OUTER JOIN (
-          SELECT cr.customer_id as customer_id, json_strip_nulls(array_to_json(array_agg(row_to_json(cr)))) as credentials
+        LEFT JOIN (
+                    SELECT
+                      cr.customer_id AS customer_id,
+                      json_strip_nulls(array_to_json(array_agg(row_to_json(cr)))) AS credentials
 
-          FROM (
-           SELECT customer_id, code as reference, 'ticket' as type
-            FROM tickets
-            WHERE tickets.customer_id IN (#{ids.join(', ')})
+                    FROM (
+                           SELECT
+                             customer_id,
+                             code     AS reference,
+                             'ticket' AS type
+                           FROM tickets
+                           WHERE tickets.customer_id IN
+                                 (#{ids.join(', ')})
 
-            UNION
-            SELECT customer_id, tag_uid as reference, 'gtag' as type
-            FROM gtags
-            WHERE gtags.active = true AND gtags.customer_id IN (#{ids.join(', ')})
-          ) cr
-          GROUP BY cr.customer_id
-        ) cred
-        ON customers.id = cred.customer_id
+                           UNION ALL
+                           SELECT
+                             customer_id,
+                             tag_uid AS reference,
+                             'gtag'  AS type
+                           FROM gtags
+                           WHERE gtags.active = TRUE AND gtags.customer_id IN
+                                                         (#{ids.join(', ')})
+                         ) cr
+                    GROUP BY cr.customer_id
+                  ) cred ON customers.id = cred.customer_id
+        LEFT JOIN (
+                    SELECT
+                      o.customer_id                                              AS customer_id,
+                      json_strip_nulls(array_to_json(array_agg(row_to_json(o)))) AS orders
+                    FROM (
+                           SELECT
+                             customer_id,
+                             counter AS id,
+                             amount,
+                             catalog_item_id,
+                             redeemed,
+                             orders.status
 
-        LEFT OUTER JOIN (
-          SELECT o.customer_id as customer_id, json_strip_nulls(array_to_json(array_agg(row_to_json(o)))) as orders
+                           FROM order_items
 
-          FROM (
-            SELECT
-              customer_id,
-              counter as id,
-              amount,
-              catalog_item_id,
-              redeemed,
-              orders.status
-
-            FROM order_items
-
-            INNER JOIN catalog_items ON catalog_items.id = order_items.catalog_item_id
-            INNER JOIN orders ON orders.id = order_items.order_id
-            WHERE orders.status IN ('completed', 'cancelled') AND orders.customer_id IN (#{ids.join(', ')})
-          ) o
-          GROUP BY o.customer_id
-        ) ord
-        ON customers.id = ord.customer_id
-        WHERE customers.id IN (#{ids.join(', ')})
-      ) cep
+                             JOIN catalog_items ON catalog_items.id = order_items.catalog_item_id
+                             JOIN orders ON orders.id = order_items.order_id
+                                            AND orders.status IN ('completed', 'cancelled') AND
+                                            orders.customer_id IN (#{ids.join(', ')})
+                         ) o
+                    GROUP BY o.customer_id
+                  ) ord
+          ON customers.id = ord.customer_id
+      WHERE customers.id IN (#{ids.join(', ')})
+    ) cep
     SQL
     ActiveRecord::Base.connection.select_value(sql)
   end
