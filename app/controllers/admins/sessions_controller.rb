4c4a18f7a00d3e1296ff56c869e8932fc1c3ffc0
Jake <jcpenche@gmail.com>
2017-05-08 20:32:22 +0200

    fix (sessions): adds stored_location for admin log in

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -3,8 +3,8 @@ class Admins::SessionsController < Devise::SessionsController
 
   private
 
-  def after_sign_in_path_for(_resource)
-    admins_events_path
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_events_path
   end
 
   def after_sign_out_path_for(_resource)
