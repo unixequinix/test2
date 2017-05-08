28247e04de7a7fdbd0d810e74907df22cd490398
Jake <jcpenche@gmail.com>
2017-05-08 21:19:47 +0200

    fix (Ga-3150): removes post -transaction action for record_purchase

--- app/jobs/transactions/credential/gtag_checker.rb
+++ app/jobs/transactions/credential/gtag_checker.rb
@@ -1,9 +1,7 @@
 class Transactions::Credential::GtagChecker < Transactions::Base
-  TRIGGERS = %w[gtag_checkin record_purchase].freeze
+  TRIGGERS = %w[gtag_checkin].freeze
 
   def perform(atts)
-    gtag = atts[:gtag_id] ? Gtag.find(atts[:gtag_id]) : Event.find(atts[:event_id]).gtags.find_by(tag_uid: atts[:customer_tag_uid])
-    Transaction.find(atts[:transaction_id]).update!(gtag: gtag) unless atts[:gtag_id]
-    gtag.update!(customer_id: atts[:customer_id], redeemed: true)
+    Gtag.find(atts[:gtag_id]).update!(redeemed: true)
   end
 end
