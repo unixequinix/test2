fca3309b4164b9a93d496ecaf43cbeec739b0d1d
Jake <jcpenche@gmail.com>
2017-05-10 18:45:16 +0200

    fix (device_transactions): makes device_transactions process in batches

--- app/controllers/api/v1/events/device_transactions_controller.rb
+++ app/controllers/api/v1/events/device_transactions_controller.rb
@@ -1,20 +1,21 @@
 class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseController
   def create
-    counter = @current_event.device_transactions.where(device_uid: permitted_params[:device_uid]).count + 1
-    device = @current_event.devices.find_or_create_by!(mac: permitted_params[:device_uid])
-    transaction = @current_event.device_transactions.new(permitted_params.merge(counter: counter, device: device))
+    render(status: :bad_request, json: :bad_request) && return unless params[:_json]
 
-    if transaction.save
-      render(status: :created, json: :created)
-    else
-      render json: transaction.errors, status: :unprocessable_entity
+    params[:_json].each_with_index do |atts, _index|
+      atts[:device_created_at_fixed] = atts[:device_created_at]
+      counter = @current_event.device_transactions.where(device_uid: atts[:device_uid]).count + 1
+      device = @current_event.devices.find_or_create_by!(mac: atts[:device_uid])
+      transaction = @current_event.device_transactions.new(atts.merge(counter: counter, device: device))
+      transaction.save!
     end
+
+    render(status: :created, json: :created)
   end
 
   private
 
   def permitted_params
-    # params.permit!.to_h[:_json].first.slice(:action, :device_uid, :initialization_type, :number_of_transactions, :battery)
-    params.require(:device_transaction).permit(:action, :device_uid, :initialization_type, :number_of_transactions, :battery)
+    # params.require(:device_transaction).permit(:action, :device_uid, :initialization_type, :number_of_transactions, :battery)
   end
 end
