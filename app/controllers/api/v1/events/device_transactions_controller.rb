48602811402f461a921b5a3ef8ec4a4b8ca71122
Jake <jcpenche@gmail.com>
2017-05-10 18:59:43 +0200

    fix (device_transactions): ammend to last commit

--- app/controllers/api/v1/events/device_transactions_controller.rb
+++ app/controllers/api/v1/events/device_transactions_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseContr
 
     params.permit!.to_h[:_json].each_with_index do |atts, _index|
       atts.delete(:device_created_at)
+      atts.delete(:device_db_index)
       counter = @current_event.device_transactions.where(device_uid: atts[:device_uid]).count + 1
       device = @current_event.devices.find_or_create_by!(mac: atts[:device_uid])
       transaction = @current_event.device_transactions.new(atts.merge(counter: counter, device: device))
