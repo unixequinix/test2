ede8e4585ae0702d284349df055f00f0b40a56bd
Jake <jcpenche@gmail.com>
2017-05-10 10:12:00 +0200

    refactor (credentials): updates credential visualization in customer portal

--- app/controllers/events/tickets_controller.rb
+++ app/controllers/events/tickets_controller.rb
@@ -1,6 +1,6 @@
 class Events::TicketsController < Events::EventsController
   def show
     @ticket = @current_event.tickets.find_by(id: params[:id], customer: current_customer)
-    @products = @ticket.ticket_type&.catalog_item&.all_catalog_items
+    @item = @ticket.ticket_type&.catalog_item
   end
 end
