c352e1c9faffd0f9cf3d0ea5302b47ef45f8fc27
Jake <jcpenche@gmail.com>
2017-05-11 12:47:20 +0200

    refactor (device_transactions): backward compatibility

--- app/controllers/api/v1/events/device_transactions_controller.rb
+++ app/controllers/api/v1/events/device_transactions_controller.rb
@@ -1,13 +1,10 @@
 class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseController
   def create
-    render(status: :bad_request, json: :bad_request) && return unless params[:_json]
-
-    params.permit!.to_h[:_json].each_with_index do |atts, _index|
+    permitted_params.each do |atts|
       atts = atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions)
       counter = @current_event.device_transactions.where(device_uid: atts[:device_uid]).count + 1
       device = @current_event.devices.find_or_create_by!(mac: atts[:device_uid])
-      transaction = @current_event.device_transactions.new(atts.merge(counter: counter, device: device))
-      transaction.save!
+      @current_event.device_transactions.create!(atts.merge(counter: counter, device: device))
     end
 
     render(status: :created, json: :created)
@@ -16,6 +13,8 @@ class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseContr
   private
 
   def permitted_params
-    # params.require(:device_transaction).permit(:action, :device_uid, :initialization_type, :number_of_transactions, :battery)
+    all_params = params.permit!.to_h
+    all_params = [all_params[:device_transaction]] + [all_params[:_json]].flatten
+    all_params.map { |atts| atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions) }
   end
 end
