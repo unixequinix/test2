eacc99edd11ebc1f90d83fd8a96b1ba5fde4c7ec
Jake <jcpenche@gmail.com>
2017-05-11 12:59:48 +0200

    fix (device_transactions): ammend to last commit

--- app/controllers/api/v1/events/device_transactions_controller.rb
+++ app/controllers/api/v1/events/device_transactions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseController
   def create
     permitted_params.each do |atts|
-      atts = atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions)
+      next if atts.empty?
       counter = @current_event.device_transactions.where(device_uid: atts[:device_uid]).count + 1
       device = @current_event.devices.find_or_create_by!(mac: atts[:device_uid])
       @current_event.device_transactions.create!(atts.merge(counter: counter, device: device))
@@ -13,8 +13,8 @@ class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseContr
   private
 
   def permitted_params
-    all_params = params.permit!.to_h
+    all_params = params.permit!.to_h.symbolize_keys
     all_params = [all_params[:device_transaction]] + [all_params[:_json]].flatten
-    all_params.compact.flatten.map { |atts| atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions) }
+    all_params.compact.flatten.map { |atts| atts.symbolize_keys.slice(:action, :device_uid, :initialization_type, :number_of_transactions) }
   end
 end
