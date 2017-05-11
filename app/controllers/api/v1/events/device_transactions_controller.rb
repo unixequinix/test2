5e68b91fdd90264eb6bb5c51b1a88ee28fb1532d
Jake <jcpenche@gmail.com>
2017-05-11 12:51:51 +0200

    fix (device_transactions): ammend to last commit

--- app/controllers/api/v1/events/device_transactions_controller.rb
+++ app/controllers/api/v1/events/device_transactions_controller.rb
@@ -15,6 +15,6 @@ class Api::V1::Events::DeviceTransactionsController < Api::V1::Events::BaseContr
   def permitted_params
     all_params = params.permit!.to_h
     all_params = [all_params[:device_transaction]] + [all_params[:_json]].flatten
-    all_params.map { |atts| atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions) }
+    all_params.compact.flatten.map { |atts| atts.slice(:action, :device_uid, :initialization_type, :number_of_transactions) }
   end
 end
