91972bcf8061ff9edd14c96d29a5a895a848e987
Jake <jcpenche@gmail.com>
2017-05-18 11:05:14 +0200

    fix (order_redeemer): now uses customer id instead of gtag

--- app/jobs/transactions/order/order_redeemer.rb
+++ app/jobs/transactions/order/order_redeemer.rb
@@ -2,8 +2,8 @@ class Transactions::Order::OrderRedeemer < Transactions::Base
   TRIGGERS = %w[order_redeemed].freeze
 
   def perform(atts)
-    gtag = Gtag.find(atts[:gtag_id])
-    gtag.customer.order_items.find_by(counter: atts[:order_item_counter]).update!(redeemed: true)
-    gtag.customer.touch
+    customer = Event.find(atts[:event_id]).customers.find(atts[:customer_id])
+    customer.order_items.find_by(counter: atts[:order_item_counter]).update!(redeemed: true)
+    customer.touch
   end
 end
