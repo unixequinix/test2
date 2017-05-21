61c04ca4dafd717641f924151ffaba14e64b5188
Jake <jcpenche@gmail.com>
2017-05-21 16:24:02 +0200

    fix (devices): adds device registration duplicate removal to mirgations

--- db/migrate/20170521142235_remove_duplicate_devices.rb
+++ db/migrate/20170521142235_remove_duplicate_devices.rb
@@ -8,5 +8,12 @@ class RemoveDuplicateDevices < ActiveRecord::Migration[5.0]
       DeviceRegistration.where(device_id: devices.map(&:id)).update_all(device_id: good_one.id)
       devices.map(&:destroy!)
     end
+
+    DeviceRegistration.group(:device_id, :event_id).count.each do |atts, count|
+      next unless count > 1
+      registrations = DeviceRegistration.where(device_id: atts.first, event_id: atts.last).to_a
+      registrations.shift
+      registrations.map(&:destroy!)
+    end
   end
 end
