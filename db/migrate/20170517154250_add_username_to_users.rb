fd2fd8a66fd2b84470bac1f7deacec5d25e9ac14
Jake <jcpenche@gmail.com>
2017-05-21 16:56:30 +0200

    fix (usernames): ammend script to add random string

--- db/migrate/20170517154250_add_username_to_users.rb
+++ db/migrate/20170517154250_add_username_to_users.rb
@@ -2,7 +2,7 @@ class AddUsernameToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :username, :string
     User.all.each do |user|
-      user.update!(username: user.email.split("@").first)
+      user.update(username: user.email.split("@").first + SecureRandom.hex)
     end
     add_index :users, :username, unique: true
   end
