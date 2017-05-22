b399ccf3417addf2359b9fbafb9ae8c645c2ae52
Jake <jcpenche@gmail.com>
2017-05-22 18:22:43 +0200

    feature (login): allows for users to log in with username or password

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,10 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+
+  # Username or email
+  config.authentication_keys = [ :login ]
+
   # config.secret_key = Rails.application.secrets.secret_key_base
   config.secret_key = '92b0f6a8ba7a3a8fa377bbe3c29a903e75eddff403c309ea15db385957bdcef743007fd4e08bbbd01048d02c9f5529b9523d6742903e42f5e3ad4255536898a4'
 
