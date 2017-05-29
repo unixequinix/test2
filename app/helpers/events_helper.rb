dbdb5549f0d262d14c7117bf7a1c74dc71edf8bd
Jake <jcpenche@gmail.com>
2017-05-29 14:36:37 +0200

    style (best_in_place): checkboxes now appear as traks and thumbs

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,7 @@
 module EventsHelper
   def best_in_place_checkbox(url)
-    yes = content_tag :span, "done", class: "icon material-icons"
-    no = content_tag :span, "clear", class: "icon material-icons"
+    yes = render "layouts/checkbox_yes"
+    no = render "layouts/checkbox_no"
     { collection: { false: no, true: yes }, place_holder: no, as: :checkbox, url: url }
   end
 end
