6f0276944128afa8ddf8d467c72c6141d1ebe92d
Jake <jcpenche@gmail.com>
2017-06-01 12:45:41 +0200

    fix (operator_permissions): send station_event_id instead of station_id

--- app/controllers/api/v1/events/operator_permissions_controller.rb
+++ app/controllers/api/v1/events/operator_permissions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Events::OperatorPermissionsController < Api::V1::Events::BaseCont
   before_action :set_modified
 
   def index
-    permissions = @current_event.operator_permissions
+    permissions = @current_event.operator_permissions.includes(:station)
     permissions = permissions.where("catalog_items.updated_at > ?", @modified) if @modified
     date = permissions.maximum(:updated_at)&.httpdate
     permissions = permissions.map { |a| Api::V1::OperatorPermissionSerializer.new(a) }.to_json if permissions.present?
