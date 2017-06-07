1c38153accb49ea13d5d51716d7952fe7956ca92
Jake <jcpenche@gmail.com>
2017-06-07 19:09:32 +0200

    fix (alerts): only send to accepted registrations

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -10,7 +10,7 @@ class Alert < ApplicationRecord
   enum priority: { low: 0, medium: 1, high: 2 }
 
   def self.propagate(event, body, priority, subject)
-    ids_of_users = event.event_registrations.where(role: :promoter).pluck(:user_id)
+    ids_of_users = event.event_registrations.where(role: :promoter).where.not(user_id: nil).pluck(:user_id)
     ids_of_users.map { |id_of_user| create!(event: event, body: body, priority: priority, subject: subject, user_id: id_of_user) }
   end
 end
