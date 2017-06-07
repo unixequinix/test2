ff8bde1ab6dbd4278c35a7e87aa4e97ce13ef4a5
Jake <jcpenche@gmail.com>
2017-06-07 17:52:41 +0200

    feature (API V2): adds refund and topup settings in events#show

--- app/serializers/api/v2/event_serializer.rb
+++ app/serializers/api/v2/event_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V2::EventSerializer < ActiveModel::Serializer
-  attributes :name, :slug, :logo, :background, :currency, :credit_name, :state
+  attributes :name, :slug, :logo, :background, :currency, :credit_name, :state, :open_topups, :open_refunds
 
   def credit_name
     object.credit.name
