a6464eda1b4ebbc04a2b2c558a659a326a6f8e4a
Jake <jcpenche@gmail.com>
2017-06-12 12:33:19 +0200

    fix (operator permissions): check role against event_id

--- app/models/operator_permission.rb
+++ app/models/operator_permission.rb
@@ -10,7 +10,7 @@ class OperatorPermission < CatalogItem
   validates :station_id, absence: true, if: (-> { group.present? })
   validates :group, absence: true, if: (-> { station_id.present? })
 
-  validates :role, uniqueness: { scope: %i[group station_id], message: (->(object, _data) { "This role has already been used for #{'group ' + object.group if object.group}#{'station ' + object.station.name if object.station}" }) } # rubocop:disable Metrics/LineLength
+  validates :role, uniqueness: { scope: %i[event_id group station_id], message: (->(object, _data) { "This role has already been used for #{'group ' + object.group if object.group}#{'station ' + object.station.name if object.station}" }) } # rubocop:disable Metrics/LineLength
 
   enum role: { operator: 1, manager: 2 }
   enum group: { glownet: 0, checkin: 1, box_office: 2, banking: 3, vendors: 4, bars: 5, management: 6 }
