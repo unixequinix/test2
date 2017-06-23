e5051e8cadbbafbe2d746dca45d3fc54d537aac7
Jake <jcpenche@gmail.com>
2017-06-23 12:13:20 +0200

    fix (GA-3256): ticket_type update touches all tickets

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -12,4 +12,12 @@ class TicketType < ApplicationRecord
 
   scope(:for_devices, -> { where.not(catalog_item_id: nil) })
   scope(:no_catalog_item, -> { where(catalog_item_id: nil) })
+
+  after_update :touch_tickets
+
+  private
+
+  def touch_tickets
+    tickets.update_all(updated_at: updated_at)
+  end
 end
--- spec/models/ticket_type_spec.rb
+++ spec/models/ticket_type_spec.rb
@@ -1,10 +1,16 @@
 require "spec_helper"
 
 RSpec.describe TicketType, type: :model do
-  subject { build(:ticket_type) }
   let(:event) { create(:event) }
+  subject { create(:ticket_type, event: event) }
 
   it "has a valid factory" do
     expect(subject).to be_valid
+  end
+
+  it "touches tickets on update" do
+    tickets = create_list(:ticket, 10, event: event, ticket_type: subject)
+    subject.update!(name: "name")
+    tickets.each { |t| expect(t.reload.updated_at).to eql(subject.updated_at) }
   end
 end
