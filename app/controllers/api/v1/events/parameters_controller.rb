8862e102731adaf18db3f2c3803890ca13a7d69b
Jake <jcpenche@gmail.com>
2017-06-26 16:31:44 +0200

    fix (parameters): old_gtag_keys must be of events, not "test value"

--- app/controllers/api/v1/events/parameters_controller.rb
+++ app/controllers/api/v1/events/parameters_controller.rb
@@ -11,14 +11,17 @@ class Api::V1::Events::ParametersController < Api::V1::Events::BaseController
               initial_topup_fee topup_fee maximum_gtag_balance stations_apply_orders stations_initialize_gtags]
 
     body = cols.map { |col| { name: col, value: @current_event.send(col) } }
-
-    value = Rails.env.production? || Rails.env.demo? || Rails.env.hotfix? ? @current_event.gtag_key : "11111111111111111111111111111111"
+    production_env = Rails.env.production? || Rails.env.demo? || Rails.env.hotfix?
+    fake_key = "11111111111111111111111111111111"
+    value = production_env ? @current_event.gtag_key : fake_key
 
     body << { name: "ultralight_c_private_key", value: value } if @current_event.gtag_type.eql?("ultralight_c")
     body << { name: "gtag_key", value: value }
     body << { name: "cypher_enabled", value: @current_event.name.downcase.tr("ó", "o").include?("sonar") }
 
-    old_events = @current_event.event_serie ? @current_event.event_serie.events.where.not(id: @current_event.id).collect { |_e| value } : []
+    # rubocop:disable Style/NestedTernaryOperator
+    serie = @current_event.event_serie
+    old_events = serie ? serie.events.where.not(id: @current_event.id).map { |e| production_env ? e.gtag_key : fake_key } : []
     body << { name: "old_event_keys", value: old_events }
 
     render_entity(body.as_json, @current_event.updated_at&.httpdate)
