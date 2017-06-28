ed25f62b27a543ea7c30f0ac02807bc6fed9add7
Jake <jcpenche@gmail.com>
2017-06-28 15:55:47 +0200

    fix deploy): moves production environments to develop

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -8,7 +8,7 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 server "sandbox.glownet.com", user: "deploy", roles: %w[web app db]
 
-set :branch, "master"
+set :branch, "develop"
 set :rails_env, "sandbox"
 
 # role-based syntax
