864be14832fc4170f27a87e8c4fd31d78f96cf02
Jake <jcpenche@gmail.com>
2017-06-29 10:24:23 +0200

    fix (gtag_checker): alert needs an event

--- app/jobs/transactions/credential/gtag_checker.rb
+++ app/jobs/transactions/credential/gtag_checker.rb
@@ -3,6 +3,6 @@ class Transactions::Credential::GtagChecker < Transactions::Base
 
   def perform(atts)
     gtag = Gtag.find(atts[:gtag_id])
-    gtag.redeemed? ? Alert.propagate(event, "has been redeemed twice", :high, gtag) : gtag.update!(redeemed: true)
+    gtag.redeemed? ? Alert.propagate(Event.find(atts[:event_id]), "has been redeemed twice", :high, gtag) : gtag.update!(redeemed: true)
   end
 end
