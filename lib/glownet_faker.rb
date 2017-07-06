27e1ab259b5ab4d48d2825ef596c681af68739e4
Jake <jcpenche@gmail.com>
2017-07-06 13:21:15 +0200

    fix (faker): adds rails logger instead of logger

--- lib/glownet_faker.rb
+++ lib/glownet_faker.rb
@@ -6,37 +6,37 @@ class GlownetFaker
   end
 
   def tickets
-    logger.info " -- creating #{@amount} tickets for current ticket types"
+    Rails.logger.info " -- creating #{@amount} tickets for current ticket types"
     @event.ticket_types.each { |tt| @amount.times { |i| @values << [@event.id, tt.id, "#{tt.name}#{i}"] } }
     Ticket.import %i[event_id ticket_type_id code], @values, validate: false
-    logger.info "DONE"
+    Rails.logger.info "DONE"
   end
 
   def gtags
-    logger.info " -- creating #{@amount} gtags for current event"
+    Rails.logger.info " -- creating #{@amount} gtags for current event"
     @amount.times { |i| @values << ["TAG#{i}", @event.id] }
     Gtag.import %i[tag_uid event_id], @values, validate: false
-    logger.info "DONE"
+    Rails.logger.info "DONE"
   end
 
   def customers
-    logger.info " -- creating #{@amount} customers"
+    Rails.logger.info " -- creating #{@amount} customers"
     password = "$2a$11$M4nToRzivPjDzYjzwa9NMu3LY.ZlAxMuJXb0fEd7vVFL/HOlc514u"
     columns = %w[event_id agreed_on_registration email encrypted_password first_name last_name].map(&:to_sym)
     @amount.times { |i| @values << [@event.id, true, "customer#{i}@fake.com", password, "Customer#{i}", "Smith#{i}"] }
     Customer.import columns, @values, validate: false
-    logger.info "DONE"
+    Rails.logger.info "DONE"
   end
 
   def gtags_for_customers
-    logger.info " -- creating #{@amount} gtags for current event"
+    Rails.logger.info " -- creating #{@amount} gtags for current event"
     @event.customers.pluck(:id).each { |id| @values << ["CUSTTAG#{id}", @event.id, id] }
     Gtag.import %i[tag_uid event_id customer_id], @values, validate: false
-    logger.info "DONE"
+    Rails.logger.info "DONE"
   end
 
   def orders
-    logger.info " -- creating one order per customer for current customers"
+    Rails.logger.info " -- creating one order per customer for current customers"
     columns = %w[event_id status completed_at gateway customer_id].map(&:to_sym)
     @event.customers.pluck(:id).each { |id| @values << [@event.id, "completed", Time.zone.now, "paypal", id] }
     Order.import columns, @values, validate: false
@@ -49,6 +49,6 @@ class GlownetFaker
       @values << [order, credit_id, total, total, true, 1]
     end
     OrderItem.import columns, @values, validate: false
-    logger.info "DONE"
+    Rails.logger.info "DONE"
   end
 end
