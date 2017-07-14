8c89c8e99b4ee98286b455efac16a054197b2bd1
Jake <jcpenche@gmail.com>
2017-07-14 15:20:43 +0200

    revert (GA-3278): removes yellow card station to non closed events

--- db/migrate/20170713125001_add_yellow_card_to_all_events.rb
+++
@@ -1,7 +0,0 @@
-class AddYellowCardToAllEvents < ActiveRecord::Migration[5.1]
-  def change
-    Event.where.not(state: "closed").each do |event|
-      event.stations.create! name: "Yellow Card", category: "yellow_card"
-    end
-  end
-end
