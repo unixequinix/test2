25a765c9d68d2ad1328857c28322eafef635092a
Jake <jcpenche@gmail.com>
2017-07-20 21:46:31 +0200

    fix (sidekiq): start every minute

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,3 +29,7 @@ end
 every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
   runner "Event.reload_stats"
 end
+
+every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+  rake "sidekiq:start"
+end
