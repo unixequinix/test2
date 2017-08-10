cb58e12582c94424c0536d386f00927cba5576f7
Jake <jcpenche@gmail.com>
2017-08-10 17:36:58 +0200

    feature (GA-3380): Bring back device registration pop-up in the device

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::EventsController < Api::V1::BaseController
   def index
     @events = params[:filter].to_s.eql?("active") ? Event.where(state: :launched) : Event.all
     @events = @events.where(open_api: true)
-    render json: @events, each_serializer: Api::V1::EventSerializer
+
+    device = Device.find_or_create_by!(mac: params[:mac])
+    status = device.asset_tracker.blank? ? :accepted : :ok
+
+    render status: status, json: @events, each_serializer: Api::V1::EventSerializer
   end
 end
