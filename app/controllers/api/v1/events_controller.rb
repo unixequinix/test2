91443629bb7a076eadf3d22f590d7d70eecbf621
Jake <jcpenche@gmail.com>
2017-08-22 12:14:25 +0200

    fix (GA-3391): skip asset request for name if param is not present

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::EventsController < Api::V1::BaseController
     @events = params[:filter].to_s.eql?("active") ? Event.where(state: :launched) : Event.all
     @events = @events.where(open_api: true)
 
+    render(stats: :ok, json: @events, each_serializer: Api::V1::EventSerializer) if params[:mac].blank?
+
     device = Device.find_or_create_by!(mac: params[:mac])
     status = device.asset_tracker.blank? ? :accepted : :ok
 
