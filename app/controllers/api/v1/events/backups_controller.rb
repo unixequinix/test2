8ced21a5b0e9205973ad433fbbe404b795df7c74
Jake <jcpenche@gmail.com>
2017-08-28 12:52:21 +0200

    fix (GA-3419): backup_created_at not sent should give bad_request

--- app/controllers/api/v1/events/backups_controller.rb
+++ app/controllers/api/v1/events/backups_controller.rb
@@ -3,18 +3,18 @@ class Api::V1::Events::BackupsController < Api::V1::Events::BaseController
 
   def create
     keys = %i[device_uid backup_created_at backup].any? { |i| params[i] }
+    render(status: :bad_request, json: { error: "params missing" }) && return unless keys
 
     secrets = Rails.application.secrets
     credentials = Aws::Credentials.new(secrets.s3_access_key_id, secrets.s3_secret_access_key)
     s3 = Aws::S3::Resource.new(region: 'eu-west-1', credentials: credentials)
 
     device = params[:device_uid].to_s.delete("\"")
-    time = Time.parse(params[:backup_created_at]).to_i
+    time = Time.parse(Time.zone.now).to_i
     name = "gspot/event/#{params[:event_id]}/backups/#{device}/#{time}.db"
     obj = s3.bucket(Rails.application.secrets.s3_bucket).object(name)
     obj.put(body: params[:backup])
 
-    render(status: :bad_request, json: { error: "params missing" }) && return unless keys
     render(status: :created, json: :created)
   end
 end
