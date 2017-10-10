a8cbf0f05d9831a8b29dc543b840a103076cb58f
Fran <francisco.moya.martinez@gmail.com>
2017-10-10 17:20:54 +0200

    fix (vouchup): add sandbox env redirection (#329)
    * fix (vouchup): add sandbox env redirection
    
    * refactor (code): cleans up case for return assignment operator

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -2,7 +2,10 @@ module PaymentGatewaysHelper
   def store_redirection(event)
     return new_event_order_path(event) unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
 
-    url = Rails.env.production? ? "wiredlion.io/register" : "dev.wiredlion.io/register"
+    url = "dev.wiredlion.io/register"
+    url = "wiredlion.io/register" if Rails.env.production?
+    url = "demo.wiredlion.io/register" if Rails.env.sandbox?
+
     "https://#{url}/#{event.slug}/glownet/#{current_customer.id}"
   end
 end
