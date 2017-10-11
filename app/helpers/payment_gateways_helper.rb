d2498d4712da7b6e1617f8d1930a66b0e2d3994f
Jake <jcpenche@gmail.com>
2017-10-11 18:12:46 +0200

    refactor (GA-351): slims ode a bit

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -1,14 +1,12 @@
 module PaymentGatewaysHelper
   def store_redirection(event, type, options = {})
-    path = new_event_refund_path(event) if type == :refund
-    path = new_event_order_path(event) if type == :order
+    return send("new_event_#{type}_path", event) unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
 
-    return path unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
+    host = YAML.load_file(Rails.root.join('config', 'glownet', 'payment_gateways.yml'))["vouchup"]["host"][Rails.env]
 
-    host = YAML.load_file("#{Rails.root}/config/glownet/payment_gateways.yml")["vouchup"]["host"][Rails.env]
-    full_uri = URI::HTTP.build(host: host, path: "/refund/#{event.slug}/customer-details", query: "email=#{current_customer.email}&gtag=#{options[:gtag_id]}") if type == :refund
-    full_uri = URI::HTTP.build(host: host, path: "/register/#{event.slug}/glownet/#{current_customer.id}") if type == :order
+    params = { path: "/refund/#{event.slug}/customer-details", query: "email=#{current_customer.email}&gtag=#{options[:gtag_id]}" } if type == :refund
+    params = { path: "/register/#{event.slug}/glownet/#{current_customer.id}" } if type == :order
 
-    "https://#{full_uri}"
+    "https://#{URI::HTTP.build({ host: host }.merge(params))}"
   end
 end
