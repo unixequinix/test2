93354f983f15a20c9f016f3210338d1fc040a010
Jake <jcpenche@gmail.com>
2017-10-11 18:39:39 +0200

    fix (stats): rename refund to online_refund for stat separation

--- app/jobs/stats/money.rb
+++ app/jobs/stats/money.rb
@@ -2,7 +2,7 @@ class Stats::Money < Stats::Base
   include StatsHelper
 
   # MoneyTransaction#topup actually does not exist, since online topups are called purchases. But in case it happens
-  TRIGGERS = %w[onsite_topup onsite_refund refund topup].freeze
+  TRIGGERS = %w[onsite_topup onsite_refund online_refund].freeze
 
   def perform(transaction_id)
     t = MoneyTransaction.find(transaction_id)
