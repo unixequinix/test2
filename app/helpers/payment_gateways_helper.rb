05b5329fe5154c3d72ccbb323ef4bc701c7565f7
Fran <francisco.moya.martinez@gmail.com>
2017-10-11 18:03:27 +0200

    feature (wiredlion): refunds wiredlion redirection (#331)
    * feature (wiredlion): refunds wiredlion redirection
    
    * feature (wiredlion): fix helper tests

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -1,11 +1,14 @@
 module PaymentGatewaysHelper
-  def store_redirection(event)
-    return new_event_order_path(event) unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
+  def store_redirection(event, type, options = {})
+    path = new_event_refund_path(event) if type == :refund
+    path = new_event_order_path(event) if type == :order
 
-    url = "dev.wiredlion.io/register"
-    url = "wiredlion.io/register" if Rails.env.production?
-    url = "demo.wiredlion.io/register" if Rails.env.sandbox?
+    return path unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
 
-    "https://#{url}/#{event.slug}/glownet/#{current_customer.id}"
+    host = YAML.load_file("#{Rails.root}/config/glownet/payment_gateways.yml")["vouchup"]["host"][Rails.env]
+    full_uri = URI::HTTP.build(host: host, path: "/refund/#{event.slug}/customer-details", query: "email=#{current_customer.email}&gtag=#{options[:gtag_id]}") if type == :refund
+    full_uri = URI::HTTP.build(host: host, path: "/register/#{event.slug}/glownet/#{current_customer.id}") if type == :order
+
+    "https://#{full_uri}"
   end
 end
