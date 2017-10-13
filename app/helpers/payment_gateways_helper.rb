95a48478118d5428c16161146b2c89c9db4e1bcf
Jake <jcpenche@gmail.com>
2017-10-13 16:03:58 +0200

    chore (database): changed connection pool for all except PRD and HFX

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -5,7 +5,7 @@ module PaymentGatewaysHelper
     return path unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
 
     params = { path: "/register/#{event.slug}/glownet/#{current_customer.id}" }
-    params = { path: "/refund/#{event.slug}/customer-details", query: { email: current_customer.email, gtag: options[:gtag_uid] }.to_param } if type == :refund # rubocop:disable Metric/LineLength
+    params = { path: "/refund/#{event.slug}/customer-details", query: { email: current_customer.email, gtag: options[:gtag_uid] }.to_param } if type == :refund # rubocop:disable Metrics/LineLength
 
     url = { host: YAML.load_file(Rails.root.join('config', 'glownet', 'payment_gateways.yml'))["vouchup"]["host"][Rails.env] }.merge(params)
 
