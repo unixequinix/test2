e883e69f0f9d32508019d8af287f5dc3095b73c9
Jake <jcpenche@gmail.com>
2017-10-16 01:17:21 +0200

    fix (stats): adds precision 10 to money stats

---
+++ db_stats/migrate/20171015230821_change_purchase_precisions.rb
@@ -0,0 +1,6 @@
+class ChangeSaleItemsPrecisions < ActiveRecord::Migration[5.1]
+  def change
+    change_column :stats, :monetary_unit_price, :decimal, precision: 10, scale: 2
+    change_column :stats, :monetary_total_price, :decimal, precision: 10, scale: 2
+  end
+end
