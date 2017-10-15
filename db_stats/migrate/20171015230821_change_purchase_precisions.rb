d04716bf688c101b3c71c0bb79c81e75e8eaab31
Jake <jcpenche@gmail.com>
2017-10-16 01:18:55 +0200

    fix (stats): refactor to last commit

--- db_stats/migrate/20171015230821_change_purchase_precisions.rb
+++ db_stats/migrate/20171015230821_change_purchase_precisions.rb
@@ -1,4 +1,4 @@
-class ChangeSaleItemsPrecisions < ActiveRecord::Migration[5.1]
+class ChangePurchasePrecisions < ActiveRecord::Migration[5.1]
   def change
     change_column :stats, :monetary_unit_price, :decimal, precision: 10, scale: 2
     change_column :stats, :monetary_total_price, :decimal, precision: 10, scale: 2
