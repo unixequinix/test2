6966d3c2304ab547801d25b1f9e273c4dcbeffe7
Fran <francisco.moya.martinez@gmail.com>
2017-10-19 13:24:39 +0200

    fix (wiredlion): remove duplicated return (#338)

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -2,8 +2,6 @@ module PaymentGatewaysHelper
   def store_redirection(event, type, options = {})
     return send("new_event_#{type}_path", event) unless event.payment_gateways.vouchup.any?
 
-    return path unless event.payment_gateways.topup.pluck(:name).include?('vouchup')
-
     params = { path: "/register/#{event.slug}/glownet/#{current_customer.id}" }
     params = { path: "/refund/#{event.slug}/customer-details", query: { email: current_customer.email, gtag: options[:gtag_uid] }.to_param } if type == :refund # rubocop:disable Metrics/LineLength
 
