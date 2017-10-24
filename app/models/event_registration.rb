8a064b60553e1be4428110c2b0010831038cd98a
Jake <jcpenche@gmail.com>
2017-10-24 11:31:58 +0200

    fix (user registrations): avoid error when inviting more than 1 user

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -3,7 +3,7 @@ class EventRegistration < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :email, format: Devise.email_regexp
-  validates :user_id, uniqueness: { scope: :event_id }
+  validates :user_id, uniqueness: { scope: :event_id }, allow_nil: true
 
   enum role: { promoter: 1, support: 2 }
 end
