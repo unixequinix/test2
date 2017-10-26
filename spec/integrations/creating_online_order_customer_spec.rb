6fd4e67b3fec30c2f48c33eef7b3657379044c35
Bojan Marjanovic <marjanovic93@gmail.com>
2017-10-26 12:59:00 +0200

    test(integration customer order): Added more specifications to cover additional cases (#351)

--- spec/integrations/creating_online_order_customer_spec.rb
+++ spec/integrations/creating_online_order_customer_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Create an online order", type: :feature do
   let(:event) { create(:event, state: "created") }
-  let(:customer) { create(:customer, email: "test@customer.com", event: event) }
+  let(:customer) { create(:customer, email: "test@customer.com", event: event, anonymous: false) }
 
   before do
-    @ticket = create(:ticket, event: event)
+    @ticket = create(:ticket, event: event, customer: customer)
     @gtag = create(:gtag, event: event)
     @catalog_item = create(:catalog_item, event: event)
+    @item_crd = event.catalog_items.find_by(name: "CRD")
+    @station = create(:station, event: event)
+    @station_catalog_item_crd = create(:station_catalog_item, station: @station, catalog_item: @item_crd, price: 1.0)
+    @station_catalog_item = create(:station_catalog_item, station: @station, catalog_item: @catalog_item, price: 0.0)
+    create(:payment_gateway, event: event, topup: true, name: "paypal", login: "test.glownet.com", password: "test", signature: "test")
     login_as(customer, scope: :customer)
     visit customer_root_path(event)
   end
@@ -19,10 +24,10 @@ RSpec.describe "Create an online order", type: :feature do
   it "is located in ticket_assignments/new and can activate a ticket" do
     find_link("add_new_ticket_link").click
     expect(page).to have_current_path(new_event_ticket_assignment_path(event))
-
+    ticket = create(:ticket, event: event)
     expect do
       within("#new_ticket") do
-        fill_in 'ticket_reference', with: @ticket.code
+        fill_in 'ticket_reference', with: ticket.code
       end
       find("input[name=commit]").click
     end.to change(customer.tickets, :count).by(1)
@@ -56,5 +61,53 @@ RSpec.describe "Create an online order", type: :feature do
     order.complete!
 
     expect(order.order_items.map(&:catalog_item)).to include(@catalog_item)
+  end
+
+  it "is located in orders/new and topup with credits" do
+    find_link("new_top_up").click
+    expect(page).to have_current_path(new_event_order_path(event))
+    expect do
+      within("#checkout-form") do
+        fill_in 'input-range', with: 50
+      end
+
+      find("button[name=commit]").click
+      @order = event.orders.last
+      expect(page).to have_current_path(event_order_path(event, @order))
+      @order.complete!
+      expect(@order.order_items.map(&:catalog_item)).to include @item_crd
+    end.to change(customer, :global_credits)
+  end
+
+  it "is located in orders/new and buy items with total of 0" do
+    find_link("new_top_up").click
+    expect(page).to have_current_path(new_event_order_path(event))
+
+    within("#checkout-form") do
+      fill_in 'input-range', with: 0
+      all("#amount-input-#{@catalog_item.id} option")[1].select_option
+    end
+
+    find("button[name=commit]").click
+    @order = event.orders.last
+    expect(page).to have_current_path(event_order_path(event, @order))
+    @order.complete!
+    expect(@order.order_items.map(&:catalog_item)).to include @catalog_item
+  end
+
+  it "is located in orders/new and buy items with total greater than 0" do
+    find_link("new_top_up").click
+    expect(page).to have_current_path(new_event_order_path(event))
+
+    within("#checkout-form") do
+      fill_in 'input-range', with: 50
+      all("#amount-input-#{@catalog_item.id} option")[1].select_option
+    end
+
+    find("button[name=commit]").click
+    @order = event.orders.last
+    expect(page).to have_current_path(event_order_path(event, @order))
+    @order.complete!
+    expect(@order.order_items.map(&:catalog_item)).to include @catalog_item
   end
 end
