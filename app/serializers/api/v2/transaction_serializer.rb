4694ed15eef5b646aae6d1d9e82bff1e5a978e2c
Kashir <miguelkashir@users.noreply.github.com>
2017-11-06 14:42:11 +0200

    feature (GA-3544): add action to customer transactions endpoint (#364)

--- app/serializers/api/v2/transaction_serializer.rb
+++ app/serializers/api/v2/transaction_serializer.rb
@@ -1,5 +1,5 @@
 module Api::V2
   class TransactionSerializer < ActiveModel::Serializer
-    attributes :id, :gtag_id, :station_id, :credits, :refundable_credits, :final_balance, :final_refundable_balance, :sale_items
+    attributes :id, :action, :gtag_id, :station_id, :credits, :refundable_credits, :final_balance, :final_refundable_balance, :sale_items
   end
 end
