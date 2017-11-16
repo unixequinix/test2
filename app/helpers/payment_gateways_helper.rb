a6bded5412188203b68c2b9ff1f31857e3515102
Jake <jcpenche@gmail.com>
2017-11-16 13:54:52 +0100

    fix (wiredlion redirection): urgent prod change to avoid wiredlion redirection

--- app/helpers/payment_gateways_helper.rb
+++ app/helpers/payment_gateways_helper.rb
@@ -1,6 +1,10 @@
 module PaymentGatewaysHelper
   def store_redirection(event, type, options = {})
-    return send("new_event_#{type}_path", event) unless event.payment_gateways&.vouchup&.topup&.any? || event.payment_gateways&.vouchup&.refund&.any?
+    if type == :refund
+      return send("new_event_#{type}_path", event) unless event.payment_gateways&.vouchup&.refund&.any?
+    else
+      return send("new_event_#{type}_path", event) unless event.payment_gateways&.vouchup&.topup&.any?
+    end
 
     params = { path: "/register/#{event.slug}/glownet/#{current_customer.id}" }
     params = { path: "/refund/#{event.slug}/customer-details", query: { email: current_customer.email, gtag: options[:gtag_uid] }.to_param } if type == :refund # rubocop:disable Metrics/LineLength
