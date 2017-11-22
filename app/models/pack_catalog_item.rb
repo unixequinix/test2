618c99f99a9f3186793bc8e49c134e4270fe04da
Bojan Marjanovic <marjanovic93@gmail.com>
2017-11-22 12:29:49 +0100

    fix (GA-3606): add operator permissions count validation (#394)
    * fix (packs): add operator permissions count validation
    
    * fix (packs): add validation for uniqueness of items

--- app/models/pack_catalog_item.rb
+++ app/models/pack_catalog_item.rb
@@ -1,20 +1,32 @@
 class PackCatalogItem < ApplicationRecord
-  belongs_to :pack, touch: true
+  belongs_to :pack, inverse_of: :pack_catalog_items, touch: true
   belongs_to :catalog_item
 
   validates :amount, numericality: { greater_than: 0, less_than_or_equal_to: (->(item) { item.pack.event.maximum_gtag_balance }) }
 
   validate :limit_amount, if: :infinite?
   validate :packception
-
-  private
-
-  def packception
-    errors[:catalog_item] << "Cannot be a pack" if catalog_item.is_a?(Pack)
-  end
+  validate :operator_permissions_count
+  validate :item_uniqueness
 
   def infinite?
     catalog_item.infinite? if catalog_item.is_a?(Access)
+  end
+
+  private
+
+  def operator_permissions_count
+    count = pack.pack_catalog_items.count { |item| item.catalog_item.is_a?(OperatorPermission) && !item.marked_for_destruction? }
+    errors[:catalog_item_id] << 'Cannot have more than 8 operator permissions' if count > 8
+  end
+
+  def item_uniqueness
+    condition = pack.pack_catalog_items.size != pack.pack_catalog_items.group_by(&:catalog_item_id).size
+    errors[:catalog_item_id] << 'Cannot add the same item twice' if condition
+  end
+
+  def packception
+    errors[:catalog_item] << "Cannot be a pack" if catalog_item.is_a?(Pack)
   end
 
   def limit_amount
