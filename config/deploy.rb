e6edd464c5ab7d77d93bf87325ddae56019bc973
Fran <francisco.moya.martinez@gmail.com>
2017-11-23 14:19:52 +0100

    feature (capistrano): ask branch on capistrano deploy (#401)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, "git@github.com:Gl0wnet/web-core.git"
 set :migration_role, :app
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy"
