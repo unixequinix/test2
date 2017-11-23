f3918d35c2bdc741d2200c1fc32673313fcadf0c
Jake <jcpenche@gmail.com>
2017-11-23 16:37:10 +0100

    feature (API V2): adds refundable to customers endpoint

--- app/serializers/api/v2/full/customer_serializer.rb
+++ app/serializers/api/v2/full/customer_serializer.rb
@@ -1,10 +1,14 @@
 module Api::V2
   class Full::CustomerSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :email, :phone, :birthdate, :phone, :postcode, :address,
-               :city, :country, :gender, :global_refundable_money, :global_money, :locale, :anonymous
+    attributes :id, :first_name, :last_name, :email, :phone, :birthdate, :phone, :postcode, :address, :city, :country, :gender,
+               :global_refundable_money, :global_money, :locale, :anonymous, :refundable
 
     has_many :gtags, serializer: Simple::GtagSerializer
     has_many :tickets, serializer: Simple::TicketSerializer
     has_many :orders, serializer: OrderSerializer
+
+    def refundable
+      object.valid_balance? && !object.active_gtag&.banned?
+    end
   end
 end
