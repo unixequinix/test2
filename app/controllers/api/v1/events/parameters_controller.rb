f84c6c95bef96eca722bac2e7c7bad5622cf9311
Jake <jcpenche@gmail.com>
2017-12-13 18:13:06 +0100

    fix (GA-3661): remove staging from gtag_key hack

--- app/controllers/api/v1/events/parameters_controller.rb
+++ app/controllers/api/v1/events/parameters_controller.rb
@@ -9,9 +9,9 @@ class Api::V1::Events::ParametersController < Api::V1::Events::BaseController
     cols = %w[uid_reverse sync_time_gtags sync_time_tickets transaction_buffer days_to_keep_backup sync_time_customers fast_removal_password
               private_zone_password sync_time_server_date sync_time_basic_download sync_time_event_parameters gtag_type gtag_deposit_fee
               initial_topup_fee topup_fee maximum_gtag_balance stations_apply_orders stations_initialize_gtags]
-
+    
     body = cols.map { |col| { name: col, value: @current_event.send(col) } }
-    production_env = Rails.env.production? || Rails.env.demo? || Rails.env.hotfix? # rubocop:disable Rails/UnknownEnv
+    production_env = Rails.env.production? || Rails.env.demo? || Rails.env.hotfix? || Rails.env.staging? # rubocop:disable Rails/UnknownEnv
     fake_key = "11111111111111111111111111111111"
     value = production_env ? @current_event.gtag_key : fake_key
 
