372eddd7224bf6dd432ecd9117fea391e42c1f7f
Jake <jcpenche@gmail.com>
2017-12-15 12:26:27 +0100

    test (integration): fix

--- app/controllers/events/sessions_controller.rb
+++ app/controllers/events/sessions_controller.rb
@@ -3,7 +3,7 @@ class Events::SessionsController < Devise::SessionsController
 
   before_action :set_event
   before_action :resolve_locale
-    
+
   skip_before_action :verify_authenticity_token
   layout "customer"
 
