a4755d98c4c7a519e4fd10c1176487444b5d4fb7
Jake <jcpenche@gmail.com>
2017-12-28 17:15:46 +0100

    fix (policies): addes status) and status_9 to poke policy

--- app/policies/poke_policy.rb
+++ app/policies/poke_policy.rb
@@ -22,4 +22,12 @@ class PokePolicy < ApplicationPolicy
   def update_multiple?
     user.glowball?
   end
+
+  def status_9?
+    admin_or_promoter && event_open
+  end
+
+  def status_0?
+    admin_or_promoter && event_open
+  end
 end
