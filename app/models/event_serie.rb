de93ff6193aa7e3d2c910eb4efe186f460da0521
Jake <jcpenche@gmail.com>
2018-01-03 13:23:37 +0100

    fix (GA-3704): event serie name must be unique

--- app/models/event_serie.rb
+++ app/models/event_serie.rb
@@ -1,5 +1,5 @@
 class EventSerie < ApplicationRecord
   has_many :events, dependent: :nullify
 
-  validates :name, presence: true, allow_blank: false
+  validates :name, presence: true, allow_blank: false, uniqueness: true
 end
