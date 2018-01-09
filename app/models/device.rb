dc223fcc8c80e77ff069531270218f3c2b09a804
Jake <jcpenche@gmail.com>
2018-01-09 14:18:13 +0100

    fix (teams): updates UI to review device status

--- app/models/device.rb
+++ app/models/device.rb
@@ -4,7 +4,10 @@ class Device < ApplicationRecord
   belongs_to :team
   has_many :device_registrations, dependent: :destroy
   has_many :events, through: :device_registrations, dependent: :destroy
+  has_one :device_registration, -> { where(allowed: false) }, dependent: :destroy
+  has_one :event, through: :device_registration, dependent: :destroy
   has_many :device_transactions, dependent: :restrict_with_error
+
   validates :mac, uniqueness: true, allow_blank: false
   validates :mac, presence: true
 
