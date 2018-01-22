934f33c42012de7643d535ce03957e82efd20d48
Jake <jcpenche@gmail.com>
2018-01-22 17:05:19 +0100

    fix (API V1): adds catalog_item_type to orders serializer

--- app/serializers/api/v1/order_item_serializer.rb
+++ app/serializers/api/v1/order_item_serializer.rb
@@ -1,8 +1,12 @@
 module Api
   module V1
     class OrderItemSerializer < ActiveModel::Serializer
-      attributes :catalog_item_id, :amount, :status, :redeemed
+      attributes :catalog_item_id, :catalog_item_type, :amount, :status, :redeemed
       attribute :counter, key: :id
+
+      def catalog_item_type
+        catalog_item.class.to_s if catalog_item
+      end
 
       def status
         object.order.status
