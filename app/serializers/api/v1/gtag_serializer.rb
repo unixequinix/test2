384b68b4dc9f5670e2ee22a1a5609990b81f9a6a
Jake <jcpenche@gmail.com>
2018-01-22 16:51:17 +0100

    feature (API V1): adds ticket attributes to gtag call

--- app/serializers/api/v1/gtag_serializer.rb
+++ app/serializers/api/v1/gtag_serializer.rb
@@ -1,7 +1,15 @@
 module Api
   module V1
     class GtagSerializer < ActiveModel::Serializer
-      attributes :reference, :redeemed, :banned, :customer
+      attributes :reference, :redeemed, :banned, :catalog_item_id, :catalog_item_type, :ticket_type_id, :customer
+
+      def catalog_item_id
+        object.ticket_type&.catalog_item_id
+      end
+
+      def catalog_item_type
+        object.ticket_type&.catalog_item.class.to_s
+      end
 
       def customer
         CustomerSerializer.new(object.customer).as_json if object.customer
