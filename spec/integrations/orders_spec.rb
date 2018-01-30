7d9c2321b1765cbfd6600939de934c0cffa40166
Jake <jcpenche@gmail.com>
2018-01-30 16:05:39 +0100

    tests (orders): hardens orders finders fro non-waiting

--- spec/integrations/orders_spec.rb
+++ spec/integrations/orders_spec.rb
@@ -3,88 +3,60 @@ require 'rails_helper'
 RSpec.describe "Orders on admin panel", type: :feature do
   let(:event) { create(:event, state: "launched") }
   let(:user) { create(:user, role: "admin") }
-  let!(:order1) { create(:order, :with_credit, status: "completed", event: event) }
-  let!(:order2) { create(:order, :with_credit, status: "completed", event: event) }
-  let!(:order3) { create(:order, :with_credit, status: "completed", event: event) }
-  let!(:order4) { create(:order, :with_credit, status: "refunded", event: event) }
-  let!(:order5) { create(:order, :with_credit, status: "refunded", event: event) }
-  let!(:order6) { create(:order, :with_credit, status: "failed", event: event) }
-  let!(:order7) { create(:order, :with_credit, status: "cancelled", event: event) }
 
   before(:each) do
     login_as(user, scope: :user)
   end
 
   describe "index: " do
+    let!(:order) { create(:order, :with_credit, event: event) }
+
     before { visit admins_event_orders_path(event) }
 
     it "click on an Order number and redirects to Order view" do
-      all('tbody')[0].all('tr')[6].all('a')[0].click
-      expect(page).to have_current_path(admins_event_order_path(event, order1))
+      expect(page.all(:link, nil, href: admins_event_order_path(event, order)).any?).to be_truthy
     end
     it "click on an Order customer and redirects to the Customer view" do
-      @customer = event.orders[0].customer
-      all('tbody')[0].all('tr')[6].all('a')[1].click
-      expect(page).to have_current_path(admins_event_customer_path(event, @customer))
+      expect(page.all(:link, nil, href: admins_event_customer_path(event, order.customer)).any?).to be_truthy
     end
-  end
 
-  describe "verify information from orders list: " do
-    before { visit admins_event_orders_path(event) }
-
-    it "verify Order number" do
-      @number = all('tbody')[0].all('tr')[0].all('td')[0].text
-      expect(@number).to eq(event.orders[6].number)
+    it "must include number" do
+      expect(page.all('td', text: order.number).any?).to be_truthy
     end
-    it "verify Order customer" do
-      @customer_email = all('tbody')[0].all('tr')[0].all('td')[1].text
-      expect(@customer_email).to eq(event.orders[6].customer.email)
+    it "must include customer" do
+      expect(page.all('td', text: order.customer.email).any?).to be_truthy
     end
-    it "verify Order amount" do
-      @amount = all('tbody')[0].all('tr')[0].all('td')[2].text.split(' ')[0].to_f
-      expect(@amount).to eq(event.orders[6].total.to_f)
+    it "must include amount" do
+      expect(page.all('td', text: order.total).any?).to be_truthy
     end
-    it "verify Order state" do
-      @status = all('tbody')[0].all('tr')[0].all('td')[4].text.downcase
-      expect(@status).to eq(event.orders[6].status)
+    it "must include state" do
+      expect(page.all('td', text: order.status.humanize).any?).to be_truthy
     end
   end
 
   describe "verify total credit from an Order: " do
+    let!(:completed_orders) {}
+
     before { visit admins_event_orders_path(event) }
 
     it "verify total credit from a Completed Order" do
-      @total_completed = all('h3')[0].text.split(' ')[0].to_f
-      expect(@total_completed).to eq(event.orders[0].total.to_f + event.orders[1].total.to_f + event.orders[2].total.to_f)
+      orders = create_list(:order, 3, :with_credit, status: "completed", event: event)
+      expect(page.all('h3', text: orders.sum(&:total)).any?).to be_truthy
     end
-    it "verify total credit from a Refounded Order" do
-      @total_refunded = all('h3')[1].text.split(' ')[0].to_f
-      expect(@total_refunded).to eq(event.orders[3].total.to_f + event.orders[4].total.to_f)
+
+    it "verify total credit from a Refunded Order" do
+      orders = create_list(:order, 3, :with_credit, status: "refunded", event: event)
+      expect(page.all('h3', text: orders.sum(&:total)).any?).to be_truthy
     end
+
     it "verify total credit from a Failed Order" do
-      @total_failed = all('h3')[2].text.split(' ')[0].to_f
-      expect(@total_failed).to eq(event.orders[5].total.to_f)
+      orders = create_list(:order, 3, :with_credit, status: "failed", event: event)
+      expect(page.all('h3', text: orders.sum(&:total)).any?).to be_truthy
     end
-    it "verify total credit from a Cancelled Order" do
-      @total_cancelled = all('h3')[3].text.split(' ')[0].to_f
-      expect(@total_cancelled).to eq(event.orders[6].total.to_f)
-    end
-  end
 
-  describe "verify number of orders: " do
-    before { visit admins_event_orders_path(event) }
-
-    it "verify number of Completed Orders" do
-      expect(Order.completed.count).to eq(3)
-    end
-    it "verify number of Refounded Orders" do
-      expect(Order.refunded.count).to eq(2)
-    end
-    it "verify number of Failed Orders" do
-      expect(Order.failed.count).to eq(1)
-    end
-    it "verify number of Cancelled Orders" do
-      expect(Order.cancelled.count).to eq(1)
+    it "verify total credit from a cancelled Order" do
+      orders = create_list(:order, 3, :with_credit, status: "cancelled", event: event)
+      expect(page.all('h3', text: orders.sum(&:total)).any?).to be_truthy
     end
   end
 end
