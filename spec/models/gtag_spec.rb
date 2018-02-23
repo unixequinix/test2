4b18b0b2aa0922b30ad0919100686741304664f3
Jake <jcpenche@gmail.com>
2018-02-23 15:52:08 +0100

    fix (missing): to last commit

--- spec/models/gtag_spec.rb
+++ spec/models/gtag_spec.rb
@@ -45,22 +45,6 @@ RSpec.describe Gtag, type: :model do
       it "changes the gtags final_virtual_balance" do
         expect { subject.recalculate_balance }.to change { subject.reload.final_virtual_balance.to_f }.from(0.00).to(20)
       end
-
-      it "sets consistent to the value of valid_balance?" do
-        value = [true, false].sample
-        allow(subject).to receive(:valid_balance?).and_return(value)
-        expect { subject.recalculate_balance }.to change { subject.reload.consistent? }.to(value)
-      end
-
-      it "sets the value of complete to true if there are no counters" do
-        subject.pokes_as_customer.update_all(gtag_counter: nil)
-        expect { subject.recalculate_balance }.to change { subject.reload.complete? }.to(true)
-      end
-
-      it "sets the value of complete to true if all counters are present" do
-        subject.pokes_as_customer.map.with_index { |p, counter| p.update!(gtag_counter: counter + 1) }
-        expect { subject.recalculate_balance }.to change { subject.reload.complete? }.to(true)
-      end
     end
 
     it "creates an alert if final_balance is negative" do
