3bf22322bee7a33ef3c38bd1271ac584b59b010f
Jake <jcpenche@gmail.com>
2018-02-28 15:08:03 +0100

    fix (customer): alias for money

--- app/serializers/api/v2/full/customer_serializer.rb
+++ app/serializers/api/v2/full/customer_serializer.rb
@@ -1,7 +1,7 @@
 module Api::V2
   class Full::CustomerSerializer < ActiveModel::Serializer
     attributes :id, :first_name, :last_name, :email, :phone, :birthdate, :phone, :postcode, :address, :city, :country, :gender,
-               :virtual_money, :money, :locale, :anonymous, :refundable
+               :virtual_money, :money, :global_refundable_money, :locale, :anonymous, :refundable
 
     has_many :gtags, serializer: Simple::GtagSerializer
     has_many :tickets, serializer: Simple::TicketSerializer
@@ -12,7 +12,7 @@ module Api::V2
     end
 
     def global_refundable_money
-      money
+      object.money
     end
   end
 end
