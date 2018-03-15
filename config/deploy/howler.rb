4fae9c58551271f07a6f507d8cf55e2d4e72bca1
Jake <jcpenche@gmail.com>
2018-03-15 15:09:46 +0100

    chore (deploy): move howlers api

--- config/deploy/howler.rb
+++ config/deploy/howler.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "52.211.103.33", user: "deploy", roles: %w[web app db]
+server "howler.glownet.com", user: "deploy", roles: %w[web app db]
 
 ask(:branch, proc { `git rev-parse --abbrev-ref develop`.chomp })
 set(:rails_env, "howler")
