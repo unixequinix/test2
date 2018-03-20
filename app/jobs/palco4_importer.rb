38e4d8f6a20412f6bb66bbaef7f1a142e8ddb252
Jake <jcpenche@gmail.com>
2018-03-20 11:53:13 +0100

    fix (companies): remove from palco4 import

--- app/jobs/palco4_importer.rb
+++ app/jobs/palco4_importer.rb
@@ -2,7 +2,6 @@ class Palco4Importer < ApplicationJob
   def perform(ticket, event)
     ticket = Hashie::Mash.new ticket
 
-    name = "Sónar+D 2018"
     company_code = ticket.zoneId
     ticket_name = ticket.zoneName
     ticket_code = ticket.code
@@ -10,8 +9,7 @@ class Palco4Importer < ApplicationJob
     last_name = ticket.buyerSurName
     email = ticket.buyerEmail
 
-    company = event.companies.find_or_create_by(name: name)
-    ticket_type = event.ticket_types.find_or_initialize_by(company_code: company_code, company: company)
+    ticket_type = event.ticket_types.find_or_initialize_by(company_code: company_code, company: "Sónar+D 2018")
     ticket_type.update!(name: ticket_name)
     new_ticket = event.tickets.find_or_initialize_by(code: ticket_code)
     new_ticket.update!(ticket_type: ticket_type, purchaser_first_name: first_name, purchaser_last_name: last_name, purchaser_email: email)
