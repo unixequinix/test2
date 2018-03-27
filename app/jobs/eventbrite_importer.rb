e195abf6836c15dd146fa2f8341d9554ee1f8600
Jake <jcpenche@gmail.com>
2018-03-27 12:10:06 +0200

    fix (eventbrite): company belongs to ticket_type

--- app/jobs/eventbrite_importer.rb
+++ app/jobs/eventbrite_importer.rb
@@ -10,13 +10,13 @@ class EventbriteImporter < ApplicationJob
       guest["barcodes"].each do |barcode|
         profile = guest["profile"]
         begin
-          ctt = TicketType.find_or_initialize_by(company_code: guest["ticket_class_id"], event_id: event_id)
+          ctt = TicketType.find_or_initialize_by(company: "Eventbrite - #{order[:event_id]}", company_code: guest["ticket_class_id"], event_id: event_id)
           ctt.update(name: guest["ticket_class_name"])
         rescue ActiveRecord::RecordNotUnique
           retry
         end
         begin
-          ticket = Ticket.find_or_initialize_by(company: "Eventbrite - #{order[:event_id]}", code: barcode["barcode"], ticket_type: ctt, event_id: event_id)
+          ticket = Ticket.find_or_initialize_by(code: barcode["barcode"], ticket_type: ctt, event_id: event_id)
           ticket.update(created_at: barcode["created"])
         rescue ActiveRecord::RecordNotUnique
           retry
