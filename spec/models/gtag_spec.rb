ad6445c31183556b7f77e8bb1a7ad2385faa5ca7
Jake <jcpenche@gmail.com>
2018-04-05 11:20:25 +0200

    tests (gtag): recalculate_balance tested

--- spec/models/gtag_spec.rb
+++ spec/models/gtag_spec.rb
@@ -22,4 +22,58 @@ RSpec.describe Gtag, type: :model do
     subject.customer = nil
     expect(subject).not_to be_assigned
   end
+
+  describe ".recalculate_balance" do
+    context "with payments" do
+      before do
+        create_list(:poke, 3, customer_gtag: subject, credit: event.credit, credit_amount: 30, final_balance: 2)
+        create_list(:poke, 3, customer_gtag: subject, credit: event.virtual_credit, credit_amount: 70, final_balance: 20)
+      end
+
+      it "changes the gtags credits" do
+        expect { subject.recalculate_balance }.to change { subject.credits.to_f }.from(0.00).to(90)
+      end
+
+      it "changes the gtags virtual_credits" do
+        expect { subject.recalculate_balance }.to change { subject.virtual_credits.to_f }.from(0.00).to(210)
+      end
+
+      it "changes the gtags final_balance" do
+        expect { subject.recalculate_balance }.to change { subject.reload.final_balance.to_f }.from(0.00).to(2)
+      end
+
+      it "changes the gtags final_virtual_balance" do
+        expect { subject.recalculate_balance }.to change { subject.reload.final_virtual_balance.to_f }.from(0.00).to(20)
+      end
+
+      it "sets consistent to the value of valid_balance?" do
+        allow(subject).to receive(:valid_balance?).and_return(false)
+        expect { subject.recalculate_balance }.to change { subject.reload.consistent? }.from(true).to(false)
+      end
+
+      it "sets the value of complete to true if there are no counters" do
+        subject.pokes.update_all(gtag_counter: nil)
+        subject.update!(complete: false)
+        expect { subject.recalculate_balance }.to change { subject.reload.complete? }.from(false).to(true)
+      end
+
+      it "sets the value of complete to true if all counters are present" do
+        subject.pokes.map.with_index { |p, counter| p.update!(gtag_counter: counter + 1) }
+        subject.update!(complete: false)
+        expect { subject.recalculate_balance }.to change { subject.reload.complete? }.from(false).to(true)
+      end
+    end
+
+    it "creates an alert if final_balance is negative" do
+      allow(subject).to receive(:final_balance).and_return(-10)
+      expect(Alert).to receive(:propagate).once
+      subject.recalculate_balance
+    end
+
+    it "creates an alert if final_virtual_balance is negative" do
+      allow(subject).to receive(:final_virtual_balance).and_return(-10)
+      expect(Alert).to receive(:propagate).once
+      subject.recalculate_balance
+    end
+  end
 end
