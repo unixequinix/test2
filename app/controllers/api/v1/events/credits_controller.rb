c6b8e4194e37ee808ed69b5a26f530de15b21189
Fran <francisco.moya.martinez@gmail.com>
2018-04-23 17:59:19 +0200

    fix (GC-205): fix customer transactions report (#598)
    * fix (GC-205): fix customer transactions report
    
    * fix (GC-205): fix develop fail test and guard improvements
    
    * fix (GC-205): fix wrong link helper
    
    * fix (GC-205): remove test correction to avoid conflicts

--- app/controllers/api/v1/events/credits_controller.rb
+++ app/controllers/api/v1/events/credits_controller.rb
@@ -5,7 +5,7 @@ module Api
         def index
           credits = @current_event.catalog_items.where(type: %w[Credit VirtualCredit])
           credits = credits.map { |credit| CreditSerializer.new(credit) }
-          render status: 200, json: credits.as_json
+          render status: :ok, json: credits.as_json
         end
       end
     end
