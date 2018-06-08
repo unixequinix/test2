d0b13d76b31f839ad989ced0b9d4f6cc53d967ca
Jake <jcpenche@gmail.com>
2018-06-08 16:51:34 +0200

    tests (rubocop): pass

--- app/controllers/companies/api/v1/base_controller.rb
+++ app/controllers/companies/api/v1/base_controller.rb
@@ -8,7 +8,7 @@ class Companies::Api::V1::BaseController < Companies::BaseController
     authenticate_or_request_with_http_basic do |event_token, company_token|
       @current_event = Event.find_by(id: event_token)
       @user = @current_event&.users&.find_by(access_token: company_token)
-      @current_event && @user && @current_event.open_api? || render(status: 403, json: :unauthorized)
+      @current_event && @user && @current_event.open_api? || render(status: :forbidden, json: :unauthorized)
     end
   end
 end
