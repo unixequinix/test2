5e0d6cea62bd387892519b0b510b4048d4e2d192
Jake <jcpenche@gmail.com>
2018-06-19 12:16:54 +0200

    fix (GC-255): customer management search is limited to only customers

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -4,9 +4,9 @@ class Admission
   KLASSES = { ticket: Ticket, gtag: Gtag, customer: Customer }.freeze
 
   def self.search(event, query)
-    Customer.where(event: event).search(email_or_first_name_or_last_name_cont: query).result +
-      Gtag.where(event: event).includes(:customer).search(tag_uid_cont: query).result +
-      Ticket.where(event: event).includes(:customer).search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result
+    Customer.where(event: event, operator: false).search(email_or_first_name_or_last_name_cont: query).result +
+      Gtag.where(event: event, operator: false).includes(:customer).search(tag_uid_cont: query).result +
+      Ticket.where(event: event, operator: false).includes(:customer).search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result
   end
 
   def self.find(event, id, klass)
@@ -15,13 +15,13 @@ class Admission
 
   def self.all(event, admission, query)
     if admission.customer && !admission.customer.anonymous?
-      (event.tickets.where(customer: nil).search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result +
-        event.gtags.where(customer: nil).search(tag_uid_cont: query).result +
-        event.customers.anonymous.search(email_or_first_name_or_last_name_cont: query).result) - [admission]
+      (event.tickets.where(customer: nil, operator: false).search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result +
+        event.gtags.where(customer: nil, operator: false).search(tag_uid_cont: query).result +
+        event.customers.where(operator: false).anonymous.search(email_or_first_name_or_last_name_cont: query).result) - [admission]
     else
-      (event.tickets.search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result +
-        event.gtags.search(tag_uid_cont: query).result +
-        event.customers.search(email_or_first_name_or_last_name_cont: query).result) - [admission]
+      (event.tickets.where(operator: false).search(code_or_purchaser_email_or_purchaser_first_name_or_purchaser_last_name_cont: query).result +
+        event.gtags.where(operator: false).search(tag_uid_cont: query).result +
+        event.customers.where(operator: false).search(email_or_first_name_or_last_name_cont: query).result) - [admission]
     end
   end
 end
