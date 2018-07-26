3bfcc4663138177cf1217a4d01dfc3a775473573
Jake <jcpenche@gmail.com>
2018-07-26 14:55:04 +0200

    feature (GC-188): adds sales analytics to API V2 stations#get

--- app/serializers/api/v2/station_serializer.rb
+++ app/serializers/api/v2/station_serializer.rb
@@ -1,7 +1,21 @@
 module Api::V2
   class StationSerializer < ActiveModel::Serializer
     attributes :id, :name, :group, :location, :category, :reporting_category, :address, :registration_num, :official_name, :hidden
+    attribute :credits_sold, if: :pos_station?
+    attribute :virtual_credits_sold, if: :pos_station?
 
     has_many :products, serializer: ProductSerializer
+
+    def credits_sold
+      object.credit_pos_sales_total(credit_filter: object.event.credit)
+    end
+
+    def virtual_credits_sold
+      object.credit_pos_sales_total(credit_filter: object.event.virtual_credit)
+    end
+
+    def pos_station?
+      object.category.in?(%w[bar vendor])
+    end
   end
 end
