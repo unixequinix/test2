2dc33b04f50eb8c5a748660c8cf5a733ecb82d52
Angel Garcia <angel.garcia@eu.equinix.com>
2018-09-24 13:54:02 +0200

    update afterdeploy.sh_v2

--- afterdeploy.sh
+++ afterdeploy.sh
@@ -6,10 +6,6 @@ rm -f /home/deploy/current/appspec.yml
 rm -f /home/deploy/current/afterdeploy.sh
 cd /home/deploy/current
 
-rvm install 2.5.0
-gem install bundler
-sudo apt-get install libpq-dev
-gem install pg -v '0.21.0'
 service nginx restart
 
 
