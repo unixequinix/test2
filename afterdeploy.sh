8485afcc5a4fc245e46ad4080b0e2ae0f9f9875d
Angel Garcia <angel.garcia@eu.equinix.com>
2018-10-02 12:12:22 +0200

    update affterdeploy.sh_v3

--- afterdeploy.sh
+++ afterdeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /home/deploy/current
-
+ln -s /home/deploy/shared/config/application.yml /home/deploy/current/config/application.yml
 /home/deploy/.rvm/bin/rvm 2.5.1 do bundle install --path /home/deploy/shared/bundle --without development test --deployment --quiet
 rm -f /home/deploy/current/beforedeploy.sh
 rm -f /home/deploy/current/appspec.yml
